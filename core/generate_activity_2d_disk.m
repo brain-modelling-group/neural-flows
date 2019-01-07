3dd1223bf7d7dc74b869b074b7cff13ec2ec4bcd
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-01-07 16:51:14 +1000

    Update

--- core/generate_activity_2d_disk.m
+++ core/generate_activity_2d_disk.m
@@ -1,6 +1,16 @@
-
-% For EEG - MEG
 function generate_activity_2d_disk()
-% This function should generate activity natively on a disk OR
+%% This function should generate activity natively on a disk OR
 % use the topographic projection from a cloud of points in 3D to 2D
+%
+% ARGUMENTS:
+%    
+% OUTPUT:
+%
+% AUTHOR:
+%     Paula Sanz-Leon
+% USAGE:
+%{
+    
+%}
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 end
