8575e3f2222b381b415c691e17a398deda96d5c0
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-01-31 11:46:36 +1000

    OCD

--- core/calculate_derivatives_hsd3.m
+++ core/calculate_derivatives_hsd3.m
@@ -27,23 +27,23 @@ function [Ix, Iy, Iz, It] = calculate_derivatives_hsd3(F1, F2)
 end % function calculate_derivatives_hsd3()
 
 function [Gx, Gy, Gz] = get_sobel_3d_operator()
-    % Returns the 3D Normalized Sobel kernels
+% Returns the 3D Normalized Sobel kernels
         
-        norm_factor = 44; % sum(abs(G_i(:))); I just happen to know it's 44
-        Gx(:,:,1) = [-1  0  1; -3  0  3; -1  0  1];
-        Gx(:,:,2) = [-3  0  3; -6  0  6; -3  0  3];
-        Gx(:,:,3) = [-1  0  1; -3  0  3; -1  0  1];
-        Gx = Gx./norm_factor;
+    norm_factor = 44; % sum(abs(G_i(:))); I just happen to know it's 44
+    Gx(:,:,1) = [-1  0  1; -3  0  3; -1  0  1];
+    Gx(:,:,2) = [-3  0  3; -6  0  6; -3  0  3];
+    Gx(:,:,3) = [-1  0  1; -3  0  3; -1  0  1];
+    Gx = Gx./norm_factor;
 
-        Gy(:,:,1) = [-1 -3 -1;  0  0  0;  1  3  1];
-        Gy(:,:,2) = [-3 -6 -3;  0  0  0;  3  6  3];
-        Gy(:,:,3) = [-1 -3 -1;  0  0  0;  1  3  1];
-        Gy = Gy./norm_factor;
+    Gy(:,:,1) = [-1 -3 -1;  0  0  0;  1  3  1];
+    Gy(:,:,2) = [-3 -6 -3;  0  0  0;  3  6  3];
+    Gy(:,:,3) = [-1 -3 -1;  0  0  0;  1  3  1];
+    Gy = Gy./norm_factor;
 
-        Gz(:,:,1) = [-1 -3 -1; -3 -6 -3; -1 -3 -1];
-        Gz(:,:,2) = [ 0  0  0;  0  0  0;  0  0  0];
-        Gz(:,:,3) = [ 1  3  1;  3  6  3;  1  3  1];
-        Gz = Gz./norm_factor;
+    Gz(:,:,1) = [-1 -3 -1; -3 -6 -3; -1 -3 -1];
+    Gz(:,:,2) = [ 0  0  0;  0  0  0;  0  0  0];
+    Gz(:,:,3) = [ 1  3  1;  3  6  3;  1  3  1];
+    Gz = Gz./norm_factor;
         
 end % function get_sobel_3d_operator()
 
