b105251db32ed407276abb976beb046c4a7cc06a
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-01-31 17:08:04 +1000

    Use standard convolution

--- core/calculate_derivatives_hsd3.m
+++ core/calculate_derivatives_hsd3.m
@@ -1,4 +1,4 @@
-function [Ix, Iy, Iz, It] = calculate_derivatives_hsd3(F1, F2)
+function [Ix, Iy, Iz, It] = calculate_derivatives_hsd3(F1, F2, operator_3d)
 %This fuction computes 3D derivatives between two 3D images. 
 %There are four derivatives here; three along X, Y, Z axes and one along
 %timeline axis.
@@ -9,22 +9,30 @@ function [Ix, Iy, Iz, It] = calculate_derivatives_hsd3(F1, F2)
 %   - Ix, Iy, Iz : derivatives along X, Y and Z axes respectively
 %   - It         : derivatives along timeline axis
 %             Modified - Paula Sanz-Leon optimized, handles nans
-
+    
+    if nargin < 3 
+        operator_3d = @get_sobel_3d_operator;
+    end
 
     % Sobel 3D Kernel along X, Y and Z direction
-    [Gx, Gy, Gz] = get_sobel_3d_operator();
+    [Gx, Gy, Gz] = operator_3d();
 
     Gt = ones(3, 3, 3);
-    % I'm gonna average a little bit before calculating the time derivative
-    Gt = Gt./ sum(Gt(:));
-
+    % Make these parameters
+    hx = 1; % 1 mm
+    hy = 1; % 1 mm
+    hz = 1; % 1 mm
+    ht = 1; % 1 ms
 
     % Spatial derivatives are computed as the average of 
     % the two image/frame gradients along each direction
-    Ix = 0.5 * (nanconvn(F1, Gx, 'same') + nanconvn(F2, Gx, 'same'));
-    Iy = 0.5 * (nanconvn(F1, Gy, 'same') + nanconvn(F2, Gy, 'same'));
-    Iz = 0.5 * (nanconvn(F1, Gz, 'same') + nanconvn(F2, Gz, 'same'));
-    It = 0.5 * (nanconvn(F1, Gt, 'same') - nanconvn(F2, Gt, 'same'));
+    
+
+    sobel_norm_factor = 44;
+    Ix = 0.5 * (convn(F1, Gx, 'same') + convn(F2, Gx, 'same'));
+    Iy = 0.5 * (convn(F1, Gy, 'same') + convn(F2, Gy, 'same'));
+    Iz = 0.5 * (convn(F1, Gz, 'same') + convn(F2, Gz, 'same'));
+    It = 0.5 * (convn(F1, Gt, 'same') - convn(F2, Gt, 'same'));
 
 end % function calculate_derivatives_hsd3()
 
@@ -37,16 +45,37 @@ function [Gx, Gy, Gz] = get_sobel_3d_operator()
     Gx(:,:,3) = [-1  0  1; -3  0  3; -1  0  1];
     Gx = Gx./norm_factor;
 
-    Gy(:,:,1) = [-1 -3 -1;  0  0  0;  1  3  1];
-    Gy(:,:,2) = [-3 -6 -3;  0  0  0;  3  6  3];
-    Gy(:,:,3) = [-1 -3 -1;  0  0  0;  1  3  1];
-    Gy = Gy./norm_factor;
-
-    Gz(:,:,1) = [-1 -3 -1; -3 -6 -3; -1 -3 -1];
-    Gz(:,:,2) = [ 0  0  0;  0  0  0;  0  0  0];
-    Gz(:,:,3) = [ 1  3  1;  3  6  3;  1  3  1];
+    Gz(:,:,1) = [-1 -3 -1;  0  0  0;  1  3  1];
+    Gz(:,:,2) = [-3 -6 -3;  0  0  0;  3  6  3];
+    Gz(:,:,3) = [-1 -3 -1;  0  0  0;  1  3  1];
     Gz = Gz./norm_factor;
+
+    Gy(:,:,1) = [-1 -3 -1; -3 -6 -3; -1 -3 -1];
+    Gy(:,:,2) = [ 0  0  0;  0  0  0;  0  0  0];
+    Gy(:,:,3) = [ 1  3  1;  3  6  3;  1  3  1];
+    Gy = Gy./norm_factor;
         
 end % function get_sobel_3d_operator()
+
+function [Gx, Gy, Gz] = get_laplacian_3d_operator()
+    Gx = zeros(3,3,3); 
+    Gy = Gx; 
+    Gz = Gx;
+    
+    Gx(:,:,1) = [-1 0 1; -2 0 2; -1 0 1]; 
+    Gx(:,:,2) = [-1 0 1; -2 0 2; -1 0 1]; 
+    Gx(:,:,3) = [-1 0 1; -2 0 2; -1 0 1]; 
+    Gx = Gx./sum(abs(Gx(:)));
+
+    Gy(:,:,1) = Gx(:,:,1)';
+    Gy(:,:,2) = Gx(:,:,2)';
+    Gy(:,:,3) = Gx(:,:,3)';
+    
+    Gz(:,:,1) = [-1 -2 -1; -1 -2 -1; -1 -2 -1]; 
+    Gz(:,:,2) = [ 0  0  0;  0  0  0;  0  0  0]; 
+    Gz(:,:,3) = [ 1  2  1;  1  2  1;  1  2  1]; 
+    Gz = Gz./sum(abs(Gz(:)));
+
+end
 
  
