4bb5286d5ff50cb08a9f72189288d3552d107070
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-02-04 18:52:34 +1000

    Update

--- core/interpolate_3d_data.m
+++ core/interpolate_3d_data.m
@@ -1,4 +1,4 @@
-function interpolate_3d_data(data, locs, X, Y, Z)
+function interpolate_3d_data(data, locs, X, Y, Z, in_bdy_mask)
 % This is a wrapper function for scattered interpolant. We can interpolate
 % each frame independtly using parfor and save the interpolated data for 
 % later use with optical flow and then just delete the interpolated data
@@ -6,6 +6,8 @@ function interpolate_3d_data(data, locs, X, Y, Z)
 
 %  locs: locations of known data
 %  data: scatter data known at locs of size tpts x nodes
+% X, Y Z -- grid points to get interpolation out
+
 % This is the key step for the optical flow method to work
 % These parameters are eesential
 
@@ -24,15 +26,22 @@ mfile_object = matfile('temp_file_interpolated_data.mat','Writable', true);
 
 mfile_object.data(size(X, x_dim), size(Y, y_dim), size(Z, z_dim), tpts) = 0;
 
-parfor this_tpt=1:tpts
-    
-data_interpolant = scatteredInterpolant(locs(1:end, x_dim), ...
-                                        locs(1:end, y_dim), ...
-                                        locs(1:end, z_dim), ...
-                                        data(this_tpt, :), ...
-                                        neighbour_method, ...
-                                        extrapolation_method);
+temp_data = nan(size(X));
 
-mfile_object(:, :, :, this_tpt) = data_interpolant(X, Y, Z);
+for this_tpt=1:tpts
+    
+    data_interpolant = scatteredInterpolant(locs(1:end, x_dim), ...
+                                            locs(1:end, y_dim), ...
+                                            locs(1:end, z_dim), ...
+                                            data(this_tpt, :).', ...
+                                            neighbour_method, ...
+                                            extrapolation_method);
+
+    temp_data(in_bdy_mask) = data_interpolant(X(in_bdy_mask).', Y(in_bdy_mask).', Z(in_bdy_mask).');
+    
+    % Only get 
+    mfile_object.data(:, :, :, this_tpt) = temp_data;
+
+end
 
 end
