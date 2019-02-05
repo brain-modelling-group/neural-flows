25e921b574fdfe7865e49ebe470e1264d8e9ff42
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-02-05 11:06:51 +1000

    Document function

--- utils/find_min_max.m
+++ utils/find_min_max.m
@@ -1,6 +1,29 @@
 function [min_d, max_d] = find_min_max(data, minmax_mode)
-% Finds minimum and maximum across all elements of data
-
+%% Finds minimum and maximum across all elements of input array data
+%
+% ARGUMENTS:
+%        data        -- an nD array with numeical values (int, float, etc)
+%        minmax_mode -- an string specifying what type of min max values the 
+%                       function returns. The mode 'symmetric' is the only vailable
+%                       alternative now. This mode is useful for diverging data around zero.
+%                       The min max values returned are useful for setting
+%                       caxis in image-based figures.
+%
+% OUTPUT: 
+%        min_d -- scalar. Minimum value of data.
+%        max_d -- scalar. Maximum value of data.
+%
+% REQUIRES: 
+%        none
+%
+% USAGE:
+%{
+    
+%}
+%
+% AUTHOR: 
+%        Paula Sanz-Leon, QIMR Berghofer, 2019
+% TODO: use switch case block once there are more minma_mode options
 if nargin < 2
     minmax_mode = 'none';
 end
@@ -15,4 +38,4 @@ else
 end
 
 
-end
+end % function fin_min_max()
