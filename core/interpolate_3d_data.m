63f471a7d5519b59fe777ffab029aabfa1111aba
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-02-05 13:01:13 +1000

    Handle file creation and data storage

--- core/interpolate_3d_data.m
+++ core/interpolate_3d_data.m
@@ -1,4 +1,4 @@
-function [mfile_obj, temp_file_sentinel] = interpolate_3d_data(data, locs, X, Y, Z, in_bdy_mask)
+function [mfile_interp_obj, temp_file_sentinel] = interpolate_3d_data(data, locs, X, Y, Z, in_bdy_mask)
 % This is a wrapper function for scattered interpolant. We can interpolate
 % each frame independtly using parfor and save the interpolated data for 
 % later use with optical flow and then just delete the interpolated data
@@ -16,15 +16,14 @@ function [mfile_obj, temp_file_sentinel] = interpolate_3d_data(data, locs, X, Y,
     extrapolation_method = 'none';
 
 
-    % Frame A
-
     x_dim = 1;
     y_dim = 2;
     z_dim = 3;
 
-    root_fname = 'temp_interp_data';
-    [mfile_obj, temp_file_sentinel] = create_temp_file(root_fname);
-    mfile_obj.data(size(X, x_dim), size(Y, y_dim), size(Z, z_dim), tpts) = 0;
+    root_fname = 'temp_interp';
+    [mfile_interp_obj, temp_file_sentinel] = create_temp_file(root_fname);
+    % Write file to disk
+    mfile_interp_obj.data(size(X, x_dim), size(Y, y_dim), size(Z, z_dim), tpts) = 0;
     
     temp_data = nan(size(X));
 
@@ -32,9 +31,9 @@ function [mfile_obj, temp_file_sentinel] = interpolate_3d_data(data, locs, X, Y,
     %       Also, with a parfor we can't use the matfile object
     for this_tpt=1:tpts
 
-        data_interpolant = scatteredInterpolant(locs(1:end, x_dim), ...
-                                                locs(1:end, y_dim), ...
-                                                locs(1:end, z_dim), ...
+        data_interpolant = scatteredInterpolant(locs(:, x_dim), ...
+                                                locs(:, y_dim), ...
+                                                locs(:, z_dim), ...
                                                 data(this_tpt, :).', ...
                                                 neighbour_method, ...
                                                 extrapolation_method);
@@ -42,7 +41,7 @@ function [mfile_obj, temp_file_sentinel] = interpolate_3d_data(data, locs, X, Y,
         temp_data(in_bdy_mask) = data_interpolant(X(in_bdy_mask).', Y(in_bdy_mask).', Z(in_bdy_mask).');
 
         % Only get 
-        mfile_obj.data(:, :, :, this_tpt) = temp_data;
+        mfile_interp_obj.data(:, :, :, this_tpt) = temp_data;
 
     end
 
