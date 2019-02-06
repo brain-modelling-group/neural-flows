9c599aad82a7c4c13c64321706504f93ad2afeb4
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-02-06 12:24:12 +1000

    Convert to function and generlise

--- dev-scripts/matfileobj_parfor.m
+++ dev-scripts/matfileobj_parfor.m
@@ -1,45 +1,59 @@
-parpool(12);
-%%
-%%step 1: create a mat-file per worker using SPMD
+function spmd_parfor_with_matfiles(number_of_things, parfun, temp_fname_obj, field_expression)
+% This is a gorrible name, but I'm not sure what else to write
+% Assumes a parpool is open
+% number_of_things is the variable we are iterating over
+% temp_fname_obj, the file where we will accumulate/glue final results
+% field of interest -- where to store the data in temp_fname_ibj
+% after
+% the parfor loop
+
 N = 400;
 
-
+% Step 1: create a mat-file per worker using SPMD
 spmd
-    WorkerFname = tempname(pwd); % each worker gets a unique filename and writes to disk
-    myMatfile   = matfile(WorkerFname, 'Writable', true);
+    WorkerFname      = tempname(pwd); % each worker gets a unique filename and writes to disk
+    worker_matfile   = matfile(WorkerFname, 'Writable', true);  
     % Seed the variables in the matfile object
-    myMatfile.data_interp = cell(1, N);
-    myMatfile.gotResult   = false(1, N);
+    worker_matfile.result = cell(1, number_of_things);
+    worker_matfile.got_result = false(1, number_of_things);
 end
-%%step 2: create a parallel.pool.Constant from the 'Composite'
+
+% Step 2: create a parallel.pool.Constant from the 'Composite' 
 % This allows the worker-local-variable to used inside PARFOR
-myMatfileConstant = parallel.pool.Constant(myMatfile);
+matfile_constant = parallel.pool.Constant(worker_matfile);
 
 %%Step 3: run PARFOR
 m = 3;
 n = 2;
 
 parfor idx = 1:N
-    resultToSave = rand(m, n);
-    matfileObj   = myMatfileConstant.Value;
+    % DO THE THING HERE
+    resultToSave = parfun(idx);
+    matfileObj   = matfile_constant.Value;
     % Assign into 'testOut'
-    matfileObj.data_interp(1, idx) = {resultToSave}
-    matfileObj.gotResult(1, idx)   = true;
+    matfileObj.result(1, idx) = {resultToSave}
+    matfileObj.got_result(1, idx) = true;
 end
-%%Step 4: accumulate the results on the client
-% Here we retrieve the filenames from 'myFname' Composite,
-% and use them to accumulate the overall result in memory
-% We're going to concatenate results in the 3rd dimension
+
+% Step 4: accumulate results on a separate file 
+% Here we retrieve the filenames from 'WorkerFname' Composite,
+% and use them to accumulate the overall result in dik
+% We're going to concatenate results in the 4th dimension
 %%
-[temp_fname_obj, temp_file_sentinel] = create_temp_file('dummy_parfor', true);
-temp_fname_obj.data(m, n, N) = 0; 
-for idx = 1:numel(WorkerFname)
-    workerFname = WorkerFname{idx};
-    workerMatfile = matfile(workerFname);
-    for jdx = 1:N
-        if workerMatfile.gotResult(1, jdx)
-            pageCell = workerMatfile.data_interp(1, jdx);
-            temp_fname_obj.data(:, :, jdx) = pageCell{1};
+
+for this_temp_file = 1:numel(WorkerFname)
+    worker_fname = WorkerFname{this_temp_file};
+    % Load the worker temp file
+    worker_matfile = matfile(worker_filename);
+    
+    for jdx = 1:number_of_things
+        if worker_matfile.got_result(1, jdx)
+             result = worker_matfile.result(1, jdx);
+            %  This line should be generic and adaptbale
+            
+            temp_fname_obj.data(:, :, :, jdx) = result{1};
         end
     end
+    delete([worker_fname '.mat'])
 end
+end % function spdm_parfor_with_matfiles()
