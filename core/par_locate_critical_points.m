782a540c1a2eae78d19edd1d3e4f2e48795f8294
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-03-22 14:09:52 +1000

    Parallel-ish locatin of critical points

---
+++ core/par_locate_critical_points.m
@@ -0,0 +1,79 @@
+function [xyz_idx] = par_locate_critical_points(mfile_isosurf_obj, mfile_vel_obj, index_mode)
+% Locates the critical points at the intersection of the three isosrufaces (one per velocity component) at v = critical_isovalue.
+% Returns the location of the singularities as linear indices xyz_lidx, or as 
+% subscripts x_idx, y_idx, z_idx 
+% 
+% ARGUMENTS:
+%      
+%    
+% OUTPUT:
+%       
+% AUTHOR:
+%     Paula Sanz-Leon, QIMR Berghofer 2019
+% USAGE:
+%{
+    
+%}
+
+
+if nargin < 5
+    % Magnitude of the velocity that is considered almost 0
+    index_mode = 'subscript';
+end
+
+
+tpts = size(mfile_isosurf_obj, 'isosurfs', 2);
+X = mfile_vel_obj.X;
+Y = mfile_vel_obj.Y;
+Z = mfile_vel_obj.Z;
+
+xyz_idx = cell(tpts, 1);
+    for tt=1:tpts
+    
+
+    % Get linear indices of each vertex (approximate location of critical points)
+        % TODO: this function returns a very rough approximation
+        % The location may not be exact and we may need to interpolate
+        temp_struct = mfile_isosurf_obj.isosurfs(1, tpts);
+        xyz_lidx_x = coordinate_to_linear_index(temp_struct.vertices_x, X, Y, Z);
+        xyz_lidx_y = coordinate_to_linear_index(temp_struct.vertices_y, X, Y, Z);
+        xyz_lidx_z = coordinate_to_linear_index(temp_struct.vertices_z, X, Y, Z);
+
+        % Do the same for the different isosurfaces. Works better than using the
+        % magnitud. 
+
+        xyz_lidx = intersect(intersect(xyz_lidx_x, xyz_lidx_y), xyz_lidx_z);
+
+        switch index_mode
+            case 'linear'
+                xyz_idx{tt} = xyz_lidx;
+            case 'subscript'
+                [x_idx, y_idx, z_idx] = ind2sub(size(X),xyz_lidx);
+                xyz_idx{tt} = [x_idx, y_idx, z_idx];
+        end
+    end     
+
+end % function locate_critical_points()
+
+function xyz_lidx = coordinate_to_linear_index(points, X, Y, Z)
+ % Quick and dirty solution to find indices in the 3d grid
+ % points is N x Dimension 
+    X = X(:);
+    Y = Y(:);
+    Z = Z(:);
+    
+    % Allocate memory
+    xyz_lidx(size(points, 1), 1) = 0;
+
+    parfor idx=1:size(points, 1)
+        [~, temp_dist] = min(abs( (X-points(idx, 1)).^2 + ...
+                                  (Y-points(idx, 2)).^2 + ...
+                                  (Z-points(idx, 3)).^2 ));
+        xyz_lidx(idx) = temp_dist;
+    end
+    
+    xyz_lidx = unique(xyz_lidx);
+
+end % function coordinate_to_linear_index()
+
+
