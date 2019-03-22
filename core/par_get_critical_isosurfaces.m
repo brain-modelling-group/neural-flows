2a03a39de4b0408889f6fb99735c26d903f8b289
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-03-22 13:32:03 +1000

    Ouptut matfile and sentinel for isosurfaces

--- core/par_get_critical_isosurfaces.m
+++ core/par_get_critical_isosurfaces.m
@@ -1,26 +1,38 @@
-function par_get_critical_isosurfaces(mfile_vel)
-% this function plots a brain as a butterfly
-% only for visualisation purposes
+function [mfile_surf_obj,  mfile_surf_sentinel] = par_get_critical_isosurfaces(mfile_vel)
+% This function calculates the isosurfaces at velocity=0 for each of the
+% three orthogonal velocity components. This function calculate isosurfaces 
+% in parallel -- saves results to disk
 
-% input: a matfile object where the velocity components are saved
-
-
-    % calculate isosurfaces in parallel -- saves results to disk
+% 
+% ARGUMENTS:
+%       mfile_vel: matfile handle to file with the velocity component data 
+%    
+% OUTPUT:
+%       mfile_interp_obj: matfile handle to the file with the interpolated
+%                         data.
+%       mfile_interp_sentinel: OnCleanUp object. If keep_interp_data is
+%                              true, then this variable is an empty array.
+%
+% AUTHOR:
+%     Paula Sanz-Leon, QIMR Berghofer 2019
+% USAGE:
+%{
+    
+%}
 
     X  = mfile_vel.X;
     Y  = mfile_vel.Y;
     Z  = mfile_vel.Z;
 
-    critical_isovalue = 0;
+    critical_isovalue = 0; % this isovalue could be greather than 0
 
     tpts = size(mfile_vel.ux, 4);
 
 
-    % Create file for the interpolated data
+    % Create file for the isosurface data
     root_fname = 'temp_isosurf';
     keep_surf_data = true;
-    [mfile_surf_obj, ~] = create_temp_file(root_fname, keep_surf_data);
-
+    [mfile_surf_obj, mfile_surf_sentinel] = create_temp_file(root_fname, keep_surf_data);
 
     % Open a pallell pool using all available workers
     open_parpool(1)
