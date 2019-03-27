3491fc91ab4986ddc40b26983737807d0f0b46da
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-03-27 16:06:42 +1000

    Enable critical point detection using different data

--- core/par_locate_critical_points.m
+++ core/par_locate_critical_points.m
@@ -1,7 +1,9 @@
-function [xyz_idx] = par_locate_critical_points(mfile_isosurf_obj, mfile_vel_obj, index_mode)
+function [xyz_idx] = par_locate_critical_points(mfile_surf_obj, mfile_vel_obj, data_mode, index_mode)
 % Locates the critical points at the intersection of the three isosrufaces (one per velocity component) at v = critical_isovalue.
 % Returns the location of the singularities as linear indices xyz_lidx, or as 
 % subscripts x_idx, y_idx, z_idx 
+% data_mode: 'surf' or 'vel' -- data to use to detect the locations of
+% critical points
 % 
 % ARGUMENTS:
 %      
@@ -15,32 +17,19 @@ function [xyz_idx] = par_locate_critical_points(mfile_isosurf_obj, mfile_vel_obj
     
 %}
 
-
-if nargin < 3
+if nargin < 4
     % Magnitude of the velocity that is considered almost 0
     index_mode = 'linear';
 end
 
-
-tpts = size(mfile_isosurf_obj, 'isosurfs', 2);
-X = mfile_vel_obj.X;
-Y = mfile_vel_obj.Y;
-Z = mfile_vel_obj.Z;
-
-xyz_idx = struct([]); %(tpts, 1); %TODO: change to struct
-    parfor tt=1:tpts
-    
-    % Get linear indices of each vertex (approximate location of critical points)
-        % TODO: this function returns a very rough approximation
-        % The location may not be exact and we may need to interpolate
-
-        %xyz_idx(tt).xyz_idx = locate_coordinates(mfile_isosurf_obj.isosurfs(1, tt), X, Y, Z, index_mode);
-         xyz_idx(tt).xyz_idx = locate_null_velocity_coordinates(mfile_vel_obj.ux(:, :, :, tt), mfile_vel_obj.uy(:, :, :, tt), mfile_vel_obj.uz(:, :, :, tt), X,index_mode);
-
+switch data_mode
+    case 'surf'
+        xyz_idx = use_isosurfaces(mfile_surf_obj, mfile_vel_obj, index_mode);
+    case 'vel'
+        xyz_idx = use_velocity_fields(mfile_vel_obj, index_mode);   
+end
         
-    end     
-
-end % function locate_critical_points()
+end % function par_locate_critical_points()
 
 function xyz_lidx = vertex_coordinate_to_linear_index(points, X, Y, Z)
  % Quick and dirty solution to find indices in the 3d grid
@@ -61,26 +50,8 @@ function xyz_lidx = vertex_coordinate_to_linear_index(points, X, Y, Z)
     
     xyz_lidx = unique(xyz_lidx);
 
-end % function coordinate_to_linear_index()
+end % vertex_function coordinate_to_linear_index()
 
-% function xyz_lidx = coordinate_to_linear_index(points, X, Y, Z)
-%  % Quick and dirty solution to find indices in the 3d grid
-%  % points is N x Dimension 
-%     X = X(:); % Fix this -- no  need to recast into vectot every time
-%     Y = Y(:);
-%     Z = Z(:);
-%     
-%     % Allocate memory
-%     %xyz_lidx(size(points, 1), 1) = 0;
-%     px  = points(:, 1).';
-%     py  = points(:, 2).';
-%     pz  = points(:, 3).';
-%     
-%     [~, temp_dist_idx] = min(abs((X-px).^2 + (Y-py).^2 + (Z-pz).^2), [], 1);
-%     
-%     xyz_lidx = unique(temp_dist_idx);
-% 
-% end % function coordinate_to_linear_index()
 
 function xyz_idx = locate_null_velocity_coordinates(ux, uy, uz, X, index_mode)
         
@@ -90,19 +61,11 @@ function xyz_idx = locate_null_velocity_coordinates(ux, uy, uz, X, index_mode)
         null_uz = find(uz < eps);
 
         xyz_lidx = intersect(intersect(null_ux, null_uy), null_uz);
-
-        switch index_mode
-            case 'linear'
-                xyz_idx = xyz_lidx;
-            case 'subscript'
-                [x_idx, y_idx, z_idx] = ind2sub(size(X),xyz_lidx);
-                xyz_idx = [x_idx, y_idx, z_idx];
-        end
-
+        xyz_idx = switch_index_mode(xyz_lidx, index_mode, X);
 end
 
 
-function xyz_idx = locate_coordinates(temp_surf_struct, X, Y, Z, index_mode)
+function xyz_idx = locate_null_surf_coordinates(temp_surf_struct, X, Y, Z, index_mode)
 
         xyz_lidx_x = vertex_coordinate_to_linear_index(temp_surf_struct.vertices_x, X, Y, Z);
         xyz_lidx_y = vertex_coordinate_to_linear_index(temp_surf_struct.vertices_y, X, Y, Z);
@@ -110,13 +73,49 @@ function xyz_idx = locate_coordinates(temp_surf_struct, X, Y, Z, index_mode)
 
         xyz_lidx = intersect(intersect(xyz_lidx_x, xyz_lidx_y), xyz_lidx_z);
 
+        xyz_idx = switch_index_mode(xyz_lidx, index_mode, X);
+        
+
+end
+
+
+function xyz_idx = use_velocity_fields(mfile_vel_obj, index_mode)
+
+tpts = size(mfile_vel_obj, 'isosurfs', 4);
+X = mfile_vel_obj.X;
+xyz_idx = struct([]); 
+
+    parfor tt=1:tpts
+         xyz_idx(tt).xyz_idx = locate_null_velocity_coordinates(mfile_vel_obj.ux(:, :, :, tt), ...
+                                                                mfile_vel_obj.uy(:, :, :, tt), ...
+                                                                mfile_vel_obj.uz(:, :, :, tt), X, index_mode);       
+    end 
+
+end
+
+
+function xyz_idx = use_isosurfaces(mfile_surf_obj, mfile_vel_obj, index_mode)
+
+tpts = size(mfile_isosurf_obj, 'isosurfs', 2);
+X = mfile_vel_obj.X;
+Y = mfile_vel_obj.Y;
+Z = mfile_vel_obj.Z;
+xyz_idx = struct([]); 
+
+    parfor tt=1:tpts
+        xyz_idx(tt).xyz_idx = locate_null_surf_coordinates(mfile_surf_obj.isosurfs(1, tt), X, Y, Z, index_mode);        
+    end 
+
+end
+
+
+function xyz_idx = switch_index_mode(xyz_lidx, index_mode, X)
+
         switch index_mode
             case 'linear'
                 xyz_idx = xyz_lidx;
             case 'subscript'
-                [x_idx, y_idx, z_idx] = ind2sub(size(X),xyz_lidx);
+                [x_idx, y_idx, z_idx] = ind2sub(size(X),xyz_lidx); 
                 xyz_idx = [x_idx, y_idx, z_idx];
         end
-
 end
-
