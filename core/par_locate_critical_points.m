8bf91e586c847e1e03555ed31c10d6457477bcf7
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-04-16 11:45:16 +1000

    Dcoument

--- core/par_locate_critical_points.m
+++ core/par_locate_critical_points.m
@@ -122,7 +122,7 @@ function xyz_idx = switch_index_mode(xyz_lidx, index_mode, X)
         switch index_mode
             case 'linear'
                 xyz_idx = xyz_lidx;
-            case 'subscript'
+            case 'subscript' % NOTe: I vaguely remember this part not working properly.
                 [x_idx, y_idx, z_idx] = ind2sub(size(X),xyz_lidx); 
                 xyz_idx = [x_idx, y_idx, z_idx];
         end
