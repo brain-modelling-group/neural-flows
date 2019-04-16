35f561432232028274a5b50416b4cfc2ecdcbe32
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-04-16 16:16:54 +1000

    Add color vector for each singularity type

--- core/map_str2int.m
+++ core/map_str2int.m
@@ -30,38 +30,57 @@ function [label, color] = map_str2int(str_label)
 % complex eigenvalues inside the unit circle  : spiral sink p.o
 
 
+
+
 switch str_label
+    % FIXED POINTS
     case 'source'
         label = 1;
+        color = [215,48,39];
     case 'sink'
         label = 2;
+        color = [69,117,180];
     case '1-2-saddle'
         label = 3;
+        color = [254,224,144];
     case '2-1-saddle'
         label = 4;
-    case 'nan'
-        label = nan;
+        color = [224,243,248];
     case 'spiral-source'
         label = 5;
+        color = [145,191,219];
     case 'spiral-sink'
         label = 6;
+        color = [69,117,180];
+    % ORBITS
+    
+
+
     case 'source-po'
         label = 7;
+        color = [197,27,125];
     case 'sink-po'
         label = 8;
+        color = [77,146,33];
     case 'saddle-po'
         label = 9;
+        color = [253,224,239];
     case 'twisted-po'
         label = 10;
+        color = [230,245,208];
     case 'spiral-source-po'
         label = 11;
+        color = [233,163,201];
     case 'spiral-sink-po'
-        label = 12;      
+        label = 12;
+        color = [161,215,106];
+    case {'nan', 'orbit?'}
+        label = nan;
+        color = [0 0 0]; 
     otherwise
         error(['neuralflows:' mfilename ':BadInput'], ...
            'The input xyz_idx must be a N x 3 array');
-        
-
-
+end
+color = color./255;
 end
 % function map_str2int()
