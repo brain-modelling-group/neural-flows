ec7c3245a77122144e23bdb8296562e27d13b45a
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-04-16 11:20:00 +1000

    Wrap reducpatch function to decimate a sequence of isosurfaces

---
+++ core/decimate_surfaces.m
@@ -0,0 +1,38 @@
+function decimated_surf_obj = decimate_surfaces(mfile_surf, fraction_to_keep)
+% This function is a wrapper of reducepatch. 
+% It takes as input a matfile with the full resolution iossurfaces of zero
+% veolcity and outputs structure (surf_obj) with the decimated version.
+% The output can be then used to track singularities with the surface
+% intersection methods. 
+
+
+% Get a decimated version of a surface
+% Maybe we should compute this when we're calculatinhg the surfaces
+
+if nargin < 2
+    % Agressive decimation based on the running times estimates
+    % of singularity tracking methods
+    fraction_to_keep = 0.1;
+end
+decimated_surf_obj.isosurfs =  struct([]);
+
+tpts = size(mfile_surf, 'isosurfs', 2);
+
+for tt=1:tpts
+   
+   isosurfs =  mfile_surf.isosurfs(1, tt);
+   
+   [F, V] = reducepatch(isosurfs.faces_x, isosurfs.vertices_x, fraction_to_keep);
+   decimated_surf_obj.isosurfs(1, tt).vertices_x = V;
+   decimated_surf_obj.isosurfs(1, tt).faces_x = F;
+   
+   [F, V] = reducepatch(isosurfs.faces_y, isosurfs.vertices_y, fraction_to_keep);
+   decimated_surf_obj.isosurfs(1, tt).vertices_y = V;
+   decimated_surf_obj.isosurfs(1, tt).faces_y = F;
+   
+   [F, V] = reducepatch(isosurfs.faces_z, isosurfs.vertices_z, fraction_to_keep);
+   decimated_surf_obj.isosurfs(1, tt).vertices_z = V;
+   decimated_surf_obj.isosurfs(1, tt).faces_x = F;
+   
+end
+% function decimate_surfaces()
