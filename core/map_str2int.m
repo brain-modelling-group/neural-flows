665dd5edaa7b22f95dd07a07c093e593b2b99876
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-04-17 11:32:24 +1000

    Include alpha value in colour vector

--- core/map_str2int.m
+++ core/map_str2int.m
@@ -29,54 +29,48 @@ function [label, color] = map_str2int(str_label)
 % complex eigenvalues outside the unit circle : spiral source p.o
 % complex eigenvalues inside the unit circle  : spiral sink p.o
 
-
-
-
 switch str_label
     % FIXED POINTS
     case 'source'
         label = 1;
-        color = [215,48,39];
+        color = [215, 48, 39, 1];
     case 'sink'
         label = 2;
-        color = [69,117,180];
+        color = [69, 117, 180, 1];
     case '1-2-saddle'
         label = 3;
-        color = [254,224,144];
+        color = [254, 224, 144, 1];
     case '2-1-saddle'
         label = 4;
-        color = [224,243,248];
+        color = [224, 243, 248, 1];
     case 'spiral-source'
         label = 5;
-        color = [145,191,219];
+        color = [145, 191, 219, 1];
     case 'spiral-sink'
         label = 6;
-        color = [69,117,180];
+        color = [69, 117, 180, 1];
     % ORBITS
-    
-
-
     case 'source-po'
         label = 7;
-        color = [197,27,125];
+        color = [197, 27, 125, 1];
     case 'sink-po'
         label = 8;
-        color = [77,146,33];
+        color = [77, 146, 33, 1];
     case 'saddle-po'
         label = 9;
-        color = [253,224,239];
+        color = [253, 224, 239, 1];
     case 'twisted-po'
         label = 10;
-        color = [230,245,208];
+        color = [230, 245, 208, 1];
     case 'spiral-source-po'
         label = 11;
-        color = [233,163,201];
+        color = [233, 163, 201, 1];
     case 'spiral-sink-po'
         label = 12;
-        color = [161,215,106];
+        color = [161, 215, 106, 1];
     case {'nan', 'orbit?'}
-        label = nan;
-        color = [0 0 0]; 
+        label = 0;
+        color = [0, 0, 0, 0];
     otherwise
         error(['neuralflows:' mfilename ':BadInput'], ...
            'The input xyz_idx must be a N x 3 array');
