a783c1a94c449bdd55675262c4f08777e006b0a9
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-05-15 15:20:46 +1000

    Update

--- core/classify_orbits_3d.m
+++ core/classify_orbits_3d.m
@@ -1,8 +1,17 @@
 function singularity_type = classify_orbits_3d(E)
-% classification of orbits in 3D based on the eigenvalues of the jacobian
+% classification of periodi orbits (p. o) in 3D based on the eigenvalues of the jacobian
 % matrix
 % E -- vector with eigenvalues
-
+%Hyperbolic periodic orbits in 3D can be classified as follows:
+% Two real eigenvalues (one complex):
+%  If real eigenvalues inside the unit circle: source p.o 
+%                      both outside unit circle: sink p.o
+%                      one inside, two outside: 
+%                                             both positive: saddle p.o                    
+%                                             bith negative: twisted p.o
+% One real eigenvalue, two complex conjugate:
+% complex eigenvalues outside the unit circle : spiral source p.o
+% complex eigenvalues inside the unit circle  : spiral sink p.o
 singularity_type = 'orbit?';
 
 end
