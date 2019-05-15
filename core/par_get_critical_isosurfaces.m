da08e427d9607879d0402a9d9f5f494309016858
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-05-15 18:12:08 +1000

    Decimate surfaces

--- core/par_get_critical_isosurfaces.m
+++ core/par_get_critical_isosurfaces.m
@@ -52,16 +52,27 @@ function [mfile_surf_obj,  mfile_surf_sentinel] = par_get_critical_isosurfaces(m
     function temp_data = isosurface_step(idx)
 
 
-    [faces_x, vertices_x] = isosurface(X, Y, Z, mfile_vel.ux(:, :, :, idx), critical_isovalue);
-    [faces_y, vertices_y] = isosurface(X, Y, Z, mfile_vel.uy(:, :, :, idx), critical_isovalue);
-    [faces_z, vertices_z] = isosurface(X, Y, Z, mfile_vel.uz(:, :, :, idx), critical_isovalue);
+        [faces_x, vertices_x] = isosurface(X, Y, Z, mfile_vel.ux(:, :, :, idx), critical_isovalue);
+        [faces_y, vertices_y] = isosurface(X, Y, Z, mfile_vel.uy(:, :, :, idx), critical_isovalue);
+        [faces_z, vertices_z] = isosurface(X, Y, Z, mfile_vel.uz(:, :, :, idx), critical_isovalue);
 
-    temp_data.vertices_x = vertices_x; 
-    temp_data.vertices_y = vertices_y;
-    temp_data.vertices_z = vertices_z;
-    temp_data.faces_x = faces_x;
-    temp_data.faces_y = faces_y;
-    temp_data.faces_z = faces_z;
+        %HACK: should be removed from here or parameter 'fraction ot keep'
+        %should  be available.
+
+        fraction_to_keep = 0.15; 
+        % Ux surface
+        [Fx, Vx] = reducepatch(faces_x, vertices_x, fraction_to_keep);    
+        temp_data.vertices_x = Vx;
+        temp_data.faces_x = Fx;
+        % Uy surface
+        [Fy, Vy] = reducepatch(faces_y, vertices_y, fraction_to_keep);    
+        temp_data.vertices_x = Vy;
+        temp_data.faces_x = Fy;
+
+        % Uz surface
+        [Fz, Vz] = reducepatch(faces_z, vertices_z, fraction_to_keep);    
+        temp_data.vertices_x = Vz;
+        temp_data.faces_x = Fz;
 
     end
 
