f4b29d6332b3d597811ce9c0d12be2e23b6572bd
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-05-16 13:38:54 +1000

    Check that critical points are not at the grid boundary

--- core/classify_singularities.m
+++ core/classify_singularities.m
@@ -39,6 +39,12 @@ if size(xyz_idx(1).xyz_idx, 2) < 2
 
     for tt=1:tpts
         xyz_subs = switch_index_mode(xyz_idx(tt).xyz_idx, 'subscript', mfile_vel_obj.X);
+        
+        % Check if any subscript -- 1. This will cause a problem in the
+        % jacobian calculation
+        if intersect(xyz_subs, 1)
+            continue
+        end
         xyz_idx(tt).xyz_idx = xyz_subs;
     end    
     
@@ -60,6 +66,7 @@ for tt=1:tpts % parallizable stuff but the classification runs very fast
        singularity_labels  = cell(num_critical_points, 1);
 
        for ss=1:num_critical_points
+           ss
            % Calculate the Jacobian at each critical point 
            [J3D] = jacobian3d(xyz_idx(tt).xyz_idx(ss, :), mfile_vel_obj.ux(:, :, :, tt), mfile_vel_obj.uy(:, :, :, tt), mfile_vel_obj.uz(:, :, :, tt), hx, hy, hz);
            singularity_labels{ss} = classify_critical_points_3d(J3D);
