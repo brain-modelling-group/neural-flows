8a1c50c659219f8d0bbb9b0421736f65ae3782f5
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-05-20 16:35:42 +1000

    update script

--- dev-scripts/script_test_track_singularity_lateral.m
+++ dev-scripts/script_test_track_singularity_lateral.m
@@ -1,47 +1,37 @@
-X = mfile_vel.X;
-Y = mfile_vel.Y;
+[singularity_classification] =   classify_singularities(xyz_idx, mfile_vel);
 
-% Get colors for each singularity
 
 num_sing = cellfun(@length, singularity_classification);
 
 % get colors
-for tt=5:200
-    
+for tt=1:200
     for ss=1:num_sing(tt)
+        singularity_classification{tt}{ss}
      [label(tt).label(ss), color_map(tt).colormap(ss, :)] = map_str2int(singularity_classification{tt}{ss});
-    
     end
 end
 
+out = count_singularities(label);
+
+% Get text labels
+singularity_list = get_singularity_list();
+
+% Get colours                
+for ii=1:length(singularity_list)
+  [~, cmap(ii, :)] = map_str2int(singularity_list{ii});
+end
 
 figure_handle = figure;
-ax = subplot(1,1,1);
-hold(ax, 'on')
-this_frame = 5;
-size_marker = 100;
-
-%ax.XLim = [min(X(:)) max(X(:))];
-%ax.YLim = [min(Y(:)) max(Y(:))];
-%ax.ZLim = [min(Z(:)) max(Z(:))];
-
-
-
-for tt=this_frame+1:200
-    
-    for ss=1:num_sing(tt)
-        
-        if strcmp(singularity_classification{tt}{ss}, 'nan')
-            disp('nan')          
-        else
-            p3_handle = scatter(ax, tt.*ones(length(xyz_idx(tt).xyz_idx(ss))), xyz_idx(tt).xyz_idx(ss), 100, color_map(tt).colormap(ss, 1:3), 'filled');
-
-        end    
-            
-        
-    
-    end
-    
- drawnow
- pause(0.1)   
+for ii=1:size(out, 2)
+    ax(ii) = subplot(8,2,ii);
+    hold(ax(ii), 'on')
 end
+
+
+for ii=1:size(out, 2)
+stairs(ax(ii), log(out(:, ii)), 'color', cmap(ii, 1:3), 'linewidth', 2)
+ax(ii).Title.String = singularity_list{ii};
+end
+
+
+
