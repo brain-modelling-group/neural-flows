694611a08e99f6c69d7e0704300b760a437dc719
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-05-28 12:22:17 +1000

    Minor update

--- vis/draw_stream_arrow.m
+++ vis/draw_stream_arrow.m
@@ -57,25 +57,59 @@ dd = min([dx dy]);
 rs    = dd/100;   % Dot size if we don't plot arrows
 
 switch options.start_points_mode
-    case 'grid'
+    case {'grid', 'gridded'}
         XY = stream2(X0,Y0,U,V,X0,Y0);
         Vmag = standardise_range(options.Vmag);
         x0=X0(:); 
         y0=Y0(:);
         
-    case 'random_sparse'
-        start_X = min(X0(:)) + (max(X0(:)) - min(X0(:))) * rand(size(X0));
-        start_Y = min(Y0(:)) + (max(Y0(:)) - min(Y0(:))) * rand(size(Y0));
+    case {'random_sparse', 'random-sparse'}
+        start_X = min(X0(:)) + (max(X0(:)) - min(X0(:))) * rand(floor(size(X0)./2));
+        start_Y = min(Y0(:)) + (max(Y0(:)) - min(Y0(:))) * rand(floor(size(Y0)./2));
         XY = stream2(X0, Y0, U, V, start_X, start_Y);
         Vmag = standardise_range(options.Vmag);
         x0 = start_X(:); 
         y0 = start_Y(:);
         
-    case 'random_dense'
+    case {'random_dense', 'random-dense'}
         % This method may be slow:
-        %Interpolate missing values of V
-        start_X = min(X0(:)) + (max(X0(:)) - min(X0(:))) * rand(size(X0)*2);
-        start_Y = min(Y0(:)) + (max(Y0(:)) - min(Y0(:))) * rand(size(Y0)*2);
+        %Interpolate missing values of V%% Identifies open figures from handles in graphics object array.
+%
+% ARGUMENTS: 
+%    X0 -- 
+%    Y0 -- 
+%    U  --
+%    V  --
+%    options
+%           .stream_length_steps  -- an integer number of 'time' steps to
+%                                    display. It determines the length of
+%                                    the streamlines.
+%           .curved_arrows        -- true or false, 0 or 1. Draws arrows at
+%                                    the head of the streamline
+%
+% OUTPUT:
+%    stream_handle -- streamline objects handle
+%
+% AUTHOR:
+%    Paula Sanz-Leon, (2018), QIMR Berghofer
+%
+% USAGE:
+%{ 
+
+load wind
+options.stream_length_steps=42;
+options.curved_arrow = 1;
+options.start_points_mode = 'grid';
+
+draw_stream_arrow(x(:, :, 1), y(:, :, 1), u(:, :, 1), v(:, :, 1), options)
+
+%}
+%
+% REQUIRES:
+%         standardise_range()
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        start_X = min(X0(:)) + (max(X0(:)) - min(X0(:))) * rand(size(X0));
+        start_Y = min(Y0(:)) + (max(Y0(:)) - min(Y0(:))) * rand(size(Y0));
         XY = stream2(X0,Y0,U,V,start_X, start_Y);
         Vmag = interp2(X0, Y0, options.Vmag, start_X, start_Y, 'cubic');
         Vmag = standardise_range(Vmag);
@@ -101,7 +135,41 @@ if curved_arrow
    head_marker = @plot_arrow;  
 else
    head_marker = @plot_dot; 
-end
+end%% Identifies open figures from handles in graphics object array.
+%
+% ARGUMENTS: 
+%    X0 -- 
+%    Y0 -- 
+%    U  --
+%    V  --
+%    options
+%           .stream_length_steps  -- an integer number of 'time' steps to
+%                                    display. It determines the length of
+%                                    the streamlines.
+%           .curved_arrows        -- true or false, 0 or 1. Draws arrows at
+%                                    the head of the streamline
+%
+% OUTPUT:
+%    stream_handle -- streamline objects handle
+%
+% AUTHOR:
+%    Paula Sanz-Leon, (2018), QIMR Berghofer
+%
+% USAGE:
+%{ 
+
+load wind
+options.stream_length_steps=42;
+options.curved_arrow = 1;
+options.start_points_mode = 'grid';
+
+draw_stream_arrow(x(:, :, 1), y(:, :, 1), u(:, :, 1), v(:, :, 1), options)
+
+%}
+%
+% REQUIRES:
+%         standardise_range()
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 for kk=1:length(long_streams)
     
