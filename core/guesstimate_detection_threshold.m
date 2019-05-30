83b2c6ce931843f7817de5d2071c5c734b622c96
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-05-30 16:02:24 +1000

    Update

--- core/guesstimate_detection_threshold.m
+++ core/guesstimate_detection_threshold.m
@@ -1,10 +1,10 @@
 function [detection_threshold] = guesstimate_detection_threshold(data)
 % Ad hoc way of guesstimating threshold for detecting singularities.
 
-[~, edges] = histcounts(data);
+%[~, edges] = histcounts(data);
 
 % discard first edge
-
-detection_threshold = [edges(2); edges(3)];
+% data assumed to be a sequence of minimum values of the vector field norm 
+detection_threshold = [0; median(data)];
 
 end % function guesstimate_detection_theshold()
