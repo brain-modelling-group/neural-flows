4a0b69c126c6e746642e5c63a5ff2824a9debcd6
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-06-28 17:05:20 +1000

    Bandpass filter utility

---
+++ preprocessing/bandpass_filter.m
@@ -0,0 +1,20 @@
+function [datafiltered] = bandpass_filter(data, flo, fhi, filter_order, fs)
+% Basic functionality to bandapass-filter data, wrapper of butteworth filter 
+%
+%
+% x - data  2d array of size (timepoints, channels/sensing locations/etc)
+% flow - low-frequency cutoff
+% fhi  - high-frequency cutoff
+% filter_order - filter order 
+% fs - sampling frequency
+%
+% OUTPUT
+% Paula Sanz-Leon, QIMR 2019 
+
+% Build Butterworth filter
+cutoff_freqs = [flo fhi];
+wn = cutoff_freqs / (fs/2);
+[b, a] = butter(filter_order, wn); 
+% Filter data
+datafiltered = filtfilt(b, a, data);
+end
