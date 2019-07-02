a013cd8196f227485126b796d227c10f619cf261
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-07-02 13:34:44 +1000

    Enable time lag calculation

--- analysis/spatial_xcorr.m
+++ analysis/spatial_xcorr.m
@@ -2,9 +2,9 @@
 %
 %
 % ARGUMENTS:
-%        data        -- array of size [tpts, num_nodes];
-%        seed_region -- integer with the region that will be taken as the
-%                       refrence.
+%        data        -- array of size [tpts x num_nodes/channels];
+%        seed_region -- integer with the index of region that will be taken 
+%                       as the refrence.
 %
 % OUTPUT: 
 %        
@@ -19,19 +19,34 @@
 %}
 %
 
-function [sp_corr] = spatial_xcorr(data, seed_region, max_lag)
+function [sp_xcorr, varargout] = spatial_xcorr(data, seed_region, max_lag, neighbours)
 
 if nargin < 3 
    max_lag = 0; % autocorrelation
 end
 
-num_nodes = size(data, 2);
-sp_corr(num_nodes, 1) = 0;
-
-for nn=1:num_nodes
-    [temp, ~] = xcorr(data(:, seed_region), data(:, nn), max_lag);
-    idx = floor(max_lag/2)+1;
-    sp_corr(nn) = temp(idx);    
+if nargin < 4
+    neighbours = 1:size(data, 2);
 end
-  
+ 
+num_nodes = length(neighbours);
+sp_acorr(num_nodes, 1) = 0;
+
+if max_lag > 1
+    sp_xcorr(num_nodes, 2*max_lag-1) = 0;
+
+    for nn=1:num_nodes
+        [temp, ~] = xcorr(data(:, seed_region), data(:, neighbours(nn)), max_lag);
+        idx = floor(max_lag/2)+1;
+        sp_acorr(nn) = temp(idx);
+        sp_xcorr(nn, :) = temp;
+    end
+else
+    for nn=1:num_nodes
+        [temp, ~] = xcorr(data(:, seed_region), data(:, neighbours(nn)), max_lag);
+        idx = floor(max_lag/2)+1;
+        sp_acorr(nn) = temp(idx);
+    end 
+    varargout(1) = sp_acorr;
+end  
 end %function spatial_xcorr()
