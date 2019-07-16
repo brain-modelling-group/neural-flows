6291e920491fd2b93fd609ae7042d3546199e3ef
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-07-16 13:31:30 +1000

    Document and clean up

--- preprocessing/bandpass_filter.m
+++ preprocessing/bandpass_filter.m
@@ -1,22 +1,34 @@
+%% Basic functionality to bandapass-filter data, a wrapper of butteworth filters
+%
+% ARGUMENTS:
+%          data    --  a 2D array of size [time x nodes/locs/channels/vertices] with activity.
+%          flo     --  a float with the low-frequency cutoff in Hz.
+%          fhi     --  a float with the high-frequency cutoff in Hz.
+%          filter_order -- an integere with the filter order. 
+%          fs      -- a float with the sampling frequency of data, in Hz.
+%
+% OUTPUT: 
+%          datafiltered  --  a 2D array of size [time x nodes/locs/channels/vertices] 
+%                            with the frequencies in the range [flow fhi].
+%
+% REQUIRES: 
+%
+% USAGE:
+%{     
+
+%}
+%
+% MODIFICATION HISTORY:
+%     Paula Sanz-Leon, QIMR Berghofer 2019 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function [datafiltered] = bandpass_filter(data, flo, fhi, filter_order, fs)
-% Basic functionality to bandapass-filter data, wrapper of butteworth filter 
-%
-%
-% x - data  2d array of size (timepoints, channels/sensing locations/etc)
-% flow - low-frequency cutoff
-% fhi  - high-frequency cutoff
-% filter_order - filter order 
-% fs - sampling frequency
-%
-% OUTPUT
-% Paula Sanz-Leon, QIMR 2019 
 
 % Build Butterworth filter
 cutoff_freqs = [flo fhi];
 % Normalised frequency 
 wn = cutoff_freqs / (fs/2);
 [b, a] = butter(filter_order, wn); 
-
 % Filter data
 datafiltered = filtfilt(b, a, data);
 end % function bandpass_filter()
