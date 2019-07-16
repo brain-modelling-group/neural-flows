54c6441a91e851cda0d40a7496dcef7104169741
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-07-16 14:48:15 +1000

    Generalise

--- analysis/compute_cohcorrgram_dwelltimes.m
+++ analysis/compute_cohcorrgram_dwelltimes.m
@@ -1,27 +1,25 @@
-% Estimate metastable states dwelltimes, based on the coherence-cross-correlogram 
-%
-%
+function [dwell_times, jump_times] = compute_cohcorrgram_dwelltimes(CC, opts)
+% Estimate metastable states dwelltimes, based on cohcorrgram.m
 % ARGUMENTS:
-%        CC   -- a struct obtain with cross<description>
+%        CC   -- a struct obtained with crosscorrgram, with fields:
+%                .cc -- correlation coefficient.
+%                .ct -- correlation time vector.
+%
 %        opts -- a struct woth fields:
-%            .threshtype -- a string with the type of threshold to apply.
+%                .threshtype -- a string with the type of threshold to apply.
 %
 % OUTPUT: 
-%        <output1> -- <description>
-%        <output2> -- <description>
+%        dwell_times -- a vector with dwell times. 
+%        jump_times  -- a vector with the jump/transition times.
 %
 % REQUIRES: 
-%        extract_bursts()
+%        extractbursts()
 %
 % USAGE:
 %{
-    <example-commands-to-make-this-function-run>
+
 %}
 %
-
-
-function [dwell_times, jump_times] = compute_cohcorrgram_dwelltimes(CC, opts)
-% dwellts = cohcorrgram_dwelltime(CC,opts)
 
 if nargin < 2
     opts.threshtype='mean';
@@ -33,7 +31,12 @@ switch opts.metric
     case 'inverse_var'
         metric = 1./var(CC.cc, 0, 1);
     otherwise
-        error(['neural-flows:: ', mfilename, '::Unknown metric option.'])
+        if  ischar(opts.metric) % check that metric is not string
+            error(['neural-flows:: ', mfilename, '::Unknown metric option.'])
+        else
+            % pass the metric to be used as a vector
+            metric = opts.metric;
+        end
 end 
     
 % which threshold
@@ -50,13 +53,14 @@ end
 
 % Detect upward zero-crossings
 upcrossings = find(diff(metric-transitionthr>=0)==1);
-avs = extractbursts(metric,transitionthr); % pull out the suprathreshold segments
+% Pull out the suprathreshold segments
+avs = extractbursts(metric, transitionthr); 
 % Use the peak as the representative transition time
-jumpsi = cellfun(@(x) find(x==max(x)),avs)+upcrossings(1:length(avs))-1; 
+jumpsi = cellfun(@(x) find(x==max(x)),avs) + upcrossings(1:length(avs))-1; 
 % Estimate dwell times
 dwell_times = diff(jumpsi)*(CC.ct(2)-CC.ct(1)); % dwell times
 
 if nargout > 1
     jump_times = CC.ct(jumpsi);
 end
-end % function compute_cohcorrgram_
+end % function compute_cohcorrgram_dwelltimes()
