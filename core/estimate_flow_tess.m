0176c4c8c353a1dcaaeb7722bce7ae27e05c31ab
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-07-17 15:31:05 +1000

    Remove internal function

--- core/estimate_flow_tess.m
+++ core/estimate_flow_tess.m
@@ -148,87 +148,8 @@ for tt_idx  = 2:interval_length
   end
 
 end 
-
   
 end % function estimate_flow_surf()
-
-% Auxiliary functions
-function [gradient_basis, triangle_areas, face_normals] = get_basic_geometry_of_tesselation(faces, vertices, embedding_dimension)
-% Computes some geometric quantities from a triangulated domain/tesselation.
-% 
-% INPUTS:
-%   faces                     - triangles of tesselation
-%   vertices                  - coordinates of nodes
-%   embedding_dimension       - 3 for cortical surface (default)
-%                               2 for plane (channel cap, etc)
-% OUTPUTS:
-%   gradient_basis   - gradient of basis function (FEM) on each triangle
-%   triangle_areas 	 - area of each triangle
-%   face_normals     - normal of each triangle 
-
-
-% Idiomatic indexing
-vertex_0 = 1;
-vertex_1 = 2;
-vertex_2 = 3;
-
-% Edges of each triangles
-u = vertices(faces(:, vertex_1),:) - vertices(faces(:, vertex_0),:);
-v = vertices(faces(:, vertex_2),:) - vertices(faces(:, vertex_1),:);
-w = vertices(faces(:, vertex_0),:) - vertices(faces(:, vertex_2),:);
-
-% Edge lengths
-uu = sum(u.^2, 2);
-vv = sum(v.^2, 2);
-ww = sum(w.^2, 2);
-% Angles
-uv = sum(u.*v, 2);
-vw = sum(v.*w, 2);
-wu = sum(w.*u, 2);
-
-% 3 heights of each triangle and their norm
-h1 = w-((vw./vv)*ones(1, embedding_dimension)).*v;
-h2 = u-((wu./ww)*ones(1, embedding_dimension)).*w;
-h3 = v-((uv./uu)*ones(1, embedding_dimension)).*u;
-hh1 = sum(h1.^2, 2);
-hh2 = sum(h2.^2, 2);
-hh3 = sum(h3.^2, 2);
-
-% Gradient of the 3 basis functions on a triangle 
-gradient_basis = cell(1, embedding_dimension);
-gradient_basis{1} = h1./(hh1*ones(1, embedding_dimension));
-gradient_basis{2} = h2./(hh2*ones(1, embedding_dimension));
-gradient_basis{3} = h3./(hh3*ones(1, embedding_dimension));
-
-% Remove pathological gradients
-indices1 = find(sum(gradient_basis{1}.^2,2)==0|isnan(sum(gradient_basis{1}.^2,2)));
-indices2 = find(sum(gradient_basis{2}.^2,2)==0|isnan(sum(gradient_basis{2}.^2,2)));
-indices3 = find(sum(gradient_basis{3}.^2,2)==0|isnan(sum(gradient_basis{3}.^2,2)));
-
-min_norm_grad = min([ sum(gradient_basis{1}(sum(gradient_basis{1}.^2,2) > 0,:).^2,2); ...
-                      sum(gradient_basis{2}(sum(gradient_basis{2}.^2,2) > 0,:).^2,2); ...
-                      sum(gradient_basis{3}(sum(gradient_basis{3}.^2,2) > 0,:).^2,2) ...
-                    ]);
-% Replace pathological gradients with the min_norm
-gradient_basis{1}(indices1,:) = repmat([1 1 1]/min_norm_grad, length(indices1), 1);
-gradient_basis{2}(indices2,:) = repmat([1 1 1]/min_norm_grad, length(indices2), 1);
-gradient_basis{3}(indices3,:) = repmat([1 1 1]/min_norm_grad, length(indices3), 1);
-
-% Area of each triangle/face
-triangle_areas = sqrt(hh1.*vv)/2;
-% NOTE: There should not be traingles with nan or zero area.
-% Checks on the integrity of meshes should be done before.
-triangle_areas(isnan(triangle_areas)) = 0;
-
-% Calculate normals to surface at each face
-if embedding_dimension == 3
-    face_normals = cross(w, u);
-    face_normals = face_normals./repmat(sqrt(sum(face_normals.^2,2)), 1, 3);
-else
-    face_normals = [];
-end
-
-end
 
 
 %% ===================== Calculate the tesselation's tangent bundle ============
