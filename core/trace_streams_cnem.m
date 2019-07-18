3dd3a2e480a96833cbaa9cb7ca96cbb5b53a0a1e
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-07-18 15:32:13 +1000

    Update, document and test

--- core/trace_streams_cnem.m
+++ core/trace_streams_cnem.m
@@ -1,4 +1,4 @@
-function streams = trace_streams_cnem(locs, boundary_faces, flow_field, seed_locs, time_step, max_stream_length)
+function stream_cell = trace_streams_cnem(locs, boundary_faces, flow_field, seed_locs, time_step, max_stream_length)
 %% Traces streamlines using a velocity field defined on scattered points in space
 %  based on traceStreamXYZUVW from matlab's stream3c.c, using CNEM
 %  functions.
@@ -24,12 +24,10 @@ function streams = trace_streams_cnem(locs, boundary_faces, flow_field, seed_loc
 %                                  
 %
 % OUTPUT: 
-%          streams -- velocity struct, with fields:
-%            -- vnormp - magnitude of velocity (speed) [time x nodes]
-%            -- vxp - x component of the vector field of size [time x nodes]
-%            -- vyp - y component of the vector field of size [time x nodes]
-%            -- vzp - z component of the vector field of size [time x nodes]
-%
+%          stream_cell -- a cell of length num_streams/length seed_locs
+%                         where each element is a 2D array of size
+%                         [stream_length_i x 3], and stream_length_i is at
+%                         most max_streamline_length.
 % REQUIRES: 
 %          m_cnem3d_interpol()
 %          
@@ -37,17 +35,24 @@ function streams = trace_streams_cnem(locs, boundary_faces, flow_field, seed_loc
 %{     
     load wind
     rng(42);
-    loc_idx = randperm(length(x(:)), round(length(x(:))*0.01));
+    loc_idx = randperm(length(x(:)), round(length(x(:))*0.05));
     locs = [x(loc_idx); y(loc_idx); z(loc_idx)].';
     flow_field = [u(loc_idx); v(loc_idx); w(loc_idx)].';
-    dt = 2^-4;
+    dt = 0.5; % this value needs to be carefully selected
     max_stream_length = 256;
      
     % Get boundary/convex hull
     [~, bdy] = get_boundary_info(locs, locs(:, 1), locs(:, 2), locs(:, 3));
 
  
-    streams = trace_streams_cnem(locs, bdy, flow_field, locs, dt, max_stream_length)
+    streams = trace_streams_cnem(locs, bdy, flow_field, locs, dt, max_stream_length);
+    % Visualize what we just did
+    for ii=1:length(streams);
+        plot3(squeeze(scell{ii}(:, 1)), ...
+              squeeze(scell{ii}(:, 2)), ...
+              squeeze(scell{ii}(:, 3)), 'color', [0.5 0.5 0.5 0.1]);
+    end
+
 
 
 %}
@@ -84,14 +89,15 @@ for it = 1:max_stream_length
         break
     end
     
-    % Make interpolation object
+    % Make interpolation object at every iteration. We need to get values
+    % of the vector field at the tip of the streamline.
     FInterpol = m_cnem3d_interpol(locs, boundary_faces, seed_locs(live,:), Type_FF);
     
     % terminate any streamlines that have left the interior
     livej = live(live); % live streamlines this iteration (has same length as number of live points)
     livej = livej & FInterpol.In_Out;
     
-    streams(live,:,it) = seed_locs(live,:);
+    streams(live,:, it) = seed_locs(live,:);
     
     %if already been here, done
     % % how likely is this though!?
@@ -100,7 +106,7 @@ for it = 1:max_stream_length
     flowfield_interp = FInterpol.interpolate(flow_field); % has same length as number of live points
     
     % check if step length has hit zero
-    validsteps = ~all(~flowfield_interp,2);
+    validsteps = ~all(~flowfield_interp, 2);
     %if any(~validsteps), fprintf('%d steps hit zero\n',sum(~validsteps)), end
     livej = livej&validsteps;
     
@@ -112,32 +118,26 @@ for it = 1:max_stream_length
     live(live) = livej;
     
     % update the current position
-    seed_locs(live,:) = seed_locs(live,:) + flowfield_step(livej,:);
+    seed_locs(live, :) = seed_locs(live,:) + flowfield_step(livej,:);
     
     
 end
 
+disp([mfilename ':: Pruning streamline array ...'])
+
+% Transform the streams array into a cell.
+conn = zeros(3, 3, 3);
+conn(2, 2, :) =1; 
+conncomp = bwconncomp(streams,conn);
+ids = conncomp.PixelIdxList; ids=reshape(ids,[],3);
+%slens=cellfun(@length,ids); %513x3
+%any(any(diff(slens,[],2),2)) % false if all x,y,z components are same length
+%
+ns = size(streams, 1);
+stream_cell = cell(1,ns);
+for j=1:ns
+    stream_cell{j} = streams([ids{j,:}]);
+end
 disp([mfilename ':: Done.'])
 
-if 0
-%%
-conn=zeros(3,3,3);
-conn(2,2,:)=1;
-conncomp=bwconncomp(streams,conn);
-ids=conncomp.PixelIdxList; ids=reshape(ids,[],3);
-slens=cellfun(@length,ids); %513x3
-%any(any(diff(slens,[],2),2)) % false if all x,y,z components are same length
-slens=max(slens,[],2);
-%%
-ns=size(streams,1);
-scell=cell(1,ns);
-for j=1:ns
-    scell{j}=streams([ids{j,:}]);
-end
-
-
-
-
-
-
-end
+end %function trace_streams_cnem()
