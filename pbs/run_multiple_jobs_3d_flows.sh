db909f88ca2fd6068015857340545db2c10e4750
Paula Sanz-Leon <paula.sanz-leon@sydney.edu.au>
2019-07-25 08:57:44 +1000

    Update script for multiple jobs

--- pbs/run_multiple_jobs_3d_flows.sh
+++ pbs/run_multiple_jobs_3d_flows.sh
@@ -6,6 +6,6 @@ CHUNKS=$(seq 1 195) # This variable will be used to index the chunk
         export IDX_CHUNK
         NAME=job_${IDX_CHUNK}
         echo "Submitting chunk: ${NAME}"
-        qsub -v IDX_CHUNKS run_multiple_jobs_3d_flows.pbs
+        qsub -v IDX_CHUNK run_multiple_jobs_3d_flows.pbs
         echo "done."
     done
