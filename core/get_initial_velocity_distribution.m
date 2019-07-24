1d126c6ba528f6da7635ed252edfea7e5599a7e9
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-07-24 14:10:22 +1000

    Remove unecessary calls to size, avoid duplicating large array by replacing input arguments

--- core/get_initial_velocity_distribution.m
+++ core/get_initial_velocity_distribution.m
@@ -1,4 +1,5 @@
-function [uxo, uyo, uzo] = get_initial_velocity_distribution(X, NAN_MASK, seed)
+function [uxo, uyo, uzo] = get_initial_velocity_distribution(grid_shape, NAN_MASK, seed)
+
 % X: domain shape
 % NAN_MASK: logical mask of the same size as FA
 if nargin < 3
@@ -8,9 +9,9 @@ end
   rng(seed) 
   a   = -0.125; % TODO: make it optional parameter based on data stats
   b   =  0.125; % TODO: make it optional parameter based on data stats
-  uxo = a + (b-a).*rand(size(X));
-  uyo = a + (b-a).*rand(size(X));
-  uzo = a + (b-a).*rand(size(X));
+  uxo = a + (b-a).*rand(grid_shape);
+  uyo = a + (b-a).*rand(grid_shape);
+  uzo = a + (b-a).*rand(grid_shape);
   uxo(NAN_MASK) = NaN;
   uyo(NAN_MASK) = NaN;
   uzo(NAN_MASK) = NaN;
