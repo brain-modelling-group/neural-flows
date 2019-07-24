5a134d76215452d93e34a949a5523abcf5905318
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-07-24 18:03:28 +1000

    Write script to process data on the cluster

---
+++ dev-scripts/cluster_artemis_calculate_3d_flows.m
@@ -0,0 +1,28 @@
+function cluster_artemis_calculate_3d_flows()
+% Script to process data on Artemis
+
+load('long_cd_ictime50_seg7999_outdt1_d1ms_W_coupling0.6_trial1.mat', 'soln')
+load('513COG.mat', 'COG')
+
+idx_start = 1;
+idx_stop  = 10;
+data  = soln(:, idx_start:idx_stop)';
+locs = COG;
+
+clear COG soln
+
+% Cluster properties
+local_cluster = parcluster('local');
+local_cluster.NumWorkers = 32; % This should match the requested number of cpus
+
+% Change directory to scratch, so temp files will be created there
+cd /scratch/GCMD
+
+% Options for the flow computation
+options.interp_data.file_exists = false;
+options.sing_detection.datamode = 'vel';
+options.sing_detection.indexmode = 'linear';
+
+tic;compute_neural_flows_3d_ug(data, locs, options);toc 
+
+end % 
