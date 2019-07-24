10b9b1a1cbecaaf7b750791603782cd7921335d0
Paula Sanz-Leon <paula.sanz-leon@sydney.edu.au>
2019-07-24 22:23:50 +1000

    Write small dummy bash script to launch multiple jobs

---
+++ pbs/run_multiple_jobs_3d_flows.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CHUNKS=$(seq 1 195) # This variable will be used to index the chunk
+    for IDX_CHUNK in ${CHUNKS}
+    do
+        export IDX_CHUNK
+        NAME=job_${IDX_CHUNK}
+        echo "Submitting chunk: ${NAME}"
+        qsub -v IDX_CHUNKS run_multiple_jobs_3d_flows.pbs
+        echo "done."
+    done
