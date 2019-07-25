9dbb1d51c59fb9d2c365edf5430474b3b4a68c37
Paula Sanz-Leon <paula.sanz-leon@sydney.edu.au>
2019-07-25 17:54:22 +1000

    Add new script that use another queue

---
+++ pbs/run_multiple_jobs_3d_flows_defaultQ.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Author: Paula Sanz-Leon, QIMR / Sydney Uni
+# Call pbs script using defaultQ
+CHUNKS=$(seq 19 196) # This variable will be used to index the chunk
+for IDX_CHUNK in ${CHUNKS}
+do
+    export IDX_CHUNK
+    NAME=job_${IDX_CHUNK}
+    echo "Submitting chunk: ${NAME}"
+    qsub -v IDX_CHUNK run_multiple_jobs_3d_flows_defaultQ.pbs
+    echo "done."
+done
