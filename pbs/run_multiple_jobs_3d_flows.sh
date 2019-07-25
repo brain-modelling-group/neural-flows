50175187353a4b1d75de0c0812fd9db2d30b0b26
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-07-25 10:12:20 +1000

    Fix indentation

--- pbs/run_multiple_jobs_3d_flows.sh
+++ pbs/run_multiple_jobs_3d_flows.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# Author: Paula Sanz-Leon, QIMR / Sydney Uni
 
 CHUNKS=$(seq 1 196) # This variable will be used to index the chunk
-    for IDX_CHUNK in ${CHUNKS}
-    do
-        export IDX_CHUNK
-        NAME=job_${IDX_CHUNK}
-        echo "Submitting chunk: ${NAME}"
-        qsub -v IDX_CHUNK run_multiple_jobs_3d_flows.pbs
-        echo "done."
-    done
+for IDX_CHUNK in ${CHUNKS}
+do
+    export IDX_CHUNK
+    NAME=job_${IDX_CHUNK}
+    echo "Submitting chunk: ${NAME}"
+    qsub -v IDX_CHUNK run_multiple_jobs_3d_flows.pbs
+    echo "done."
+done
