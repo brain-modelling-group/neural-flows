9a3bbb3ca08430015c3d17924d450a079d5d75ae
Paula Sanz-Leon <paula.sanz-leon@sydney.edu.au>
2019-07-26 13:23:10 +1000

    Update

--- pbs/run_multiple_jobs_3d_flows.sh
+++ pbs/run_multiple_jobs_3d_flows.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Author: Paula Sanz-Leon, QIMR / Sydney Uni
 
-CHUNKS=$(seq 1 196) # This variable will be used to index the chunk
+CHUNKS=$(seq 2 4) # This variable will be used to index the chunk
 for IDX_CHUNK in ${CHUNKS}
 do
     export IDX_CHUNK
