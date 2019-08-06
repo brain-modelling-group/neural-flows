9887ec9a28543e7b608b7dc9b8173d8a04c37656
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-08-06 15:24:07 +1000

    Change output var name

--- core/map_str2int.m
+++ core/map_str2int.m
@@ -1,4 +1,4 @@
-function [label, color] = map_str2int(str_label)
+function [numeric_label, color] = map_str2int(str_label)
 % This function maps singularity human-readable labels into integer numbers
 % for quantitative classification.
 
@@ -35,53 +35,53 @@ end
 switch str_label
     % FIXED POINTS
     case 'source' % done
-        label = 1;
+        numeric_label = 1;
         color = [165, 0, 38, 1];
     case 'sink'   % done
-        label = 2;
+        numeric_label = 2;
         color = [49, 54, 149, 1];
     case '2-1-saddle' % done - more source (red) than sink
-        label = 3;
+        numeric_label = 3;
         color = [244, 109, 67, 1];
     case '1-2-saddle'   % done - more sink (blue) than source
-        label = 4;
+        numeric_label = 4;
         color = [69, 117, 180, 1];
     case 'spiral-source' % done
-        label = 5;  
+        numeric_label = 5;  
         color = [253, 174, 97, 1];
     case 'spiral-sink' % done
-        label = 6;
+        numeric_label = 6;
         color = [116, 173, 209, 1];
     case '2-1-spiral-saddle' % done - more source (red) than sink
-        label = 7;
+        numeric_label = 7;
         color = [254 224,144, 1];
     case '1-2-spiral-saddle' % done - more sink (blue) than source
-        label = 8;
+        numeric_label = 8;
         color = [171, 217, 233, 1];        
     % ORBITS
     case 'source-po'% done
-        label = 9;
+        numeric_label = 9;
         color = [197, 27, 125, 1];
     case 'sink-po' % done
-        label = 10;
+        numeric_label = 10;
         color = [77, 146, 33, 1];
     case 'saddle-po'
-        label = 11;
+        numeric_label = 11;
         color = [253, 224, 239, 1];
     case 'twisted-po'
-        label = 12;
+        numeric_label = 12;
         color = [230, 245, 208, 1];
     case 'spiral-source-po' % done
-        label = 13;
+        numeric_label = 13;
         color = [233, 163, 201, 1];
     case 'spiral-sink-po'% done
-        label = 14;
+        numeric_label = 14;
         color = [161, 215, 106, 1];
     case {'1-1-0-saddle'} 
-        label = 15;
+        numeric_label = 15;
         color = [0 255 0 0];   % These ones may be artificial  
     case {'nan', 'orbit?', 'boundary', 'zero', 'empty'}
-        label = 16;
+        numeric_label = 16;
         color = [0, 0, 0, 0];
     otherwise
         error(['neuralflows:' mfilename ':BadInput'], ...
