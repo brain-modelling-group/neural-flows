2fa59a7cc4ad85b0fee363d9df946214222c32ad
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-08-06 15:44:22 +1000

    Update function calls after fun rename

--- analysis/count_singularities.m
+++ analysis/count_singularities.m
@@ -1,4 +1,4 @@
-function out = count_singularities(numeric_labels)
+function out = count_singularities(sing_numeric_labels)
 % <ShortDescription>
 %
 %
@@ -25,15 +25,16 @@ singularity_list = get_singularity_list();
 
 types_of_singularity(length(singularity_list)) = 0;
 
+% Get singularity numeric label assigned to each singularity string label 
 for ss=1:length(singularity_list)
-    types_of_singularity(ss) = map_str2int(singularity_list{ss});
+    types_of_singularity(ss) = get_singularity_numlabel(singularity_list{ss});
 end
 
 out = zeros(length(numeric_labels), length(types_of_singularity));
 
-for ll=1:length(numeric_labels)
-    [counts,~] = hist(numeric_labels(ll).label, types_of_singularity);
-    out(ll, :) = counts;
+for tt=1:length(sing_numeric_labels)
+    [counts,~] = hist(sing_numeric_labels(tt).label, types_of_singularity);
+    out(tt, :) = counts;
 end
 
 end % function count_singularities()
