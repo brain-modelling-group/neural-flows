d333b3a41194cf5d3291f1d05c7821b66585a59d
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-09-12 17:35:06 +1000

    Update test script

--- test-data/test_data.m
+++ test-data/test_data.m
@@ -1,24 +1,24 @@
-[wave3d, X, Y, Z, time] = generate_planewave_3d_structured('x');
-%[wave3d, X, Y, Z, time] = generate_movingbar_3d_structured();
- 
+%[wave3d, X, Y, Z, time] = generate_planewave_3d_structured('x');
+[wave3d, X, Y, Z, time] = generate_movingbar_3d_structured();
+ %%
  options.chunk = 42;  
  options.hx = 1;
  options.hy = 1;
  options.hz = 1;
  options.ht = 1;
  options.flow_calculation.init_conditions = 'random';
- options.flow_calculation.alpha_smooth   = 0.05;
+ options.flow_calculation.alpha_smooth   = 0.1;
  flows3d_compute_structured_grid(wave3d, X, Y, Z, time, options);
  %%
  clear ux uy uz
  %% 
  figure
- histogram(ux)
+ histogram(ux(2:19, 2:19, 2:19, :))
  hold
- histogram(uy)
- histogram(uz)
+ histogram(uy(2:19, 2:19, 2:19, :))
+ histogram(uz(2:19, 2:19, 2:19, :))
  %%
- for tt=1:22; pcolor3(squeeze(wave3d(tt, :, :, :))); caxis([0 2]); pause(0.5); clf;end
+ for tt=1:22; pcolor3(squeeze(wave3d(tt, :, :, :))); caxis([-40 10]); pause(0.5); clf;end
  
  %%
- for tt=1:21; clf; pcolor3(ux(:, :, :, tt));caxis([0 3]);colorbar;pause(0.5);end
+ for tt=1:21; clf; pcolor3(ux(:, :, :, tt)); caxis([0 3.5]);colorbar;pause(0.5);end
