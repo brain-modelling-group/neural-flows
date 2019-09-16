004f22b038a69c4406a519b1575e3555624f29b4
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-09-16 14:19:51 +1000

    Plot animation on the same axes

--- vis/make_movie_gif.m
+++ vis/make_movie_gif.m
@@ -5,7 +5,6 @@ function make_movie_gif(data, save_anim)
 
 % =====================================================================
 if nargin < 2
-    
    save_anim = 0;
 end
 %  Enter file name
@@ -20,7 +19,8 @@ nt = 1;
 tpts = size(data, 3);
 
 
-figure('Name', 'nflows-gifanim')
+fig_gif = figure('Name', 'nflows-gifanim');
+these_axes = subplot(1,1,1, 'Parent', fig_gif);
 pos = [0.6 0.05 0.35 0.40];
 set(gcf,'Units','normalized');
 set(gcf,'Position',pos);
@@ -28,7 +28,7 @@ set(gcf,'color','w');
 
 for cc =1:tpts
   xy = squeeze(data(cc, :, :));
-  pcolor(xy)
+  pcolor(these_axes, xy)
   shading interp
   axis off
   % NOTE: these parameters should be passed as options
