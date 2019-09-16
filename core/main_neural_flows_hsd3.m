298905f9d824d27b675250180e88f9289e4728a0
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-09-16 18:08:57 +1000

    Rename and update function calls

--- core/main_neural_flows_hsd3.m
+++ core/main_neural_flows_hsd3.m
@@ -1,4 +1,4 @@
-function varargout = compute_neural_flows_3d_ug(data, locs, options) 
+function varargout = main_neural_flows_hsd3(data, locs, options) 
     % Compute neural flows from (u)nstructured (g)rids/scattered datapoints
     % This function: 
     %              1) interpolates the data onto a regular grid.
