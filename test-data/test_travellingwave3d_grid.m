733beadc8e40618b8d3abf644fc433842449edcb
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-09-17 13:16:55 +1000

    Restructure function

--- test-data/test_travellingwave3d_grid.m
+++ test-data/test_travellingwave3d_grid.m
@@ -1,28 +1,34 @@
 function test_travellingwave3d_grid()
 
+% Generate data
 [wave3d, X, Y, Z, time] = generate_travellingwave_3d_structured();
- %%
- options.chunk = 42;  
+
  options.hx = 1;
  options.hy = 1;
  options.hz = 1;
  options.ht = 1;
  options.flow_calculation.init_conditions = 'random';
+ options.flow_calculation.seed_init_vel = 42;
  options.flow_calculation.alpha_smooth   = 0.1;
- flows3d_compute_structured_grid(wave3d, X, Y, Z, time, options);
+ options.flow_calculation.max_iterations = 64;
+
+ [mfile_flows, ~, ~] = flows3d_compute_structured_grid(wave3d, X, Y, Z, time, options);
+
+ fig_hist = figure('Name', 'nflows-test-histograms');
+
+ subplot(1, 3, 1, 'Parent', fig_hist)
+ histogram(mfile_flows.ux(2:19, 2:19, 2:19, :))
+ 
+ subplot(1, 3 ,2, 'Parent', fig_hist)
+ histogram(mfile_flows.uy(2:19, 2:19, 2:19, :))
+ 
+ subplot(1, 3, 3, 'Parent', fig_hist)
+ histogram(mfile_flows.uz(2:19, 2:19, 2:19, :))
  %%
- clear ux uy uz
- %% 
- figure
- histogram(ux(2:19, 2:19, 2:19, :))
- hold
- histogram(uy(2:19, 2:19, 2:19, :))
- histogram(uz(2:19, 2:19, 2:19, :))
- %%
- for tt=1:22; pcolor3(squeeze(wave3d(tt, :, :, :))); caxis([-40 10]); pause(0.5); clf;end
+ %for tt=1:22; pcolor3(squeeze(wave3d(tt, :, :, :))); caxis([-40 10]); pause(0.5); clf;end
  
  %%
- for tt=1:21; clf; pcolor3(ux(:, :, :, tt)); caxis([0 3.5]);colorbar;pause(0.5);end
+ %for tt=1:21; clf; pcolor3(ux(:, :, :, tt)); caxis([0 3.5]);colorbar;pause(0.5);end
  
  
-end
+end % function test_travellingwave3d_grid()
