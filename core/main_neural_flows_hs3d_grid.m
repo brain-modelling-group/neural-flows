d9779abb6a5f4264c2438551eb4bfb23e6983ee6
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-09-17 13:03:42 +1000

    Clean up function

--- core/main_neural_flows_hs3d_grid.m
+++ core/main_neural_flows_hs3d_grid.m
@@ -1,8 +1,7 @@
-function varargout = main_neural_flows_hs3d_grid(data, X, Y, Z, time_vec, options) 
- % New function name flows3d_compute_neuralflows_structured_grid() 
+function varargout = main_neural_flows_hs3d_grid(data, X, Y, Z, options) 
  % This function is mainly for testing purposes of flow calculations
- % Compute neural flows from data defined on a structured grid.
- %  [wave3d, X, Y, Z, time] = generate_planewave_3d_structured('x');
+ % Compute neural flows from synthetic data defined on a structured grid.
+ % [wave3d, X, Y, Z, time] = generate_travellingwave_3d_structured('x');
  % options.chunk = 42;  
  % options.hx = 1;
  % options.hy = 1;
@@ -11,8 +10,7 @@ function varargout = main_neural_flows_hs3d_grid(data, X, Y, Z, time_vec, option
  % options.flow_calculation.init_conditions = 'random';
  % flows3d_compute_structured_grid(wave3d, X, Y, Z, time, options);
  % 
- % This function: 
- %              2) calculates velocity fields
+ % This function only calculates velocity/flow fields
  %              
  % data: 
  %          4D array of size [timepoints x xcoord x ycoord x zcoord]
@@ -25,34 +23,27 @@ function varargout = main_neural_flows_hs3d_grid(data, X, Y, Z, time_vec, option
     
     keep_vel_file    = true;
 
-    % Labels for 2D input arrays
-    %n_dim = 2; % time
     t_dim = 1; % time
-    
     dtpts  = size(data, t_dim);
-    ht    = time_vec(2) - time_vec(1); % NOTE: should be input
       
-    % Human-readable labels for indexing dimensions of 4D arrays
-    x_dim = 1;
-    y_dim = 2;
-    z_dim = 3;
-    
+ 
     grid_size = size(X);
     hx = options.hx; 
     hy = options.hy;
     hz = options.hz;   
+    ht = options.ht;
         
     % NOTE: this is a kind of hack. For a structured grids we should not
-    % need this.
+    % need this. or maybe we do?
     in_bdy_mask = ones(grid_size);
     
 %------------------------ FLOW CALCULATION -------------------------------%
     % Parameters for optical flow-- could be changed, could be parameters
-    %alpha_smooth   = 0.05;
+    alpha_smooth   = 0.05;
     max_iterations = 128;
     
     % Save flow calculation parameters parameters 
-    %options.flow_calculation.alpha_smooth   = alpha_smooth;
+    options.flow_calculation.alpha_smooth   = alpha_smooth;
     options.flow_calculation.max_iterations = max_iterations;
     options.flow_calculation.dtpts          = dtpts;
     options.flow_calculation.grid_size      = grid_size;
@@ -65,20 +56,7 @@ function varargout = main_neural_flows_hs3d_grid(data, X, Y, Z, time_vec, option
     % Save mask with points inside the convex hull of the brain
     mfile_vel.in_bdy_mask = in_bdy_mask;
     
-    % Get some dummy initial conditions
-    if isfield(options, 'chunk')
-        seed_init_vel = options.chunk; % for cluster environments
-    else   
-        seed_init_vel = 42;
-    end
     options.flow_calculation.seed_init_vel = seed_init_vel;
-    
-    
-    % The following lines will create the file on disk
-    mfile_vel.ux(grid_size(x_dim), grid_size(y_dim), grid_size(z_dim), dtpts-1) = 0;    
-    mfile_vel.uy(grid_size(x_dim), grid_size(y_dim), grid_size(z_dim), dtpts-1) = 0;
-    mfile_vel.uz(grid_size(x_dim), grid_size(y_dim), grid_size(z_dim), dtpts-1) = 0;
-    
     
     % This function runs the loop over timepoints and saves the velocity
     % fields into a file
@@ -99,11 +77,8 @@ function varargout = main_neural_flows_hs3d_grid(data, X, Y, Z, time_vec, option
     % Permute data, it seems we saved the time dimension as the last
     % dimension
     data = permute(data, [2 3 4 1]);
-    run_neural_flows3d_loop(data, mfile_vel, options)
+    flows3d_estimate_hs3d_flow(data, mfile_vel, options)
     fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Info:: Finished estimation of neural flows.'))
-
-    %flows3d_compute_neural_flows()
-
     
     mfile_vel.options = options;
     % Delete sentinels. If these varibales are OnCleanup objects, then the 
