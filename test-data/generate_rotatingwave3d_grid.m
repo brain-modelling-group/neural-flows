4396b93bf65df47894493f44d24f7129577d5e81
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-09-19 14:53:45 +1000

    Update

--- test-data/generate_rotatingwave3d_grid.m
+++ test-data/generate_rotatingwave3d_grid.m
@@ -1,18 +1,18 @@
-function [data] = generate_rotating_wave_3d(half_size, dt, T, frequency, lambda, direction, varargin)
+function [wave3d] = generate_rotatingwave3d_grid(half_size, ht, T, frequency, lambda, direction, varargin)
 % Generates a rotating wave in 3D space. The rotation occurs on the XY-plane. 
 % The position of the centre of rotation (the tip) - on xy - changes with 
 % depth z.
 % 
 %  ARGUMENTS:
 %        half_size  -- integer with specifying half of sidelength of the image cube
-%        dt         -- time step in seconds
+%        ht         -- time step in seconds
 %        T          -- maximum time in seconds 
 %        frequency  -- frequency in [Hz]
 %        lambda     -- radial wavelength in [m].
 %        direction  -- and integer +1 or -1, defines the direction of rotation. 
 %
 %  OUTPUT:
-%        data -- a 4D array of size [side_size, side_size, side_size, T/dt]; 
+%        data -- a 4D array of size [side_size, side_size, side_size, T/ht]; 
 %                where side_size = 2*half_size - 1; 
 %
 %  REQUIRES:
@@ -24,6 +24,8 @@ function [data] = generate_rotating_wave_3d(half_size, dt, T, frequency, lambda,
 %
 %  USAGE:
 %{
+
+wave3d = generate_rotatingwave3d_grid(16, 0.1, 10, 2, 2, 1);
 
 %}
 
@@ -42,14 +44,14 @@ end
 
 
 % Time axis 
-time = dt:dt:T;
+time = ht:ht:T;
 
 % Rotating spiral tip
 tip = (1:side_size)-center_point(3);
 tip_y = sin(tip);
 tip_x = cos(tip);
 
-data = zeros(side_size, side_size, side_size, T/dt );
+wave3d = zeros(T/ht, side_size, side_size, side_size);
 
 for ii = 1:side_size
     for jj = 1:side_size
@@ -63,12 +65,12 @@ for ii = 1:side_size
         % Rotation plane is XY
         [TH, ~] = cart2pol(X,Y);
 
-        data(ii, jj, kk, :) = exp(sign(frequency).*1i.*( 2*pi*abs(frequency)*time - direction.*TH(ii,jj)* ((2*pi)./lambda) ));
+        wave3d(:, ii, jj, kk) = exp(sign(frequency).*1i.*( 2*pi*abs(frequency)*time - direction.*TH(ii,jj)* ((2*pi)./lambda) ));
         
         end
     end
     
 end
-data = data .* exp( 1i .* phase_offset );  
-data = real(data);
+wave3d = wave3d .* exp(1i .* phase_offset);  
+wave3d = real(wave3d);
 end % end generate_rotating_wave_3d()
