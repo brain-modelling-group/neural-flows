c7278d6eb61a41603b2a85cd7b7984eb85c6c6d7
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-09-19 13:54:38 +1000

    Indent and close if block

--- core/main_neural_flows_hs3d_scatter.m
+++ core/main_neural_flows_hs3d_scatter.m
@@ -159,66 +159,67 @@ function varargout = main_neural_flows_hs3d_scatter(data, locs, options)
     
 %---------------------- DETECT NULL FLOWS ---------------------------------%    
    if options.sing_detection
-   % NOTE: TODO: The criterion/value for the detection therhesold should be a
-   % parameter it can be rerun with different types.
-   
-   % Close the file to avoid corruption
-   detection_threshold = guesstimate_nullflows_threshold(mfile_vel.min_nu);
-   mfile_vel.detection_threshold = detection_threshold;
-   mfile_vel.Properties.Writable = false;
-   
-   fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Started detection of null flows.'))
-   switch options.sing_detection.datamode
-       case 'surf'
-          error(['neural-flows:: ', mfilename, '::FutureOption. This singularity detection datamode is not fully implemented yet.'])
-           %fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Started extraction of critical isosurfaces'))
-           %Calculate critical isosurfaces
-           %[mfile_surf, mfile_surf_sentinel] = xperimental_extract_null_isosurfaces_parallel(mfile_vel);
-           %fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Finished extraction of critical isosurfaces'))
-           % Detect intersection of critical isosurfaces
-           % [null_points_3d]  = flows3d_hs3d_detect_nullflows_parallel(mfile_surf, X, Y, Z, in_bdy_mask, , options.sing_detection.datamode);
-           % delete(mfile_surf_sentinel)
-       case 'vel'
-           % Use velocity vector fields
-            [null_points_3d]  = flows3d_hs3d_detect_nullflows_parallel(mfile_vel, [], [], [], [], options.sing_detection.datamode);
- 
-       otherwise
-          error(['neural-flows:: ', mfilename, '::UnknownOption. Invalid datamode for detecting singularities.'])
-   end
-   
-
-
-   % Save what we just found    
-   root_fname_sings = 'temp_snglrty';
-   keep_sings_file = true; 
-   [mfile_sings, mfile_sings_sentinel] = create_temp_file(root_fname_sings, keep_sings_file);
-   mfile_sings.null_points_3d = null_points_3d;
-   delete(mfile_sings_sentinel)
-   fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Finished detection of null flows.'))
-   
-%------------------------ CLASSIFY SINGULARITIES -------------------------%    
-   fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Started classification of singularities.'))
-   % Calculate jacobian and classify singularities
-   singularity_classification = singularity3d_classify_singularities(null_points_3d, mfile_vel);
-   mfile_sings.singularity_classification = singularity_classification;
-   mfile_sings.options = options;
-   fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Finished classification of singularities.'))
-
-%-------------------------------------------------------------------------%
-% Check if we actually want to get the handles to the matfiles 
-minnout = 0;
-maxnout = 3;
-% NOTE: not sure this check is ok
-nargoutchk(minnout, maxnout);
-
-if nargout >= 1
-    varargout{1} = mfile_vel;
+       % NOTE: TODO: The criterion/value for the detection therhesold should be a
+       % parameter it can be rerun with different types.
+       
+       % Close the file to avoid corruption
+       detection_threshold = guesstimate_nullflows_threshold(mfile_vel.min_nu);
+       mfile_vel.detection_threshold = detection_threshold;
+       mfile_vel.Properties.Writable = false;
+       
+       fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Started detection of null flows.'))
+       switch options.sing_detection.datamode
+           case 'surf'
+               error(['neural-flows:: ', mfilename, '::FutureOption. This singularity detection datamode is not fully implemented yet.'])
+               %fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Started extraction of critical isosurfaces'))
+               %Calculate critical isosurfaces
+               %[mfile_surf, mfile_surf_sentinel] = xperimental_extract_null_isosurfaces_parallel(mfile_vel);
+               %fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Finished extraction of critical isosurfaces'))
+               % Detect intersection of critical isosurfaces
+               % [null_points_3d]  = flows3d_hs3d_detect_nullflows_parallel(mfile_surf, X, Y, Z, in_bdy_mask, , options.sing_detection.datamode);
+               % delete(mfile_surf_sentinel)
+           case 'vel'
+               % Use velocity vector fields
+               [null_points_3d]  = flows3d_hs3d_detect_nullflows_parallel(mfile_vel, [], [], [], [], options.sing_detection.datamode);
+               
+           otherwise
+               error(['neural-flows:: ', mfilename, '::UnknownOption. Invalid datamode for detecting singularities.'])
+       end
+       
+       
+       
+       % Save what we just found
+       root_fname_sings = 'temp_snglrty';
+       keep_sings_file = true;
+       [mfile_sings, mfile_sings_sentinel] = create_temp_file(root_fname_sings, keep_sings_file);
+       mfile_sings.null_points_3d = null_points_3d;
+       delete(mfile_sings_sentinel)
+       fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Finished detection of null flows.'))
+       
+       %------------------------ CLASSIFY SINGULARITIES -------------------------%
+       fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Started classification of singularities.'))
+       % Calculate jacobian and classify singularities
+       singularity_classification = singularity3d_classify_singularities(null_points_3d, mfile_vel);
+       mfile_sings.singularity_classification = singularity_classification;
+       mfile_sings.options = options;
+       fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Finished classification of singularities.'))
+       
+       %-------------------------------------------------------------------------%
+       % Check if we actually want to get the handles to the matfiles
+       minnout = 0;
+       maxnout = 3;
+       % NOTE: not sure this check is ok
+       nargoutchk(minnout, maxnout);
+       
+       if nargout >= 1
+           varargout{1} = mfile_vel;
+       end
+       if nargout == 2
+           varargout{2} = mfile_interp;
+       end
+       if nargout > 2
+           varargout{3} = mfile_sings;
+       end
 end
-if nargout == 2
-    varargout{2} = mfile_interp;
-end
-if nargout > 2
-   varargout{3} = mfile_sings;
-end
-              
+   
 end % function main_neural_flows_hs3d_scatter()
