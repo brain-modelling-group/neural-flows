397c5af058e04bf6d7a680976720552505f312b0
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-09-19 10:30:55 +1000

    Document

--- test-data/test_travellingwave3d_scattered.m
+++ test-data/test_travellingwave3d_scattered.m
@@ -1,6 +1,6 @@
 function test_travellingwave3d_scattered()
 
-% Generate data
+% Generate data with these step sizes
  options.hx = 2;
  options.hy = 2;
  options.hz = 2;
@@ -10,21 +10,20 @@ function test_travellingwave3d_scattered()
  load('513COG.mat', 'COG')
  locs = COG(1:256, :);
  
- options.data_interpolation.file_exists = false;
  
  [wave3d, ~] = generate_travellingwave3d_scattered(locs, 'hxyz',  options.hx, 'ht', options.ht);
  
- 
+ options.data_interpolation.file_exists = false;
  options.flow_calculation.init_conditions = 'random';
  options.flow_calculation.seed_init_vel = 42;
  options.flow_calculation.alpha_smooth   = 0.1;
  options.flow_calculation.max_iterations = 128;
- 
  options.sing_detection = false;
  
+ % Do the stuff
+ [mfile_flows] = main_neural_flows_hs3d_scatter(wave3d, locs, options);
 
-[mfile_flows] = main_neural_flows_hs3d_scatter(wave3d, locs, options);
-
+ % Plot the velocity estimates
  fig_hist = figure('Name', 'nflows-test-histograms-random-ic');
 
  subplot(1, 3, 1, 'Parent', fig_hist)
