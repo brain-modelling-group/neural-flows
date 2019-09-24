92e623d2bd4a05a1246a00e45eb370b8a79a55d1
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-09-24 16:05:05 +1000

    Test 3d sine/plane wave

---
+++ tests/test_flow_estimation__planewave3d_grid_hs3d.m
@@ -0,0 +1,32 @@
+function test_flow_estimation__planewave3d_grid_hs3d()
+
+% Generate data
+ options.hx = 1;
+ options.hy = 1;
+ options.hz = 1;
+ options.ht = 1;
+ [wave3d, X, Y, Z, ~] = generate_planewave3d_grid('visual_debugging', false, 'hxyz', options.hx, 'ht', options.ht, 'direction', 'x');
+ 
+ options.flow_calculation.init_conditions = 'random';
+ options.flow_calculation.seed_init_vel = 42;
+ options.flow_calculation.alpha_smooth   = 0.1;
+ options.flow_calculation.max_iterations = 128;
+
+ mfile_flows = main_neural_flows_hs3d_grid(wave3d, X, Y, Z, options);
+
+ fig_hist = figure('Name', 'nflows-test-planewave3d-grid-hs3d');
+
+ subplot(1, 3, 1, 'Parent', fig_hist)
+ histogram(mfile_flows.ux(2:end-1, 2:end-1, 2:end-1, :))
+ xlabel('ux')
+ 
+ subplot(1, 3 ,2, 'Parent', fig_hist)
+ histogram(mfile_flows.uy(2:end-1, 2:end-1, 2:end-1, :))
+ xlabel('uy')
+ 
+ subplot(1, 3, 3, 'Parent', fig_hist)
+ histogram(mfile_flows.uz(2:end-1, 2:end-1, 2:end-1, :))
+ xlabel('uz')
+
+ 
+end % function test_planewave3d_grid()
