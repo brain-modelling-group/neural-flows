dd807ab29491072f78b05a31c59e697ed41ee8fc
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-09-30 10:52:22 +1000

    Replace input variable from full grid array to just vector with grid size

--- utils/switch_index_mode.m
+++ utils/switch_index_mode.m
@@ -1,11 +1,11 @@
-function xyz_idx = switch_index_mode(xyz_lidx, index_mode, X)
-% A dummy function that wraps ind2sub
+function xyz_idx = switch_index_mode(xyz_lidx, index_mode, grid_size)
+% A dummy function that wraps Matlab's ind2sub
 
         switch index_mode
             case 'linear'
                 xyz_idx = xyz_lidx;
             case 'subscript' % Note: I vaguely remember this part not working properly.
-                [x_idx, y_idx, z_idx] = ind2sub(size(X), xyz_lidx); 
+                [x_idx, y_idx, z_idx] = ind2sub(grid_size, xyz_lidx); 
                 xyz_idx = [x_idx, y_idx, z_idx];
         end
 end % function switch_index_mode()
