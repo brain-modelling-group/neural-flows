9971d9652fddf145c523fabfe3b273406e897aba
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-10-22 15:13:39 +1000

    Update function

--- test-data/generate_flow3d_abc_grid.m
+++ test-data/generate_flow3d_abc_grid.m
@@ -1,4 +1,4 @@
-function [ux, uy, uz, X, Y, Z] = generate_flow3d_abc_grid(abc)
+function [ux, uy, uz, X, Y, Z] = generate_flow3d_abc_grid(abc, varargin)
 % Generates Arnold-Beltrami-Childress (ABC) 3D flow 
 % Arnold-Beltrami-Childress (ABC) flow is an analytically defined velocity ...
 % field which is known to exhibit chaotic trajectories. 
@@ -28,13 +28,16 @@ if nargin < 1
     A = 1;
     B = sqrt(2/3);
     C = sqrt(1/3);
+    visual_debugging = false;
 else
     A = abc(1);
     B = abc(2);
     C = abc(3);
 end
-    
 
+if nargin > 1
+    visual_debugging = varargin{1}; 
+end
 
 % Author, Paula Sanz-Leon, QIMR 2019
 
@@ -50,51 +53,52 @@ uz = C .* sin(Y) + B .* cos(X);
 x = linspace(0, 1, Nx);
 [X, Y, Z] = meshgrid(x, x, x);
 
-fig_handle = figure('Name', 'nflows-abcflow');
+if visual_debugging
+    fig_handle = figure('Name', 'nflows-abcflow');
 
-for kk=1:4
-    ax(kk) = subplot(2, 2, kk, 'Parent', fig_handle);
-    ax(kk).XLabel.String = 'x';
-    ax(kk).YLabel.String = 'y';
-    ax(kk).ZLabel.String = 'z';
-    ax(kk).View = [-37.5 30];
+    for kk=1:4
+        ax(kk) = subplot(2, 2, kk, 'Parent', fig_handle);
+        ax(kk).XLabel.String = 'x';
+        ax(kk).YLabel.String = 'y';
+        ax(kk).ZLabel.String = 'z';
+        ax(kk).View = [-37.5 30];
+    end
+
+    % Norm
+    unorm = sqrt(ux.^2 + uy.^2 + uz.^2);
+
+    hcone = coneplot(ax(1), X, Y, Z, ux, uy, uz, X, Y, Z, unorm);
+    camlight right
+    lighting gouraud
+    hcone_ux = coneplot(ax(2), X, Y, Z, ux, uy, uz, X, Y, Z, ux);
+    ax(2).Colormap = bluegred(256);
+    ax(2).CLim = [-1.5 1.5];
+
+
+    camlight right
+    lighting gouraud
+    hcone_uy = coneplot(ax(3), X, Y, Z, ux, uy, uz, X, Y, Z, uy);
+    ax(3).Colormap = bluegred(256);
+    ax(3).CLim = [-1.5 1.5];
+    camlight right
+    lighting gouraud
+
+    hcone_uz = coneplot(ax(4), X, Y, Z, ux, uy, uz, X, Y, Z, uz);
+    ax(4).Colormap = bluegred(256);
+    ax(4).CLim = [-1.5 1.5];
+    camlight right
+    lighting gouraud
+
+    set(hcone,'EdgeColor','none');
+    set(hcone_ux,'EdgeColor','none');
+    set(hcone_uy,'EdgeColor','none');
+    set(hcone_uz,'EdgeColor','none');
+
+
+    hcone.DiffuseStrength = 0.8;
+    hcone_ux.DiffuseStrength = 0.8;
+    hcone_uy.DiffuseStrength = 0.8;
+    hcone_uz.DiffuseStrength = 0.8;
+
 end
-
-% Norm
-unorm = sqrt(ux.^2 + uy.^2 + uz.^2);
-
-hcone = coneplot(ax(1), X, Y, Z, ux, uy, uz, X, Y, Z, unorm);
-camlight right
-lighting gouraud
-hcone_ux = coneplot(ax(2), X, Y, Z, ux, uy, uz, X, Y, Z, ux);
-ax(2).Colormap = bluegred(256);
-ax(2).CLim = [-1.5 1.5];
-
-
-camlight right
-lighting gouraud
-hcone_uy = coneplot(ax(3), X, Y, Z, ux, uy, uz, X, Y, Z, uy);
-ax(3).Colormap = bluegred(256);
-ax(3).CLim = [-1.5 1.5];
-camlight right
-lighting gouraud
-
-hcone_uz = coneplot(ax(4), X, Y, Z, ux, uy, uz, X, Y, Z, uz);
-ax(4).Colormap = bluegred(256);
-ax(4).CLim = [-1.5 1.5];
-camlight right
-lighting gouraud
-
-set(hcone,'EdgeColor','none');
-set(hcone_ux,'EdgeColor','none');
-set(hcone_uy,'EdgeColor','none');
-set(hcone_uz,'EdgeColor','none');
-
-
-hcone.DiffuseStrength = 0.8;
-hcone_ux.DiffuseStrength = 0.8;
-hcone_uy.DiffuseStrength = 0.8;
-hcone_uz.DiffuseStrength = 0.8;
-
-
 end % function generate_abcflow_3d_grid()
