30759068713c35d712d13722ae4aee135dde4c16
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-10-22 14:44:15 +1000

    Enable testing of all critical points

--- tests/test_singularity_classification__criticalpoints3d_grid.m
+++ tests/test_singularity_classification__criticalpoints3d_grid.m
@@ -36,34 +36,42 @@ if nargin < 1
     cp_type = 'all';
 end
 
-[~, ux, uy, uz, X, ~, ~] = generate_singularity3d_hyperbolic_critical_points(cp_type);
-
-% Fake time points
-max_t = 16;
-options.flow_calculation.grid_size = size(X);
-
-null_points_3d = struct([]);
-for ii=1:max_t
-    mstruct_vel.ux(:, :, :, ii) =  ux;
-    mstruct_vel.uy(:, :, :, ii) =  uy;
-    mstruct_vel.uz(:, :, :, ii) =  uz;
-    % Location of the critical points
-    null_points_3d(ii).xyz_idx = floor(options.flow_calculation.grid_size/2);
-
+if strcmp(cp_type, 'all')
+    s3d_list = s3d_get_singularity_list();
+    s3d_list(9:end) = [];
 end
 
-mstruct_vel.options = options;
-mstruct_vel.hx = 2^-4;
-mstruct_vel.hy = 2^-4;
-mstruct_vel.hz = 2^-4;
+for this_cp=1:length(s3d_list)
+    cp_type = s3d_list(this_cp);
+    [~, ux, uy, uz, X, ~, ~] = generate_singularity3d_hyperbolic_critical_points(cp_type);
 
-% Perform classification
-[singularity_classification_list] =   singularity3d_classify_singularities(null_points_3d, mstruct_vel);
+    % Fake time points
+    max_t = 16;
+    options.flow_calculation.grid_size = size(X);
 
-err_message = ['neural-flows' mfilename '::ClassificationError:: ', ...
-               'Input singularity type (' cp_type ') does not match classification result '];
+    null_points_3d = struct([]);
+    for ii=1:max_t
+        mstruct_vel.ux(:, :, :, ii) =  ux;
+        mstruct_vel.uy(:, :, :, ii) =  uy;
+        mstruct_vel.uz(:, :, :, ii) =  uz;
+        % Location of the critical points
+        null_points_3d(ii).xyz_idx = floor(options.flow_calculation.grid_size/2);
 
-for tt=1:max_t
-    assert(strcmp(singularity_classification_list{tt}, cp_type), [err_message '(' singularity_classification_list{tt} ').']);
+    end
+
+    mstruct_vel.options = options;
+    mstruct_vel.hx = 2^-4;
+    mstruct_vel.hy = 2^-4;
+    mstruct_vel.hz = 2^-4;
+
+    % Perform classification
+    [singularity_classification_list] =   singularity3d_classify_singularities(null_points_3d, mstruct_vel);
+
+    err_message = ['neural-flows' mfilename '::ClassificationError:: ', ...
+                   'Input singularity type (' cp_type ') does not match classification result '];
+
+    for tt=1:max_t
+        assert(strcmp(singularity_classification_list{tt}, cp_type), [err_message '(' singularity_classification_list{tt} ').']);
+    end
 end
 end %function test_sing_classification_critical_points_grid()
