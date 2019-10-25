0e66b5c47f09fe57d3e2878e49683f5683f6ea0f
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-10-25 12:19:17 +1000

    Polish function

--- analysis/analyse_singularities.m
+++ analysis/analyse_singularities.m
@@ -1,4 +1,4 @@
-function sing_count = analyse_singularities(singularity_list_str, XYZ)
+function sing_count = analyse_singularities(singularity_list_str, null_points_3d)
 % This function takes as an input a matfile with the list of
 % singularities, and generates plots to give an idea of their
 % beahviour over time
@@ -34,88 +34,15 @@ singularity_list_num = s3d_str2d_num_label(singularity_list_str);
 % Count how many singularities of each type we have
 sing_count = s3d_count_singularities(singularity_list_num);
 
-
 % Plot traces of each singularity
 plot_singularity_count_traces(sing_count)
 
 % NOTE: use sing_labels, rather than the file, so we can 
 % pass directly the output of this function and save ourselves a bit of
 % time.
-plot_singularity_scatter(singularity_list_str, sing_labels, XYZ, num_frames)
+plot_singularity_scatter_xyz_vs_time(singularity_list_str, sing_labels, XYZ, num_frames)
 
 
 end % function analyse_singularities()
 
 
-function plot_singularity_scatter(mfile_sing, sing_labels, XYZ, num_frames)
-    
-    cmap_base_list = s3d_get_colours('all');
-
-    figure_handle_xyz = figure('Name', 'nflows-singularities-over-spacetime');
-    numsubplot = 3; % One for each spatial dimension
-    ax_xyz = gobjects(numsubplot);
-    for jj=1:numsubplot
-        ax_xyz(jj) = subplot(numsubplot, 1, jj, 'Parent', figure_handle_xyz);
-        hold(ax_xyz(jj), 'on')
-    end
-    
-    % Kindda hardcoded values, but at least make it idiomatic
-    source_ = 1;
-    spiral_source_ = 5;
-    saddle_source = 3;
-    saddle_source_ = 7;
-
-    sink_ = 2;
-    spiral_sink_ = 6;
-    saddle_sink = 4;
-    saddle_sink_ = 8; 
-
-    xyz_idx = mfile_sing.xyz_idx;
-    
-    y_labels = {'X', 'Y', 'Z'};
-
-    for ii=1:numsubplot     
-        for tt=256:num_frames
-            xyz = xyz_idx(1, tt).xyz_idx;  
-            idx_source = find(sing_labels(tt).numlabel == source_);
-            idx_spiral_source = find(sing_labels(tt).numlabel == spiral_source_);
-            idx_saddle_source = find(sing_labels(tt).numlabel == saddle_source);
-            idx_saddle_source_ = find(sing_labels(tt).numlabel == saddle_source_);
-
-            idx_sink = find(sing_labels(tt).numlabel == sink_);
-            idx_spiral_sink = find(sing_labels(tt).numlabel == spiral_sink_);
-            idx_saddle_sink = find(sing_labels(tt).numlabel == saddle_sink);
-            idx_saddle_sink_ = find(sing_labels(tt).numlabel == saddle_sink_);
-
-
-            plot(ax_xyz(ii), tt*ones(length(idx_source), 1), XYZ(xyz(idx_source), ii), ...
-                 '.', 'markeredgecolor', 'r')
-            plot(ax_xyz(ii), tt*ones(length(idx_spiral_source), 1), XYZ(xyz(idx_spiral_source), ii), ...
-                 '.', 'markeredgecolor', 'r')
-            % 
-            plot(ax_xyz(ii), tt*ones(length(idx_sink), 1), XYZ(xyz(idx_sink), ii), ...
-                 '.', 'markeredgecolor', 'b')
-            plot(ax_xyz(ii), tt*ones(length(idx_spiral_sink), 1), XYZ(xyz(idx_spiral_sink), ii), ...
-                 '.', 'markeredgecolor', 'b')
-
-            plot(ax_xyz(ii), tt*ones(length(idx_saddle_sink), 1), XYZ(xyz(idx_saddle_sink), ii), ...
-                 'v', 'markeredgecolor', cmap(saddle_sink_, 1:3), 'markersize', 4 )
-            plot(ax_xyz(ii), tt*ones(length(idx_saddle_sink_), 1), XYZ(xyz(idx_saddle_sink_), ii), ...
-                 'v', 'markeredgecolor', cmap(saddle_sink_, 1:3), 'markersize', 4) 
-
-            plot(ax_xyz(ii), tt*ones(length(idx_saddle_source), 1), XYZ(xyz(idx_saddle_source), ii), ...
-                 '^', 'markeredgecolor', cmap(saddle_source_, 1:3), 'markersize', 4)
-            plot(ax_xyz(ii), tt*ones(length(idx_saddle_source_), 1), XYZ(xyz(idx_saddle_source_), ii), ...
-                 '^', 'markeredgecolor', cmap(saddle_source_, 1:3), 'markersize', 4) 
-
-            
-
-        end
-        ax_xyz(ii).YLim = [-max(abs(XYZ(:, ii))) max(abs(XYZ(:, ii)))];
-        ax_xyz(ii).XLim = [1 num_frames];
-        ax_xyz(ii).YLabel.String = y_labels{ii}; 
-    end
-    
-    ax_xyz(3).XLabel.String = 'time';
-    linkaxes(ax_xyz, 'x')
-end
--- vis/plot_singularity_scatter_xyz_vs_time.m
+++ vis/plot_singularity_scatter_xyz_vs_time.m
@@ -1,12 +1,12 @@
-function figure_handle_xyz = plot_singularity_scatter_xyz_vs_time(singularity_list_num, null_points_3d, figure_handle_xyz)
+function figure_handle_xyz = plot_singularity_scatter_xyz_vs_time(singularity_list_num, null_points_3d, cp_type, figure_handle_xyz)
 
 % This function plots different type of singularities as a a function of 
 % position along one axis (X, Y, Z) vs time.
 
 numsubplot = 3; % One for each spatial dimension
 
-if nargin < 3
-    figure_handle_xyz = figure('Name', 'nflows-singularities-over-spacetime');
+if nargin < 4
+    figure_handle_xyz = figure('Name', 'nflows-singularities-over-1d-space-time');
     ax_xyz = gobjects(numsubplot);
 
     for jj=1:numsubplot
@@ -22,68 +22,48 @@ else
         
 end
 
-% Get numeric labels of singularities we want to plot
-source_ = s3d_get_numlabel('source');
-spiral_source_ = s3d_get_numlabel('spiral-source');
-saddle_source =  s3d_get_numlabel('1-2-saddle');
-saddle_source_ = s3d_get_numlabel('1-2-spiral-saddle');
-
-sink_ = s3d_get_numlabel('sink');
-spiral_sink_ = s3d_get_numlabel('spiral-sink');
-saddle_sink  = s3d_get_numlabel('2-1-saddle');
-saddle_sink_ = s3d_get_numlabel('2-1-spiral-saddle'); 
+num_frames = length(singularity_list_num);
     
 y_labels = {'X', 'Y', 'Z'};
 x_labels = {'', '', 'time'};
-
-cmap_base_list = s3d_get_colours('all');
-
-
-    for ii=1:numsubplot     
-        for tt=1:num_frames
-            x = null_points_3d(1, tt).x;
-            y = null_points_3d(1, tt).y;
-            z = null_points_3d(1, tt).z;
-            
-            idx_source = find(singularity_list_num{tt} == source_);
-            idx_spiral_source = find(singularity_list_num == spiral_source_);
-            idx_saddle_source = find(sigunlariy_list_num(tt).numlabel == saddle_source);
-            idx_saddle_source_ = find(sing_labels(tt).numlabel == saddle_source_);
-
-            idx_sink = find(sing_labels(tt).numlabel == sink_);
-            idx_spiral_sink = find(sing_labels(tt).numlabel == spiral_sink_);
-            idx_saddle_sink = find(sing_labels(tt).numlabel == saddle_sink);
-            idx_saddle_sink_ = find(sing_labels(tt).numlabel == saddle_sink_);
+xplot = 1;
+yplot = 2;
+zplot = 3;
 
 
-            plot(ax_xyz(ii), tt*ones(length(idx_source), 1), x(idx_source), ...
-                 '.', 'markeredgecolor', 'r')
-            plot(ax_xyz(ii), tt*ones(length(idx_spiral_source), 1), y(idx_spiral_source), ii), ...
-                 '.', 'markeredgecolor', 'r')
-            % 
-            plot(ax_xyz(ii), tt*ones(length(idx_sink), 1), z(idx_sink), ii), ...
-                 '.', 'markeredgecolor', 'b')
-            plot(ax_xyz(ii), tt*ones(length(idx_spiral_sink), 1), XYZ(xyz(idx_spiral_sink), ii), ...
-                 '.', 'markeredgecolor', 'b')
+num_sing_to_plot = length(cp_type);
 
-            plot(ax_xyz(ii), tt*ones(length(idx_saddle_sink), 1), XYZ(xyz(idx_saddle_sink), ii), ...
-                 'v', 'markeredgecolor', cmap(saddle_sink_, 1:3), 'markersize', 4 )
-            plot(ax_xyz(ii), tt*ones(length(idx_saddle_sink_), 1), XYZ(xyz(idx_saddle_sink_), ii), ...
-                 'v', 'markeredgecolor', cmap(saddle_sink_, 1:3), 'markersize', 4) 
+get_idx_cp_type = @(sing_list_frame, num_label_cp_type) find(sing_list_frame == num_label_cp_type);
 
-            plot(ax_xyz(ii), tt*ones(length(idx_saddle_source), 1), XYZ(xyz(idx_saddle_source), ii), ...
-                 '^', 'markeredgecolor', cmap(saddle_source_, 1:3), 'markersize', 4)
-            plot(ax_xyz(ii), tt*ones(length(idx_saddle_source_), 1), XYZ(xyz(idx_saddle_source_), ii), ...
-                 '^', 'markeredgecolor', cmap(saddle_source_, 1:3), 'markersize', 4) 
 
-            
+for cc=1:num_sing_to_plot
+    
+    num_label_cp = s3d_get_numlabel(cp_type{cc});
+    cmap_cp = s3d_get_colours(cp_type{cc});
+    % Remove alpha
+    cmap_cp(:, 4) = []; 
 
-        end
-        ax_xyz(ii).YLim = [-max(abs(XYZ(:, ii))) max(abs(XYZ(:, ii)))];
-        ax_xyz(ii).XLim = [1 num_frames];
-        ax_xyz(ii).YLabel.String = y_labels{ii}; 
-        ax_xyz(ii).YLabel.String = x_labels{ii}; 
-        
+    
+    for tt=1:num_frames
+        x = null_points_3d(1, tt).x;
+        y = null_points_3d(1, tt).y;
+        z = null_points_3d(1, tt).z;
+        idx_cp_type = get_idx_cp_type(singularity_list_num{tt}, num_label_cp);
+        plot(ax_xyz(xplot), tt*ones(length(idx_cp_type), 1), x(idx_cp_type), '.', 'markeredgecolor', cmap_cp)
+        plot(ax_xyz(yplot), tt*ones(length(idx_cp_type), 1), y(idx_cp_type), '.', 'markeredgecolor', cmap_cp)
+        plot(ax_xyz(zplot), tt*ones(length(idx_cp_type), 1), z(idx_cp_type), '.', 'markeredgecolor', cmap_cp)
+
     end
+           
+       
+        [ax_xyz(xplot:zplot).XLim] = deal([1 num_frames]);
+        ax_xyz(xplot).YLabel.String = y_labels{xplot}; 
+        ax_xyz(xplot).XLabel.String = x_labels{xplot};
+        ax_xyz(yplot).YLabel.String = y_labels{yplot}; 
+        ax_xyz(yplot).XLabel.String = x_labels{yplot}; 
+        ax_xyz(zplot).YLabel.String = y_labels{zplot}; 
+        ax_xyz(zplot).XLabel.String = x_labels{zplot}; 
+        
+end
     linkaxes(ax_xyz, 'x')
 end
