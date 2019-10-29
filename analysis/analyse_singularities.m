52d39ccb84e078749fb258992c1a53f3db9bf58a
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-10-29 12:07:54 +1000

    Catch error due to changes in the stored variable names

--- analysis/analyse_singularities.m
+++ analysis/analyse_singularities.m
@@ -14,15 +14,20 @@ function varargout = analyse_singularities(mobj_sing)
 %        s3d_get_base_singularity_list()
 %        s3d_get_numlabel()
 %        s3d_count_singularities()
+%        plot_singularity_scatter_xyz_vs_time
 %
 % USAGE:
 %{
     <example-commands-to-make-this-function-run>
 %}
 % PSL, QIMR August 2019
-
-singularity_list_num = s3d_str2num_label(mobj_sing.singularity_classification_list);
-
+try
+    singularity_list_num = s3d_str2num_label(mobj_sing.singularity_classification_list);
+catch
+    % deprecated variable name
+    singularity_list_num = s3d_str2num_label(mobj_sing.singularity_classification);
+end
+    
 % Count how many singularities of each type we have
 sing_count = s3d_count_singularities(singularity_list_num);
 
@@ -34,7 +39,7 @@ base_list = s3d_get_base_singularity_list();
 cp_base = base_list(1:4);
 cp_saddles = base_list(5:8);
 
-fig_xyz_base = plot_singularity_scatter_xyz_vs_time(singularity_list_num, mobj_sing.null_points_3d, cp_base);
+fig_xyz_base    = plot_singularity_scatter_xyz_vs_time(singularity_list_num, mobj_sing.null_points_3d, cp_base);
 fig_xyz_saddles = plot_singularity_scatter_xyz_vs_time(singularity_list_num, mobj_sing.null_points_3d, cp_saddles);
 
 varargout{1} = {fig_xyz_base};
