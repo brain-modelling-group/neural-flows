b0a6f8ae3f6ac19311590ee27f1c49961fa745ff
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-11-05 08:03:45 +1000

    Plot potential centres of periodic orbits

--- analysis/analyse_singularities.m
+++ analysis/analyse_singularities.m
@@ -1,11 +1,11 @@
-function varargout = analyse_singularities(mobj_sing)
+function varargout = analyse_singularities(mobj_sings)
 % This function takes as an input a matfile with the list of
 % singularities, and generates plots to give an idea of their
 % beahviour over time and space (1d). It's basically a visual summary.
 %
 %
 % ARGUMENTS:
-%           mobj_sing -- a MatFile or a structure with the same internal
+%           mobj_sings -- a MatFile or a structure with the same internal
 %                        structure with the singularitiy classification
 %
 % OUTPUT:   
@@ -18,11 +18,11 @@ function varargout = analyse_singularities(mobj_sing)
 %
 % USAGE:
 %{
-    <example-commands-to-make-this-function-run>
+    
 %}
 % PSL, QIMR August 2019
 
-singularity_list_num = s3d_str2num_label(mobj_sing.singularity_classification_list);
+singularity_list_num = s3d_str2num_label(mobj_sings.singularity_classification_list);
     
 % Count how many singularities of each type we have
 sing_count = s3d_count_singularities(singularity_list_num);
@@ -31,20 +31,22 @@ sing_count = s3d_count_singularities(singularity_list_num);
 plot_singularity_count_traces(sing_count)
 
 base_list = s3d_get_base_singularity_list();
+
 % Basic options
 cp_base = base_list(1:4);
 cp_saddles = base_list(5:8);
 
-fig_xyz_base    = plot_singularity_scatter_xyz_vs_time(singularity_list_num, mobj_sing.null_points_3d, cp_base);
-fig_xyz_saddles = plot_singularity_scatter_xyz_vs_time(singularity_list_num, mobj_sing.null_points_3d, cp_saddles);
-fig_xyz_all = plot_singularity_scatter_xyz_vs_time(singularity_list_num, mobj_sing.null_points_3d, base_list(1:8));
+% Plot scatters over time
+fig_xyz_base    = plot_singularity_scatter_xyz_vs_time(singularity_list_num, mobj_sings.null_points_3d, cp_base);
+fig_xyz_saddles = plot_singularity_scatter_xyz_vs_time(singularity_list_num, mobj_sings.null_points_3d, cp_saddles);
+fig_xyz_all_cp = plot_singularity_scatter_xyz_vs_time(singularity_list_num, mobj_sings.null_points_3d, base_list(1:8));
+fig_xyz_po = plot_singularity_scatter_xyz_vs_time(singularity_list_num, mobj_sings.null_points_3d, base_list(9:14));
 
-
+% Return figure handles
 varargout{1} = {fig_xyz_base};
 varargout{2} = {fig_xyz_saddles};
-varargout{3} = {fig_xyz_all};
+varargout{3} = {fig_xyz_all_cp};
+varargout{4} = {fig_xyz_po};
 
 
 end % function analyse_singularities()
-
-
