3df620aad8270cd216a954d23274948d3e7ccbe0
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-11-06 08:29:05 +1000

    Beautify plotting function

--- vis/plot_svd_modes.m
+++ vis/plot_svd_modes.m
@@ -1,38 +1,77 @@
 function fig_spatial_modes = plot_svd_modes(V, U, X, Y, Z, time_vec, num_modes, num_points, prct_var, quiver_scale_factor)
 
-  fig_spatial_modes = figure('Name', 'nflows-spatial-modes');
+    fig_spatial_modes = figure('Name', 'nflows-spatial-modes');
 
-    ax = gobjects(num_modes, 1);
-    for kk=1:num_modes
-       ax(kk) = subplot(2, num_modes, kk, 'Parent', fig_spatial_modes);
-       hold(ax(kk), 'on')
+    num_planes = 3; % number of spatial projections
+    ax = gobjects(num_planes+1, num_modes);
+    
+    for ii=1:num_planes
+        for kk=1:num_modes
+            ax(ii, kk) = subplot(num_planes+1, num_modes, sub2ind([num_modes, num_planes+1], kk, ii), 'Parent', fig_spatial_modes);
+            hold(ax(ii, kk), 'on')
+        end
     end
 
-    ax(num_modes+1) = subplot(2, num_modes, [num_modes+1 2*num_modes], 'Parent', fig_spatial_modes);
-    hold(ax(num_modes+1), 'on')
+    ax(num_planes+1, 1) = subplot(num_planes+1, num_modes, [(num_planes*num_modes)+1 (num_modes*num_planes)+num_modes], 'Parent', fig_spatial_modes);
+    hold(ax(num_planes+1, 1), 'on')
+    
+    x_idx = 1:num_points;
+    y_idx = num_points+1:2*num_points;
+    z_idx = 2*num_points+1:3*num_points;
+    
+    Vnorm =  sqrt(V(x_idx, :).^2+ V(y_idx, :).^2+ V(z_idx, :).^2);
+    % NOTE: maybe enable this check to avoid zero division
+    %Vnorm(Vnorm < 2^-9) = 1;
+    
+    cmap = plasma(num_modes+2);
+    cmap = cmap(1:num_modes, :);
+    cmap = cmap(end:-1:1, :);
 
+    xy = 1;
+    xz = 2;
+    zy = 3;
     for imode = 1:num_modes
-        x_idx = 1:num_points;
-        y_idx = num_points+1:2*num_points;
-        z_idx = 2*num_points+1:3*num_points;
-        quiver3(ax(imode), X, Y, Z, V(x_idx, imode), V(y_idx, imode), V(z_idx, imode), quiver_scale_factor);
-        ax(imode).View = [0 90];
-        ax(imode).Title.String = sprintf('Mode %i, Var = %0.1f%%', imode, prct_var(imode));
+        quiver3(ax(xy, imode), X, Y, Z, V(x_idx, imode)./Vnorm(:, imode), ...
+                                        V(y_idx, imode)./Vnorm(:, imode), ...
+                                        V(z_idx, imode)./Vnorm(:, imode), ...
+                                        quiver_scale_factor, 'Linewidth', 1, ...
+                                        'Color', cmap(imode, :));
+        quiver3(ax(xz, imode), X, Y, Z, V(x_idx, imode)./Vnorm(:, imode), ...
+                                        V(y_idx, imode)./Vnorm(:, imode), ...
+                                        V(z_idx, imode)./Vnorm(:, imode), ...
+                                        quiver_scale_factor, 'Linewidth', 1, ...
+                                        'Color', cmap(imode, :));
+        quiver3(ax(zy, imode), X, Y, Z, V(x_idx, imode)./Vnorm(:, imode), ...
+                                        V(y_idx, imode)./Vnorm(:, imode), ...
+                                        V(z_idx, imode)./Vnorm(:, imode), ...
+                                        quiver_scale_factor, 'Linewidth', 1, ...
+                                        'Color', cmap(imode, :));
+
+        ax(xy, imode).View = [ 0 90];
+        ax(xz, imode).View = [ 0  0];
+        ax(zy, imode).View = [90  0];
+
+        ax(xy, imode).Title.String = sprintf('Mode %i, Var = %0.1f%%', imode, prct_var(imode));
+        axis(ax(xy, imode), 'tight', 'off')
+        axis(ax(xz, imode), 'tight', 'off')
+        axis(ax(zy, imode), 'tight', 'off')
 
     end
     
-     smooth_window = 4; % number of samples to smooth the envelope
+     smooth_window = 8; % number of samples to smooth the envelope
      [~, Upeak] = envelope(U, smooth_window, 'peak');
      max_u_val = 1.1*max(abs(Upeak(:)));
      ulims = [-max_u_val max_u_val];
      
-     plot(ax(num_modes+1), time_vec, Upeak)
-     ax(num_modes+1).Title.String = sprintf('Modes timeseries');
-     ax(num_modes+1).YLim = ulims;
-     ax(num_modes+1).XLim = [time_vec(1), time_vec(end)];
-     line(ax(num_modes+1), ax(num_modes+1).XLim, [0 0], 'Color', 'k')
-     ax(num_modes+1).XLabel.String = 'Time';
-     ax(num_modes+1).YLabel.String = 'Component score';
+     for imode=1:num_modes
+         plot(ax(num_planes+1, 1), time_vec, Upeak(:, imode), 'Color', cmap(imode, :))
+     end
+     ax(num_planes+1, 1).Title.String = sprintf('Modes timeseries');
+     ax(num_planes+1, 1).YLim = ulims;
+     ax(num_planes+1, 1).XLim = [time_vec(1), time_vec(end)];
+     line(ax(num_planes+1, 1), ax(num_planes+1, 1).XLim, [0 0], 'Color', 'k', 'LineStyle', '--')
+     ax(num_planes+1, 1).XLabel.String = 'Time';
+     ax(num_planes+1, 1).YLabel.String = 'Component score';
 
 
  end % function plot_svd_modes()
