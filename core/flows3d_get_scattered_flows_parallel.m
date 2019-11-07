e359cd45a96206a838d021640023ffe7e233d325
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-11-07 12:33:55 +1000

    Hack to avoid nans in nodal flows -- to fix

--- core/flows3d_get_scattered_flows_parallel.m
+++ core/flows3d_get_scattered_flows_parallel.m
@@ -21,10 +21,8 @@ function [mflows_obj] = flows3d_get_scattered_flows_parallel(mflows_obj, locs)
 
     % These parameter values are essential
     neighbour_method = 'natural';
-    extrapolation_method = 'linear'; % HACK: HACK: TEO REMOVE BOUNDARY FLOW CALCULATION IS FIXED
-    % NOTE: HACK: HACK:  TO REMOVE ONCE I FIXED THE BOUNDARY CALCULATION
-    locs = 0.9*locs;
-
+    extrapolation_method = 'nearest'; 
+    
     x_dim = 1;
     y_dim = 2;
     z_dim = 3;
@@ -33,15 +31,34 @@ function [mflows_obj] = flows3d_get_scattered_flows_parallel(mflows_obj, locs)
     if tpts < 2
         disp('NOTE to self: This will fail because there is only one data point')
     end
+    X = mflows_obj.X;
+    Y = mflows_obj.Y;
+    Z = mflows_obj.Z;
     
     %in_bdy_mask_idx = find(double(mflows_obj.in_bdy_mask));
-    in_bdy_mask_idx = 1:numel(mflows_obj.in_bdy_mask);
-    X = mflows_obj.X;
-    X = X(in_bdy_mask_idx).';
-    Y = mflows_obj.Y;
-    Y = Y(in_bdy_mask_idx).';
-    Z = mflows_obj.Z;
-    Z = Z(in_bdy_mask_idx).';
+    %in_bdy_mask_idx = 1:numel(mflows_obj.in_bdy_mask);
+    %%% HACK HACK HACK
+    %%% HACK HACK HACK %% 
+    
+    in_bdy_mask_idx = find(~isnan(mflows_obj.ux(:, :, : , 1)));
+    for ii=1:size(locs, 1)
+    
+        temp = dis(locs(ii, :).', [X(in_bdy_mask_idx), Y(in_bdy_mask_idx), Z(in_bdy_mask_idx)].');
+   
+        [~, min_idx] = min(temp);
+   
+        fake_locs(ii, :) = [X(in_bdy_mask_idx(min_idx)) Y(in_bdy_mask_idx(min_idx)) Z(in_bdy_mask_idx(min_idx))];
+   
+    end
+    
+    locs = fake_locs;
+    
+    %%% END OF HACK HACK
+    
+    
+    X = X(in_bdy_mask_idx);
+    Y = Y(in_bdy_mask_idx);
+    Z = Z(in_bdy_mask_idx);
 
    
     % Write dummy data to disk
@@ -66,16 +83,16 @@ function [mflows_obj] = flows3d_get_scattered_flows_parallel(mflows_obj, locs)
             uz = mflows_obj.uz(:, :, :, idx);
             
             data_interpolant_x = scatteredInterpolant(X, Y, Z , ...
-                                                    ux(in_bdy_mask_idx).', ...
+                                                    ux(in_bdy_mask_idx), ...
                                                     neighbour_method, ...
                                                     extrapolation_method);
             data_interpolant_y = scatteredInterpolant(X, Y, Z , ...
-                                                    uy(in_bdy_mask_idx).', ...
+                                                    uy(in_bdy_mask_idx), ...
                                                     neighbour_method, ...
                                                     extrapolation_method);
                                                 
             data_interpolant_z = scatteredInterpolant(X, Y, Z , ...
-                                                    uz(in_bdy_mask_idx).', ...
+                                                    uz(in_bdy_mask_idx), ...
                                                     neighbour_method, ...
                                                     extrapolation_method);
                                                 
