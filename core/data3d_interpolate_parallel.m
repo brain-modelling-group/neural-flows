75c8d39aea764265b9d67c892052a96cc1c19e25
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-11-07 13:08:48 +1000

    Extrapolate, otherwise the interp_mask is not tken into account

--- core/data3d_interpolate_parallel.m
+++ core/data3d_interpolate_parallel.m
@@ -27,7 +27,7 @@ function [mfile_interp_obj, mfile_interp_sentinel] = data3d_interpolate_parallel
 
     % These parameter values are essential
     neighbour_method = 'natural';
-    extrapolation_method = 'none';
+    extrapolation_method = 'nearest';
 
 
     x_dim = 2;
