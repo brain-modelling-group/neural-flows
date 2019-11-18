b3c8ac2aa4504a2c177faf56e1a3772cf13750e0
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-11-18 15:11:19 +1000

    Remove deprecated check

--- core/singularity3d_detection.m
+++ core/singularity3d_detection.m
@@ -26,13 +26,8 @@ function mfile_sings = singularity3d_detection(mfile_flows, detection_threshold)
 options = mfile_flows.options; 
 
 if nargin < 2
-    try 
-        % Newer version
-        detection_threshold = guesstimate_nullflows_threshold(mfile_flows.min_un);
-    catch 
-        % Older version, to remove
-        detection_threshold = guesstimate_nullflows_threshold(mfile_flows.min_nu);
-    end
+    % Newer version
+    detection_threshold = guesstimate_nullflows_threshold(mfile_flows.min_un);
 end
 
 
