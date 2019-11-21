3f7957c4233fdc420542c49fc4277c11b3c69f7e
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-11-21 14:09:26 +1000

    Enable option to use either plot() or scatter()

--- vis/plot_singularity_scatter_xyz_vs_time.m
+++ vis/plot_singularity_scatter_xyz_vs_time.m
@@ -1,4 +1,4 @@
-function figure_handle_xyz = plot_singularity_scatter_xyz_vs_time(singularity_list_num, null_points_3d, cp_type, xyz_lims, varargin)
+function figure_handle_xyz = plot_singularity_scatter_xyz_vs_time(singularity_list_num, null_points_3d, cp_type, xyz_lims, marker_plot, varargin)
 % This function plots singularities as a a function of 
 % their position along one axis (X, Y, Z) vs time.
 %
@@ -11,6 +11,11 @@ function figure_handle_xyz = plot_singularity_scatter_xyz_vs_time(singularity_li
 %                             we want to plot the results. Normally first
 %                             call should omit this input, but passed to
 %                             the function in subsequent calls.
+%        xyz_lims    -- a 1 x 3 cell with [1 x 2] vectors determinign the
+%                       ranges of the X,Y, Z grid
+%        marker_plot -- a string to determine which function to use:
+%                       scatter or plot. 'scatter' makes prettier dots, but
+%                       pplot is faster and uses less memory.
 %         varargin   -- for the time being a 1 x 2 cell array with
 %                      {'Visible', 'off'}:
 %
@@ -56,11 +61,16 @@ num_sing_to_plot = length(cp_type);
 
 % Anonymous function to get the indices of the singularity we want to plot
 get_idx_cp_type = @(sing_list_frame, num_label_cp_type) find(sing_list_frame == num_label_cp_type);
-workers_fraction = 0.8;
-open_parpool(workers_fraction)
 
 marker_size = 10;
 marker_alpha = 0.3;
+
+if strcmp(marker_plot, 'scatter')
+   plotfun = @scatter_plot;
+else
+   plotfun = @line_plot;   
+end
+
 for cc=1:num_sing_to_plot
     
     num_label_cp = s3d_get_numlabel(cp_type{cc});
@@ -71,21 +81,7 @@ for cc=1:num_sing_to_plot
         y = null_points_3d(1, tt).y;
         z = null_points_3d(1, tt).z;
         idx_cp_type = get_idx_cp_type(singularity_list_num{tt}, num_label_cp);
-        
-        plot(ax_xyz(xplot), tt*ones(length(idx_cp_type), 1), x(idx_cp_type), '.', 'markeredgecolor', cmap_cp)
-        plot(ax_xyz(yplot), tt*ones(length(idx_cp_type), 1), y(idx_cp_type), '.', 'markeredgecolor', cmap_cp)
-        plot(ax_xyz(zplot), tt*ones(length(idx_cp_type), 1), z(idx_cp_type), '.', 'markeredgecolor', cmap_cp)
-        
-%         scatter(ax_xyz(xplot), tt*ones(length(idx_cp_type), 1), x(idx_cp_type), ...
-%                                marker_size, cmap_cp(ones(length(idx_cp_type), 1), :), ...
-%                                'filled', 'MarkerFaceAlpha', marker_alpha)
-%         scatter(ax_xyz(yplot), tt*ones(length(idx_cp_type), 1), y(idx_cp_type), ...
-%                                marker_size, cmap_cp(ones(length(idx_cp_type), 1), :), ...
-%                                'filled', 'MarkerFaceAlpha', marker_alpha)
-%         scatter(ax_xyz(zplot), tt*ones(length(idx_cp_type), 1), z(idx_cp_type), ...
-%                                marker_size, cmap_cp(ones(length(idx_cp_type), 1), :), ...
-%                                'filled', 'MarkerFaceAlpha', marker_alpha)
-
+        plotfun()
     end
         % Limits
         offset_factor = 2; % NOTE: this should be a configurable parameter
@@ -104,5 +100,30 @@ for cc=1:num_sing_to_plot
         
 end
     linkaxes(ax_xyz, 'x')
+    
+    
+    function line_plot()
+      plot(ax_xyz(xplot), tt*ones(length(idx_cp_type), 1), x(idx_cp_type), ...
+                          '.', 'markeredgecolor', cmap_cp)
+      plot(ax_xyz(yplot), tt*ones(length(idx_cp_type), 1), y(idx_cp_type), ...
+                          '.', 'markeredgecolor', cmap_cp)
+      plot(ax_xyz(zplot), tt*ones(length(idx_cp_type), 1), z(idx_cp_type), ...
+                          '.', 'markeredgecolor', cmap_cp)
+    end % line_plot()
+
+
+    function scatter_plot()
+                
+         scatter(ax_xyz(xplot), tt*ones(length(idx_cp_type), 1), x(idx_cp_type), ...
+                                marker_size, cmap_cp(ones(length(idx_cp_type), 1), :), ...
+                                'filled', 'MarkerFaceAlpha', marker_alpha)
+         scatter(ax_xyz(yplot), tt*ones(length(idx_cp_type), 1), y(idx_cp_type), ...
+                                marker_size, cmap_cp(ones(length(idx_cp_type), 1), :), ...
+                                'filled', 'MarkerFaceAlpha', marker_alpha)
+         scatter(ax_xyz(zplot), tt*ones(length(idx_cp_type), 1), z(idx_cp_type), ...
+                                marker_size, cmap_cp(ones(length(idx_cp_type), 1), :), ...
+                                'filled', 'MarkerFaceAlpha', marker_alpha)
+    end % scatter_plot()
+
 end
 % function plot_singularity_scatter_xyz_vs_time()
