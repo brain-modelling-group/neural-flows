59f52d2df5f408d2ae73fe22cca61abd431ec544
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-12-16 14:19:01 +1000

    Update options structure

--- tests/test_flow_estimation__planewave3d_scattered_hs3d.m
+++ tests/test_flow_estimation__planewave3d_scattered_hs3d.m
@@ -3,29 +3,36 @@ function test_flow_estimation__planewave3d_scattered_hs3d()
 % data.
 
 % Generate data
- options.hx = 2;
- options.hy = 2;
- options.hz = 2;
- options.ht = 2;
+ options.interpolation.hx = 2;
+ options.interpolation.hy = 2;
+ options.interpolation.hz = 2;
+ options.data.ht = 2;
  
  
  load('513COG.mat', 'COG')
  locs = COG(1:256, :);
  
  
- [wave3d, ~] = generate_wave3d_plane_scattered(locs, 'hxyz',  options.hx, 'ht', options.ht, 'direction', 'z');
+ [wave3d, ~] = generate_wave3d_plane_scattered(locs, 'hxyz', ...
+                                               options.interpolation.hx, 'ht', options.data.ht, ...
+                                               'direction', 'x');
  
  % Options
- options.data_interpolation.file_exists = false;
- options.flow_calculation.init_conditions = 'random';
- options.flow_calculation.seed_init_vel = 42;
- options.flow_calculation.alpha_smooth   = 0.1;
- options.flow_calculation.max_iterations = 128;
- options.sing_detection = false;
+ options.interpolation.file.exists = false;
+ options.interpolation.file.keep = false;
+ options.interpolation.boundary.thickness = 3;
+ options.data.slice.id = 0;
+ options.interpolation.boundary.alpha_radius = 30;
+ options.flows.file.keep = false;
+ options.flows.init_conditions.mode = 'random';
+ options.flows.init_conditions.seed = 42;
+ options.flows.method.alpha_smooth   = 0.1;
+ options.flows.method.max_iterations = 128;
+ options.singularity.detection.enabled = false;
 
  mfile_flows = main_neural_flows_hs3d_scatter(wave3d, locs, options);
 
- fig_hist = figure('Name', 'nflows-test-spiralwave3d-scatter-hs3d');
+ fig_hist = figure('Name', 'nflows-test-planewave3d-scatter-hs3d');
 
  subplot(1, 4, 1, 'Parent', fig_hist)
  histogram(mfile_flows.ux(2:end-1, 2:end-1, 2:end-1, :))
