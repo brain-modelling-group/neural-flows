4cd52d48a847bf4446378631b15cba3b3bc81c39
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-12-16 17:24:08 +1000

    Add example of biharmonic travelling wave

---
+++ test-data/generate_wave3d_travelling_biharmonic_scattered.m
@@ -0,0 +1,130 @@
+function [wave3d, time] = generate_wave3d_travelling_biharmonic_scattered(locs, varargin)
+% Generates a plane "travelling" wave moving along one of the three main 
+% orthogonal axes of Euclidean space. The data is generating as a linear 
+% function of space (x, Y, or Z), so in a sense is a like sinusoidal plane
+% wave of infinite (temporal) period. However, the wave3d array is a 2D
+% array 
+% ARGUMENTS: 
+%           locs      -- a 2D array of size [node/regions, 3] with the points 
+%                        in 3d used to sample the solid travelling 3d wave.
+%                       
+% (VAR)ARGUMENTS:
+%           direction -- a string with the desired wave propagation direction.
+%                        Available: {'x', 'y'}.
+%                        Default: {'x'}.
+%           step     -- a scalar with the space step size to generate the grid
+%           visual_debugging -- a boolean to determine whther to plot
+%                       figures or not.
+%                       Default: {true}.
+% 
+% OUTPUT:
+%          wave3d   -- a 2D array of size [num_regions, timepoints].
+% REQUIRES: 
+%          pcolor3() for visual debugging
+%edit
+% AUTHOR:
+%     Paula Sanz-Leon, QIMR Berghofer 2019, August 2019
+%
+% USAGE:
+%{
+
+
+
+%}
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TODO: generalise set different propogation velocities.
+% TODO: generalise for travelling waves along the other dimensions.
+
+
+tmp = strcmpi(varargin,'hxyz'); 
+if any(tmp)
+    hxyz = varargin{find(tmp)+1}; 
+else
+    hxyz = 1;
+end
+
+tmp = strcmpi(varargin,'ht'); 
+if any(tmp)
+    ht = varargin{find(tmp)+1}; 
+else
+    ht = 1;
+end
+
+tmp = strcmpi(varargin,'visual_debugging'); % note really a velocity but an integer scaling for circshift
+if any(tmp)
+    plot_stuff = varargin{find(tmp)+1}; 
+else
+    plot_stuff = true;
+end
+
+
+[X, Y, Z, ~] = get_structured_grid(locs, hxyz, hxyz, hxyz);
+
+[wave_temp, time] = genereate_fake_wave(X, Y, Z, ht);
+
+
+% Preallocate memory
+wave3d(length(time), size(locs, 1)) = 0;
+
+for tt=1:length(time)
+    B = wave_temp(:, :, :, tt);
+    data_interpolant = scatteredInterpolant(X(:), Y(:), Z(:), B(:), 'linear', 'nearest');
+    wave3d(tt, :) = data_interpolant(locs(:, 1), locs(:, 2), locs(:, 3));
+end
+
+if plot_stuff
+    plot_sphereanim(wave3d, locs*100, time);
+end
+
+
+end % function generate_wave3d_travelling_biharmonic_scattered()
+
+function [wave_temp, time] = genereate_fake_wave(X, Y, Z, ht)
+
+% Example: phase velocity slower than group velocity and in opposite
+% direction
+% Wave parameters
+fc  = 4;    % Hz
+nuc = 2;    % 1/m
+df  = 0.25; % Hz
+dnu = 0.25; % 1/m
+
+w1  = 2*pi*(fc+df);   %angular frequency rad/s
+k1x = 2*pi*(nuc-dnu);  %angular frequency wavenumber rad/m
+k1y = 2*pi*(nuc-dnu);  %angular frequency wavenumber rad/m
+w2  = 2*pi*(fc-df);   %angular frequency rad/s
+k2x = 2*pi*(nuc+dnu);  %angular frequency wavenumber rad/m
+k2y = 2*pi*(nuc+dnu);  %angular frequency wavenumber rad/m
+
+%k2 = sqrt(k2x.^2 + k2y.^2); %radial angular wavenumber rad/m
+%k1 = sqrt(k1x.^2 + k1y.^2); %radial angular wavenumber rad/m
+
+% Group velocity
+%dw = (w2-w1);
+%dk = (k2-k1);
+
+% Spatiotemporal grid 
+yy = Y(:, 1, 1);
+xx = X(1, :, 1);
+zz = Z(1,1,:);
+
+% NOTE: hardcoded size 
+time = 0:ht:5; % in seconds
+
+[x, y, t] = meshgrid(xx, yy, time);
+
+wave1 = cos(w1.*t-k1x.*y+k1y.*x);
+wave2 = cos(w2.*t-k2x.*y+k2y.*x);
+
+% Because imagscale flips the array
+%wave1 = wave1;
+%wave2 = wave2;
+wv = wave1 + wave2;
+
+wv = reshape(wv, size(wv, 1), size(wv, 2), 1, size(wv, 3));
+
+z_steps = length(zz);
+
+wave_temp = wv(:, :, ones(1, 1, z_steps, 1), :);
+
+end
---
+++ tests/test_flow_estimation__travellingbihwave3d_scattered_hs3d.m
@@ -0,0 +1,66 @@
+function test_flow_estimation__travellingbihwave3d_scattered_hs3d()
+% NOTE: Takes about 110 seconds @ dracarys
+% Generate data with these step sizes
+ options.interpolation.hx = 0.05;
+ options.interpolation.hy = 0.05;
+ options.interpolation.hz = 0.05;
+ options.data.ht = 0.05;
+ options.data.slice.id = 0;
+
+ % With these parameters the wave is moving at 4 m/s along the y-axis 
+ load('513COG.mat', 'COG')
+ locs = COG(1:128, :)/100;
+ 
+ [wave3d, ~] = generate_wave3d_travelling_biharmonic_scattered(locs, 'hxyz',  options.interpolation.hx, 'ht', options.data.ht);
+ 
+ %Option to generate a travelling wave moving back and forth along the
+ %chose axis
+ %wave3d = [wave3d(end:-1:1, :); wave3d];
+ 
+ options.interpolation.file.exists = false;
+ options.interpolation.file.keep = true;
+ options.interpolation.boundary.alpha_radius = 30;
+ options.interpolation.boundary.thickness = 2;
+ 
+ options.interpolation.hx = 2;
+ options.interpolation.hy = 2;
+ options.interpolation.hz = 2;
+ options.data.ht = 2;
+ 
+ % Flow calculation
+ options.flows.file.keep = true;
+ options.flows.init_conditions.mode = 'random';
+ options.flows.init_conditions.seed = 42;
+ options.flows.method.name = 'hs3d';
+ options.flows.method.alpha_smooth   = 1;
+ options.flows.method.max_iterations = 128;
+ 
+  % Singularity detection and classification
+ options.singularity.file.keep = false;
+ options.singularity.detection.enabled = false;    
+ 
+ % Do the stuff
+ [~, mfile_flows] = main_neural_flows_hs3d_scatter(wave3d, locs*100, options);
+
+ % Plot the velocity estimates
+ fig_hist = figure('Name', 'nflows-test-travellingwave3d-scattered-hs3d');
+
+ subplot(1, 4, 1, 'Parent', fig_hist)
+ histogram(mfile_flows.ux)
+ xlabel('ux [m/s]')
+ 
+ subplot(1, 4 ,2, 'Parent', fig_hist)
+ histogram(mfile_flows.uy)
+ xlabel('uy [m/s]')
+
+ subplot(1, 4, 3, 'Parent', fig_hist)
+ histogram(mfile_flows.uz)
+ xlabel('uz [m/s]')
+ 
+ subplot(1, 4, 4, 'Parent', fig_hist)
+ histogram(sqrt(mfile_flows.uxyz_sc(:, 1, : ).^2 + ...
+                mfile_flows.uxyz_sc(:, 2, : ).^2 + ...
+                mfile_flows.uxyz_sc(:, 3, : ).^2))
+ xlabel('u_{norm} [m/s]')
+ 
+end % function test_flow_estimation__travellingwave3d_scattered_hs3d()
