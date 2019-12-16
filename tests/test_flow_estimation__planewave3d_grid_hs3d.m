8128985781330c9eff87280f62f370c7011697f4
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-12-16 14:15:54 +1000

    Update options structure

--- tests/test_flow_estimation__planewave3d_grid_hs3d.m
+++ tests/test_flow_estimation__planewave3d_grid_hs3d.m
@@ -1,20 +1,20 @@
 function test_flow_estimation__planewave3d_grid_hs3d()
 
 % Generate data
- options.hx = 1;
- options.hy = 1;
- options.hz = 1;
- options.ht = 1;
+ options.interpolation.hx = 1;
+ options.interpolation.hy = 1;
+ options.interpolation.hz = 1;
+ options.data.ht = 1;
  [wave3d, X, Y, Z, ~] = generate_wave3d_plane_grid('visual_debugging', false, ...
-                                                   'hxyz', options.hx, 'ht', options.ht, ...
+                                                   'hxyz', options.interpolation.hx, 'ht', options.data.ht, ...
                                                    'direction', 'x');
  
- options.flow_calculation.init_conditions = 'random';
- options.flow_calculation.seed_init_vel = 42;
- options.flow_calculation.alpha_smooth   = 0.1;
- options.flow_calculation.max_iterations = 128;
+ options.flows.init_conditions.mode = 'random';
+ options.flows.init_conditions.seed = 42;
+ options.flows.method.alpha_smooth   = 0.1;
+ options.flows.method.max_iterations = 128;
 
- mfile_flows = main_neural_flows_hs3d_grid(wave3d, X, Y, Z, options);
+ mfile_flows = main_neural_flows_hs3d_grid(wave3d, Y, X, Z, options);
 
  fig_hist = figure('Name', 'nflows-test-planewave3d-grid-hs3d');
 
@@ -30,8 +30,8 @@ function test_flow_estimation__planewave3d_grid_hs3d()
  histogram(mfile_flows.uz(2:end-1, 2:end-1, 2:end-1, :))
  xlabel('uz [m/s]')
  
- subplot(1, 4, 4, 'Parent', fig_hist)
- histogram(sqrt(v.vxp(:).^2+v.vyp(:).^2+v.vzp(:).^2))
- xlabel('u_{norm} [m/s]')
+ %subplot(1, 4, 4, 'Parent', fig_hist)
+ %histogram(sqrt(v.vxp(:).^2+v.vyp(:).^2+v.vzp(:).^2))
+ %xlabel('u_{norm} [m/s]')
  
 end % function test_flow_estimation__planewave3d_grid_hs3d()
