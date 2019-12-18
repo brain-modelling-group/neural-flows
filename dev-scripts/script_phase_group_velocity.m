b8fec12396a141763ee7a1983ceac80c39cdb837
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2019-12-18 11:15:02 +1000

    Update script

--- dev-scripts/script_phase_group_velocity.m
+++ dev-scripts/script_phase_group_velocity.m
@@ -1,22 +1,26 @@
 % This script explores the differences between using the amplitude, the phase
-% and the unwrapped phase to determine the group velocity of a travelling. 
+% and the unwrapped phase to determine the group velocity of a travelling wave. 
 % PSL - Dec 2019
-% This script shows, I hope a few things: 
-% 1) that using the phase and amplitude of a (raw) signal can yield different results 
+% This script shows, I hope, a few things: 
+% 1) that using the phase and amplitude of a (raw) signal can yield different results in terms of flows 
 % 2) that group velocity and phsae velocity are/can be two different quantities
 % 3) that applying the hilbert transfrom mindlessly will get you in trouble
-% 4) that filtering the signal onyl in time does not yield a good
+% 4) that filtering the signal only in time does not yield a good
 %    representation of the actual physical wave -- by that I mean that in this
 %    example the temporal frequency of the wave (ie, amplitude modulation)
 %    is approximately 0.5 Hz, but if we calculate the power
-%    spectrum/spectrogram, we only see the individual frequencies of wave1
-%    and wave2. So when we filter we select one of those freq peaks and
+%    spectrum/spectrogram, we see the individual frequencies of wave1
+%    and wave2. So when we filter we most likely select one of those freq peaks and
 %    probably filter out the frequency of interest. 
-% 5) There is a strong argument to stop considering space and time as
-%    separable.
+% 5) that there is enough evidence now and a strong argument to stop considering space and time as
+%    separable variables.
 
-% Example: phase velocity slower than group velocity and in opposite
-% direction
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                      BIHARMONIC SURFACE WAVE                           %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% In this example the phase velocity and group velocity are in opposite
+% directions.
+
 % Wave parameters
 fc  = 4;    % Hz
 nuc = 2;    % 1/m
@@ -38,6 +42,9 @@ dw = (w2-w1);
 dk = (k2-k1);
 vg = dw / dk;
 
+% Wave velocity -- velocity of the carrier wave 1
+vp = w1 / k1;
+
 % Spatiotemporal grid 
 dx = 0.02;
 dy = 0.02;
@@ -47,10 +54,10 @@ yy = 0:dy:2;
 tt = 0:dt:5;
 [x, y, t] = meshgrid(xx, yy, tt);
 
+% Wave components
 wave1 = cos(w1.*t-k1x.*x+k1y.*y);
 wave2 = cos(w2.*t-k2x.*x+k2y.*y);
-
-% Because imagscale flips the array
+ 
 wave1 = fliplr(wave1);
 wave2 = fliplr(wave2);
 wv = wave1 + wave2;
@@ -59,22 +66,27 @@ wv = wave1 + wave2;
 [pdg_sentinel] = set_default_groot({'light', 'medium'});
 save_formats = {'tiff', 'jpeg'};
 output_path = '~/Work/Articles/neural-flows-article/figures';
+
 %% Figure 2 -- 2D+t wave
 nr = 3;
 nc = 4;
+these_units = 'centimeters';
 figure2_handle = figure('Name', 'fig2');
 % Assumes figure units are in cm
 figure2_handle.Position = [-35.0838  14.2875 28 19];
 
 for kk=1:8
    ax(kk) = subplot(nr, nc, kk);
+   ax(kk).Units = these_units;
    hold(ax(kk), 'on')
 end
 
 ax(9)  = subplot(nr, nc, 9);
+ax(9).Units = these_units;
 hold(ax(9), 'on')
 
 ax(10) = subplot(nr, nc, [10 12]);
+ax(10).Units = these_units;
 hold(ax(10), 'on')
 
 alpha_data = 0.75;
@@ -159,8 +171,6 @@ plot(ax(8), xx(x_idx), yy(y_idx), 'wo', 'markersize', 8, 'markeredgecolor', 'k',
 plot(ax(8), xx(x_idx), yy(y_idx), 'kx', 'markersize', 8)
 %% Frames - wave sum - amplitude
 vidfile = VideoWriter('wave_velocity.avi');
-phi = angle(hilbert(wv));
-
 open(vidfile);
 
 figure2a_handle = figure('Name', 'fig2a');
@@ -183,7 +193,6 @@ end
 %% Frames - wave sum - phase
 vidfile = VideoWriter('wave_velocity_phase.avi');
 phi = angle(hilbert(wv));
-
 open(vidfile);
 
 figure2b_handle = figure('Name', 'fig2b');
@@ -240,55 +249,34 @@ end
 ax(4).XLabel.String = 'y [m]';
 
 % Create lines across subplots
-annotation(figure3_handle,'line',[0.43 0.315], ...
-                                 [0.88 0.25],'LineStyle','--');
+an_h1 = annotation(figure3_handle,'line',[0, 0], [1, 1],'LineStyle','--',...
+                          'Units', these_units);
 
 % Create line
-annotation(figure3_handle,'line',[0.52 0.63],...
-                                 [0.84 0.185],'LineStyle','-.');
+an_h2 = annotation(figure3_handle,'line',[0, 0], [1, 1],'LineStyle','-.', ...
+                           'Units', these_units);
+an_h1.Position =  [4.4450    4.0217    2.6988   13.8112];                                         
+an_h2.Position =  [8.4138 5.0800 -1.2700 12.7529];                   
 
-%% Figure 4 -- neuropatt amplitude-based estimation raw signal
-wv_t = wv(1:35, 1:35, :); 
-NeuroPattGUI(wv_t, 50)
+%% Figure 4a -- neuropatt amplitude-based estimation raw signal
+wv_t = wv(25:65, 25:65, 20:end-20); 
+out_wvt = NeuroPattGUI(wv_t, 50);
 
-% Get figure handle
+% Get figure handle to SVD plots
 figure_handle = figure(1);
 ax(1) = figure_handle.Children(5);
 ax(2) = figure_handle.Children(3);
 
-ax(1).XLim = [10 30];
-ax(1).YLim = [10 30];
-ax(2).XLim = [10 30];
-ax(2).YLim = [10 30];
+% Save data
+U1(:, :, 1) =  ax(1).Children.UData;
+V1(:, :, 1) =  ax(1).Children.VData;
 
-ax(1).Children.Color =  [0.5500 0.5500 0.5500];
-ax(2).Children.Color =  [0.5500 0.5500 0.5500];
-figure_handle.Children(4).Children(2).Color = [0.5500 0.5500 0.5500];
-figure_handle.Children(1).Children(2).Color = [0.5500 0.5500 0.5500];
-%%
+U2(:, :, 1) =  ax(2).Children.UData;
+V2(:, :, 1) =  ax(2).Children.VData;
+
+%% Clear figures
 close all
-%% Figure 5 -- neuropatt amplitude-based estimation envelope of signal
-env   = abs(hilbert(wv));
-env   = env(25:65, 25:65, 20:end-20);
-
-NeuroPattGUI(env, 50)
-
-figure_handle = figure(1);
-ax(1) = figure_handle.Children(5);
-ax(2) = figure_handle.Children(3);
-
-ax(1).XLim = [10 30];
-ax(1).YLim = [10 30];
-ax(2).XLim = [10 30];
-ax(2).YLim = [10 30];
-
-ax(1).Children.Color =  [0.5500 0.5500 0.5500];
-ax(2).Children.Color =  [0.5500 0.5500 0.5500];
-figure_handle.Children(4).Children(2).Color = [0.5500 0.5500 0.5500];
-figure_handle.Children(1).Children(2).Color = [0.5500 0.5500 0.5500];
-%% 
-close all
-%% Figure 6 -- neuropatt phase-based estimation -- hilbert transform of raw signal
+%% Figure 4b -- neuropatt phase-based estimation -- hilbert transform of raw signal
 
 phi_env   = angle(hilbert(wv)); % wrapped phases
 phi_env = phi_env(25:65, 25:65, 20:end-20);
@@ -298,18 +286,35 @@ figure_handle = figure(1);
 ax(1) = figure_handle.Children(5);
 ax(2) = figure_handle.Children(3);
 
-ax(1).XLim = [10 30];
-ax(1).YLim = [10 30];
-ax(2).XLim = [10 30];
-ax(2).YLim = [10 30];
+% Save data
+U1(:, :, 2) =  ax(1).Children.UData;
+V1(:, :, 2) =  ax(1).Children.VData;
 
-ax(1).Children.Color =  [0.5500 0.5500 0.5500];
-ax(2).Children.Color =  [0.5500 0.5500 0.5500];
-figure_handle.Children(4).Children(2).Color = [0.5500 0.5500 0.5500];
-figure_handle.Children(1).Children(2).Color = [0.5500 0.5500 0.5500];
-%%
+U2(:, :, 2) =  ax(2).Children.UData;
+V2(:, :, 2) =  ax(2).Children.VData;
+%% Clear figures
 close all
-%% Figure 7 -- neuropatt phase-based estimation -- hilbert transform of envelope
+%% Figure 4c -- neuropatt amplitude-based estimation envelope of signal
+env   = abs(hilbert(wv));
+env   = env(25:65, 25:65, 20:end-20);
+
+NeuroPattGUI(env, 50)
+
+figure_handle = figure(1);
+ax(1) = figure_handle.Children(5);
+ax(2) = figure_handle.Children(3);
+
+
+% Save data
+U1(:, :, 3) =  ax(1).Children.UData;
+V1(:, :, 3) =  ax(1).Children.VData;
+
+U2(:, :, 3) =  ax(2).Children.UData;
+V2(:, :, 3) =  ax(2).Children.VData;
+
+%% Clear figures
+close all
+%% Figure 4d -- neuropatt phase-based estimation -- via hilbert transform of envelope
 env     = abs(hilbert(wv));
 phi_env = angle(hilbert(env));
 phi_env = phi_env(25:65, 25:65, 20:end-20);
@@ -319,12 +324,9 @@ figure_handle = figure(1);
 ax(1) = figure_handle.Children(5);
 ax(2) = figure_handle.Children(3);
 
-ax(1).XLim = [10 30];
-ax(1).YLim = [10 30];
-ax(2).XLim = [10 30];
-ax(2).YLim = [10 30];
+% Save data
+U1(:, :, 4) =  ax(1).Children.UData;
+V1(:, :, 4) =  ax(1).Children.VData;
 
-ax(1).Children.Color =  [0.5500 0.5500 0.5500];
-ax(2).Children.Color =  [0.5500 0.5500 0.5500];
-figure_handle.Children(4).Children(2).Color = [0.5500 0.5500 0.5500];
-figure_handle.Children(1).Children(2).Color = [0.5500 0.5500 0.5500];
+U2(:, :, 4) =  ax(2).Children.UData;
+V2(:, :, 4) =  ax(2).Children.VData;
