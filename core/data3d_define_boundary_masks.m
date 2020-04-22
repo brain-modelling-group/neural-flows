c65258e9140ee1aeede07294450cb5e5acab6d38
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-04-22 20:07:18 +1000

    Update function that calculates boundary masks

--- core/data3d_define_boundary_masks.m
+++ core/data3d_define_boundary_masks.m
@@ -1,23 +1,23 @@
-function [params] = data3d_define_boundary_masks()
+function [masks, params] = data3d_define_boundary_masks(locs, X, Y, Z, params)
     % Alpha radius has to be adjusted depending on the location data
     % (mostly, granularity of parcellation).
     if isfield(inparams.interpolation.boundary, 'alpha_radius')
         bdy_alpha_radius = params.interpolation.boundary.alpha_radius;
-
-%-------------------------- GRID AND MASK -------------------------------------%    
-
+    end
     
     % Get a mask with the gridded-points that are inside and outside the convex hull
     % NOTE: This mask underestimates the volume occupied by the scattered
     % points
-    [in_bdy_mask, ~] = data3d_calculate_boundary(locs, X(:), Y(:), Z(:), bdy_alpha_radius);
-    %[in_bdy_mask] = data3d_check_boundary_mask(in_bdy_mask, locs, hr);
+    [mask_innies, ~] = data3d_calculate_boundary(locs, X(:), Y(:), Z(:), bdy_alpha_radius);
 
-    in_bdy_mask = reshape(in_bdy_mask, params.interpolation.data.grid_size);
+    mask_innies = reshape(mask_innies, params.interpolation.data.shape.grid);
     % Get a mask that is slightly larger so we can define a shell with a thickness that will be 
     % the boundary of our domain. 
-    mask_thickness = params.interpolation.boundary.thickness;
-    [interp_mask, diff_mask] = data3d_calculate_interpolation_mask(in_bdy_mask, ...
-                                                                   params.interpolation.boundary.thickness);
+    [mask_outties, mask_betweenies] = data3d_calculate_interpolation_mask(mask_innies, ...
+                                                                          params.interpolation.boundary.thickness);
+
+    masks.innies = mask_innies;
+    masks.outties = mask_outties;
+    masks.mask_betweenies = mask_betweenies;
 
 end % function data3d_define_boundary_masks()
