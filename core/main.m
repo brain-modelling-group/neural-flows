661437f526d2ca46fc8d51be091a6920f60c2113
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-04-22 17:10:17 +1000

    Use copy of input parameters

--- core/main.m
+++ core/main.m
@@ -4,21 +4,24 @@ function ouparams = main(inparams)
 % Tic
 tstart = tik();
 
+% Copy structure
+tmp_params = inparams;
+
 % Check if we need to interpolate data
-if strcmp(inparams.data.grid.type, 'unstructured') && inparams.interpolation.enabled
-   % Do the interpolation  
-   data3d_interpolate()
-elseif strcmp(inparams.data.grid.type, 'structured') && inparams.interpolation.enabled
+if strcmp(tmp_params.data.grid.type, 'structured') && tmp_params.interpolation.enabled
    error(['neural-flows:' mfilename ':IncompatibleOptions'], 'Will not perform interpolation on gridded/structured data.');
-elseif strcmp(inparams.data.grid.type, 'unstructured') && ~inparams.interpolation.enabled
+elseif strcmp(tmp_paramss.data.grid.type, 'unstructured') && ~tmp_params.interpolation.enabled
    warning(['neural-flows:' mfilename ':NoInterpolation'], 'Skipping interpolation ...')
+elseif strcmp(tmp_params.data.grid.type, 'unstructured') && tmp_params.interpolation.enabled
+   % Do the interpolation  
+   tmp_params = data3d_interpolate(tmp_params)
 end
 
 % Check which method we want to use - hsd3 only works with interpolate data, while cnem
-switch inparams.flows.method.name
+switch tmp_params.flows.method.name
     case {'hs3d', 'horn-schunk', 'hs'}
         %
-        tmp_params = flows3d_estimate_hs3d(inparams);
+        tmp_params = flows3d_estimate_hs3d(tmp_params);
     case {'cnem'}
         % temp_params = flows3d_estimate_cnem(inparams)
         error(['neural-flows:' mfilename ':NotTested'], ...
@@ -29,17 +32,17 @@ switch inparams.flows.method.name
 end
 
 % Check what else we want to do
-if inparams.flows.streamlines.enabled
+if tmp_params.flows.streamlines.enabled
     % Do x
     %main_streamlines()
 end 
 
-if inparams.singularity.detection.enabled 
-    tmp_params = singularity3d_detect(temp_params)
+if tmp_params.singularity.detection.enabled 
+    tmp_params = singularity3d_detect(tmp_params)
 end
 
-if inparams.singularity.detection.enabled 
-    tmp_params = singularity3d_detect(temp_params)
+if tmp_params.singularity.detection.enabled 
+    tmp_params = singularity3d_detect(tmp_params)
 end
 
 ouparams = tmp_params;
@@ -50,8 +53,8 @@ tok(tstart, 'minutes')
 tok(tstart, 'hours')
 
 % Save parameter structure with updated fields and values
-read_write_json(inparams.general.storage.params.output.filename, ...
-                inparams.general.storage.params.output.dir, ...
+read_write_json(tmp_params.general.storage.params.output.filename, ...
+                tmp_params.general.storage.params.output.dir, ...
                 'write', ...
                 ouparams)
 
