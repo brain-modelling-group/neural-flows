332a3407de6b2cde2caaebd9b1d2ec62edf6b2de
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-04-23 15:34:29 +1000

    Clean up

--- analysis/calculate_energy_grid.m
+++ analysis/calculate_energy_grid.m
@@ -1,17 +1,14 @@
-function [energy] = calculate_energy_grid(mflow_obj)
+function [energy] = calculate_energy_grid(obj_flows)
 %Computation of kinetic energy. The kinetic energy is similar to the norm of the
 % vector field, but it emphasizes low ( val < 1) vs high energy states (val > 1)
 % ARGUMENTS:
-%  mflow_obj         -- a Matfile handle or structure with similar fields
+%  obj_flows         -- a Matfile handle or structure with similar fields
 % 
 % OUTPUT:
-%
-%   energy.node       -- Displacement energy in the flow field,
-%                             returns energy for node    
-% 
-%   energy.sum        -- Displacement energy in the flow,
+%% 
+%   energy.spatial_sum        -- Displacement energy in the flow,
 %                             summed over space. 
-%   energy.av         -- Average displacement enegy
+%   energy.spatial_average    -- Average displacement enegy
 % REQUIRES: 
 %
 % USAGE:
@@ -27,22 +24,24 @@ function [energy] = calculate_energy_grid(mflow_obj)
 
 % NOTE: Displacement energy, this is a kinectic energy-like expression: 1/2 m v^2
 % The mass is m=1
- 
-tpts = size(mflow_obj, 'ux', 4);
+
+params = obj_flows.params; 
+tpts   = params.flows.data.shape.t;
+
 
 % Probably best to iterate over time
-
-temp_energy(length(find(mflow_obj.in_bdy_mask)), tpts) = 0;
-good_idx = find(mflow_obj.interp_mask == true);
+temp_energy(length(find(obj_flows.in_bdy_mask)), tpts) = 0;
+good_idx = find(obj_flows.interp_mask == true);
 parfor tt=1:tpts
-    ux = mflow_obj.ux(:, :, :, tt);
-    uy = mflow_obj.uy(:, :, :, tt);
-    uz = mflow_obj.uz(:, :, :, tt);
+    ux = obj_flows.ux(:, :, :, tt);
+    uy = obj_flows.uy(:, :, :, tt);
+    uz = obj_flows.uz(:, :, :, tt);
     temp_energy(:, tt) = 0.5*(ux(good_idx).^2 + uy(good_idx).^2 + uz(good_idx).^2);
 end
 % 
-energy.sum = nansum(temp_energy);
+energy.nodal = 
+energy.spatial_sum = nansum(temp_energy);
 
-energy.av = nanmean(temp_energy);
+energy.spatial_average = nanmean(temp_energy);
 
 end % end calculate_energy_grid()
