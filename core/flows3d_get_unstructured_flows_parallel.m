6cc5d5a10fc9da2e12e95cd42f52717902931c90
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-04-23 11:55:17 +1000

    Clean up function

--- core/flows3d_get_unstructured_flows_parallel.m
+++ core/flows3d_get_unstructured_flows_parallel.m
@@ -1,4 +1,4 @@
-function [obj_flows] = flows3d_get_unstructured_flows_parallel(obj_flows, locs)
+function [obj_flows] = flows3d_get_unstructured_flows_parallel(obj_flows, locs, params)
 % This is a wrapper function for Matlab's ScatteredInterpolant, to obtain the 
 % flows values at exactly the centres of gravity of the original brain regions, so as to 
 % minimise the errors introduce by different grid resolutions.
@@ -19,69 +19,72 @@ function [obj_flows] = flows3d_get_unstructured_flows_parallel(obj_flows, locs)
 %     Paula Sanz-Leon, QIMR Berghofer, October 2019
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
 
+if nargin < 3
+    x_dim_locs = 1;
+    y_dim_locs = 2;
+    z_dim_locs = 3;
+else
+    x_dim_locs = params.data.x_dim_locs;
+    y_dim_locs = params.data.y_dim_locs;
+    z_dim_locs = params.data.z_dim_locs;
+end
+
     %These parameter values are essential
     neighbour_method = 'linear';
     extrapolation_method = 'linear'; 
     
-    x_dim = 1;
-    y_dim = 2;
-    z_dim = 3;
-    tpts = size(obj_flows, 'ux', 4);
+    tpts = params.flows.data.shape.t;
     
     if tpts < 2
-        disp('NOTE to self: This will fail because there is only one data point')
+        disp('NOTE to self: This will fail because there is only one data timepoint')
     end
     X = obj_flows.X;
     Y = obj_flows.Y;
     Z = obj_flows.Z;
     
-    in_bdy_mask_idx = find(obj_flows.in_bdy_mask == true);
+    masks = obj_flows.masks; 
+    innies_idx = find(masks.innies == true);
+    delete(masks)
     
-    
-    X = X(in_bdy_mask_idx);
-    Y = Y(in_bdy_mask_idx);
-    Z = Z(in_bdy_mask_idx);
+    X = X(innies_idx);
+    Y = Y(innies_idx);
+    Z = Z(innies_idx);
 
    
     % Write dummy data to disk
-    obj_flows.uxyz_sc  = zeros([size(locs), tpts]);     
-
-    %Open a pallell pool using all available workers
-    %percentage_of_workers = 0.5; % 1 --> all workers, too agressive
-    %open_parpool(percentage_of_workers);
-    
+    obj_flows.uxyz  = zeros([size(locs), tpts]);     
   
-    interpolation_3d_storage_expression = 'uxyz_sc(:, :, jdx)';
+    interpolation_3d_storage_expression = 'uxyz(:, :, jdx)';
     %spmd_parfor_with_matfiles(number_of_things, parfun, temp_fname_obj, storage_expression)
-    parfun = @interpolate_step;
+    parfun = @interpolate_flow_step;
     [obj_flows] = spmd_parfor_with_matfiles(tpts, parfun, obj_flows, interpolation_3d_storage_expression);
     
     % Child function with access to local scope variables from parent
     % function
-    function temp_data = interpolate_step(idx)
+    function temp_data = interpolate_flow_step(idx)
             % Create nan array so the output is in the right shape
             ux = obj_flows.ux(:, :, :, idx);
             uy = obj_flows.uy(:, :, :, idx);
             uz = obj_flows.uz(:, :, :, idx);
             
             data_interpolant_x = scatteredInterpolant(X, Y, Z , ...
-                                                    ux(in_bdy_mask_idx), ...
-                                                    neighbour_method, ...
-                                                    extrapolation_method);
+                                                      ux(innies_idx), ...
+                                                      neighbour_method, ...
+                                                      extrapolation_method);
             data_interpolant_y = scatteredInterpolant(X, Y, Z , ...
-                                                    uy(in_bdy_mask_idx), ...
-                                                    neighbour_method, ...
-                                                    extrapolation_method);
+                                                      uy(innies_idx), ...
+                                                      neighbour_method, ...
+                                                      extrapolation_method);
                                                 
             data_interpolant_z = scatteredInterpolant(X, Y, Z , ...
-                                                    uz(in_bdy_mask_idx), ...
-                                                    neighbour_method, ...
-                                                    extrapolation_method);
+                                                      uz(innies_idx), ...
+                                                      neighbour_method, ...
+                                                      extrapolation_method);
                                                 
 
-            temp_data = single([data_interpolant_x(locs(:, x_dim), locs(:, y_dim), locs(:, z_dim)), ...
-                                data_interpolant_y(locs(:, x_dim), locs(:, y_dim), locs(:, z_dim)), ...
-                                data_interpolant_z(locs(:, x_dim), locs(:, y_dim), locs(:, z_dim))]);
+            temp_data = single([data_interpolant_x(locs(:, x_dim_locs), locs(:, y_dim_locs), locs(:, z_dim_locs)), ...
+                                data_interpolant_y(locs(:, x_dim_locs), locs(:, y_dim_locs), locs(:, z_dim_locs)), ...
+                                data_interpolant_z(locs(:, x_dim_locs), locs(:, y_dim_locs), locs(:, z_dim_locs))]);
     end
 
-end %flows3d_get_scattered_flows_parallel()
+end %flows3d_get_unstructured_flows_parallel()
