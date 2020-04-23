e4841e83d0dcec901d0584d56393a7fc3c8a49b6
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-04-23 12:25:04 +1000

    Update name

--- core/data3d_define_boundary_masks.m
+++ core/data3d_define_boundary_masks.m
@@ -1,4 +1,4 @@
-function [masks, params] = data3d_define_boundary_masks(locs, X, Y, Z, params)
+function [masks, params] = data3d_calculate_boundary_masks(locs, X, Y, Z, params)
     % Alpha radius has to be adjusted depending on the location data
     % (mostly, granularity of parcellation).
     if isfield(inparams.interpolation.boundary, 'alpha_radius')
@@ -8,12 +8,12 @@ function [masks, params] = data3d_define_boundary_masks(locs, X, Y, Z, params)
     % Get a mask with the gridded-points that are inside and outside the convex hull
     % NOTE: This mask underestimates the volume occupied by the scattered
     % points
-    [mask_innies, ~] = data3d_calculate_boundary(locs, X(:), Y(:), Z(:), bdy_alpha_radius);
+    [mask_innies, ~] = data3d_boundary_mask_inner(locs, X(:), Y(:), Z(:), bdy_alpha_radius);
 
     mask_innies = reshape(mask_innies, params.interpolation.data.shape.grid);
     % Get a mask that is slightly larger so we can define a shell with a thickness that will be 
     % the boundary of our domain. 
-    [mask_outties, mask_betweenies] = data3d_calculate_interpolation_mask(mask_innies, ...
+    [mask_outties, mask_betweenies] = data3d_boundary_mask_outter_between(mask_innies, ...
                                                                           params.interpolation.boundary.thickness);
 
     masks.innies = mask_innies;
