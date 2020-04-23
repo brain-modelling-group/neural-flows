255146d0b765d7e894fee7fb31e108941f2804a3
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-04-23 12:37:44 +1000

    Handle which dataset to load

---
+++ utils/load_iomat_data.m
@@ -0,0 +1,18 @@
+function obj_data = load_io_mat_file(params)
+% Load interpolated data iomat or put regular data into an appropriate structure?
+% or make a temporary iomatfile??
+
+ % Load interpolated data
+    if strcmp(params.data.grid.type, 'unstructured')
+        % Load data file
+        obj_data = matfile(fullfile(params.interpolation.file.dir, ...
+                                    params.interpolation.file.name), ...
+                                    'Writable', false);
+    else
+        obj_data = matfile(fullfile(params.data.file.dir, ...
+                                    params.data.file.name), ...
+                                    'Writable', true);
+    end
+
+
+end % function load_iomat_file()
