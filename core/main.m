32ad8470e4e020a6023cf1e18a681821f08f521d
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-04-24 11:19:02 +1000

    Debug: typo

--- core/main.m
+++ core/main.m
@@ -40,7 +40,7 @@ end
 switch tmp_params.flows.method.name
     case {'hs3d', 'horn-schunk', 'hs'}
         %
-        [tmp_params, obj_flows, obj_flows_sentinel] = flows3d_estimate_hs3d(tmp_params, masks);
+        [tmp_params, obj_flows, obj_flows_sentinel] = flows3d_hs3d_estimate(tmp_params, masks);
     case {'cnem'}
         %[tmp_params, obj_flows, obj_flows_sentinel] = flows3d_estimate_cnem_hs(tmp_params, masks);
         % 
