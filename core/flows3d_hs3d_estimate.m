fd5d403750e2edf3a923d7428f0b743341058b25
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-04-25 21:46:58 +1000

    Save locs in obj_flows too

--- core/flows3d_hs3d_estimate.m
+++ core/flows3d_hs3d_estimate.m
@@ -49,6 +49,7 @@ function  [params, obj_flows, obj_flows_sentinel] = flows3d_hs3d_estimate(params
     obj_flows.X = obj_data.X;
     obj_flows.Y = obj_data.Y;
     obj_flows.Z = obj_data.Z;
+    obj_flows.locs = obj_data.locs;
    
     % Here is where the magic happens
     params = flows3d_hs3d_loop(obj_data, obj_flows, params);
