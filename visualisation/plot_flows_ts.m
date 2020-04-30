8f72a43b1ed65ef812a46bd46261f8eca219aff2
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-04-30 21:53:46 +1000

    Update variable names

--- visualisation/plot_flows_ts.m
+++ visualisation/plot_flows_ts.m
@@ -1,19 +1,21 @@
-function ax_handle = plot_flows_ts(mflow_obj, ax_handle)
+function ax_handle = plot3d_flows_ts(obj_flows, ax_handle)
 
 if nargin < 2
     ax_handle = axes;
 end
 
-num_voxels = 1004186;
+num_points = obj_flows.num_innies; 
+figure_handle = figure('Name', 'nflows-flows-average-over-time', varargin{:});
+
 
 hold(ax_handle, 'on')
 
-plot3(ax_handle, mflow_obj.sum_ux(1,1)/num_voxels, mflow_obj.sum_uy(1,1)/num_voxels, mflow_obj.sum_uz(1,1)/num_voxels, ...
+plot3(ax_handle, obj_flows.sum_ux(1,1)/num_points, obj_flows.sum_uy(1,1)/num_points, obj_flows.sum_uz(1,1)/num_points, ...
                  '.', 'markerfacecolor', 'b', 'markersize', 20)
-plot3(ax_handle, mflow_obj.sum_ux(1,end)/num_voxels, mflow_obj.sum_uy(1,end)/num_voxels, mflow_obj.sum_uz(1,end)/num_voxels, ...
+plot3(ax_handle, obj_flows.sum_ux(1,end)/num_points, obj_flows.sum_uy(1,end)/num_points, obj_flows.sum_uz(1,end)/num_points, ...
                  '.', 'markerfacecolor', 'r', 'markersize', 20)
 
-plot3(ax_handle, mflow_obj.sum_ux/num_voxels, mflow_obj.sum_uy/num_voxels, mflow_obj.sum_uz/num_voxels)
+plot3(ax_handle, obj_flows.sum_ux/num_points, obj_flows.sum_uy/num_points, obj_flows.sum_uz/num_points)
 
 
-end
+end % function plot3d_flows_ts()
