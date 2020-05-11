698771d3df0cac10fd09db7eb1de48823dcaeba7
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-05-11 18:54:58 +1000

    Refactor: use json file

--- tests/test_flow_estimation__planewave3d_grid_hs3d.m
+++ tests/test_flow_estimation__planewave3d_grid_hs3d.m
@@ -1,37 +1,60 @@
 function test_flow_estimation__planewave3d_grid_hs3d()
 
-% Generate data
- options.interpolation.hx = 1;
- options.interpolation.hy = 1;
- options.interpolation.hz = 1;
- options.data.ht = 1;
- [wave3d, X, Y, Z, ~] = generate_wave3d_plane_grid('visual_debugging', false, ...
-                                                   'hxyz', options.interpolation.hx, 'ht', options.data.ht, ...
-                                                   'direction', 'x');
- 
- options.flows.init_conditions.mode = 'random';
- options.flows.init_conditions.seed = 42;
- options.flows.method.alpha_smooth   = 0.1;
- options.flows.method.max_iterations = 128;
 
- mfile_flows = main_neural_flows_hs3d_grid(wave3d, Y, X, Z, options);
+ % Generate data
+ [data, X, Y, Z, ~] = generate_wave3d_plane_grid('visual_debugging', true, ...
+                                                 'hxyz', 1, 'ht', 1, ...
+                                                 'direction', 'radial');
+ % Save data
+ hx = 1;
+ hy = 1;
+ hz = 1;
+ ht = 1;
+ obj_data = matfile('test_flow_estimation__planewave3d_grid', 'Writable', true);
+ obj_data.data = data;
+ obj_data.X = X;
+ obj_data.Y = Y;
+ obj_data.Z = Z;
+ 
+ obj_data.hx = hx;
+ obj_data.hy = hy;
+ obj_data.hz = hz;
+ obj_data.ht = ht;
+ 
+%
+input_params_filename = 'test-flow-planewave3d-grid-in.json';
+input_params_dir  = '/home/paula/Work/Code/matlab-neuro/neural-flows/tests/generate';
+json_mode = 'read';
 
- fig_hist = figure('Name', 'nflows-test-planewave3d-grid-hs3d');
+% Load options -- TODO: the use of load_data is kind of a hack. The toolbox
+% should make a new matfile for the data. Data should be stored in a basic
+% matfile.
+input_params = read_write_json(input_params_filename, input_params_dir, json_mode);
+[~, input_params] = load_data(input_params);
+%%
+output_params = main(input_params); 
 
- subplot(1, 4, 1, 'Parent', fig_hist)
- histogram(mfile_flows.ux(2:end-1, 2:end-1, 2:end-1, :))
- xlabel('ux [m/s]')
+perform_svd_mode_decomposition(output_params);
+
+[obj_flows] = load_iomat_flows(output_params);
+
+% Plot stuff
+fig_hist = figure('Name', 'nflows-test-planewave3d-grid-hs3d');
+
+subplot(1, 4, 1, 'Parent', fig_hist)
+histogram(obj_flows.ux(2:end-1, 2:end-1, 2:end-1, :))
+xlabel('ux [m/s]')
  
- subplot(1, 4, 2, 'Parent', fig_hist)
- histogram(mfile_flows.uy(2:end-1, 2:end-1, 2:end-1, :))
- xlabel('uy [m/s]')
+subplot(1, 4, 2, 'Parent', fig_hist)
+histogram(obj_flows.uy(2:end-1, 2:end-1, 2:end-1, :))
+xlabel('uy [m/s]')
  
- subplot(1, 4, 3, 'Parent', fig_hist)
- histogram(mfile_flows.uz(2:end-1, 2:end-1, 2:end-1, :))
- xlabel('uz [m/s]')
+subplot(1, 4, 3, 'Parent', fig_hist)
+histogram(obj_flows.uz(2:end-1, 2:end-1, 2:end-1, :))
+xlabel('uz [m/s]')
  
- %subplot(1, 4, 4, 'Parent', fig_hist)
- %histogram(sqrt(v.vxp(:).^2+v.vyp(:).^2+v.vzp(:).^2))
- %xlabel('u_{norm} [m/s]')
+subplot(1, 4, 4, 'Parent', fig_hist)
+histogram(obj_flows.un(2:end-1, 2:end-1, 2:end-1, :))
+xlabel('u_{norm} [m/s]')
  
 end % function test_flow_estimation__planewave3d_grid_hs3d()
