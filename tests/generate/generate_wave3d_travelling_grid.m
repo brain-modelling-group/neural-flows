22033c2871c8f0273489cc1917595945c2998913
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-05-14 16:17:16 +1000

    Reafactor: unify generation of structured and unstructured

--- tests/generate/generate_wave3d_travelling_grid.m
+++ tests/generate/generate_wave3d_travelling_grid.m
@@ -1,4 +1,4 @@
-function [data, X, Y, Z, time] = generate_wave3d_travelling_grid(varargin)
+function [data, X, Y, Z, time] = generate_data3d_travelling_wave(varargin)
 % Generates a plane "travelling" wave moving along one of the three main 
 % orthogonal axes of Euclidean space. The data is generating as a linear 
 % function of space (x, Y, or Z), so in a sense is a like sinusoidal plane
@@ -15,7 +15,7 @@ function [data, X, Y, Z, time] = generate_wave3d_travelling_grid(varargin)
 %                       Default: {true}.
 % 
 % OUTPUT:
-%           wave3d   -- a 4D array of size [21, 21, 21, 22]. The last
+%           data   -- a 4D array of size [21, 21, 21, 22]. The last
 %                       dimension is time, and the first three dimensions are space.
 %                       This wave moves at exaclty 1 m/s.
 % REQUIRES: 
@@ -58,7 +58,6 @@ else
     ht = 1;
 end
 
-
 tmp = strcmpi(varargin,'grid_type'); 
 if any(tmp)
     grid_type = varargin{find(tmp)+1}; 
@@ -80,39 +79,19 @@ else
     plot_stuff = true;
 end
 
-% 
-max_val_x = 10;
-max_val_x1 = 10*max_val_x;
-x = -max_val_x:hxyz:max_val_x;
-len_x = length(x);
-x1 = -max_val_x1:hxyz:max_val_x1;
-len_x1 = length(x1);
-
-[X, ~, ~] = meshgrid(x1, x, x); % in metres
 
 % NOTE: hardcoded size 
 time = 0:ht:21; % in seconds
 
-A = -X;
-% Preallocate memory
-data(len_x, len_x, len_x, length(time)) = 0;
-
-idx_start = ceil(len_x1/2);
-idx_end   = idx_start+len_x-1;
-for tt=1:length(time)
-    B = circshift(A, velocity*tt, 2);
-    data(:, :, :, tt) = B(:, idx_start:idx_end, :);
-end
-
-[X, Y, Z] = meshgrid(x, x, x); % in metres
-
-switch direction
-    case 'y'
-        data = permute(data, [2 1 3 4]);        
-    case 'z'
-        data = permute(data, [3 1 2 4]);        
+switch grid_type
+    case {'unstructured', 'scattered', 'nodal'}
+        [data, X, Y, Z] = get_unstructured_data();
+    case {'structured', 'grid', 'voxel'}
+        [data, X, Y, Z] = get_structured_data();
     otherwise
+        
 end
+
 
 if plot_stuff 
     fig_handle = figure('Name', 'nflows-data3-travelling-wave-space');
@@ -123,7 +102,117 @@ if plot_stuff
         plot3d_debug_data_frame(fig_handle, data, X, Y, Z, time)
     otherwise
          error(['neural-flows:' mfilename ':UnknownCase'], ...
-                   'Requested unknown case of grid. Options: {"structured", "unstructured"}'); 
+                'Requested unknown case of grid. Options: {"structured", "unstructured"}'); 
     end
 end
+
+
+function [data, X, Y, Z] = get_structured_data()
+    max_val_x = 10;
+    max_val_x1 = 10*max_val_x;
+    x = -max_val_x:hxyz:max_val_x;
+    len_x = length(x);
+    x1 = -max_val_x1:hxyz:max_val_x1;
+    len_x1 = length(x1);
+
+    [X, ~, ~] = meshgrid(x1, x, x); % in metres
+
+    A = -X;
+    % Preallocate memory
+    data(len_x, len_x, len_x, length(time)) = 0;
+
+    idx_start = ceil(len_x1/2);
+    idx_end   = idx_start+len_x-1;
+    for tt=1:length(time)
+        B = circshift(A, velocity*tt, 2);
+        data(:, :, :, tt) = B(:, idx_start:idx_end, :);
+    end
+
+    [X, Y, Z] = meshgrid(x, x, x); % in metres
+
+    switch direction
+        case 'y'
+            data = permute(data, [2 1 3 4]);        
+        case 'z'
+            data = permute(data, [3 1 2 4]);        
+        otherwise
+    end
+end % function get_structured_data()
+
+
+function [data, X, Y, Z] = get_unstructured_data()
+    [XX, YY, ZZ, ~] = get_structured_grid(locs, hxyz, hxyz, hxyz);
+
+    % NOTE: hardcoded size 
+    time = 0:ht:10; % in seconds
+
+    % Preallocate memory
+    data(length(time), size(locs, 1)) = 0;
+
+    switch direction
+        case 'x'
+            
+          y = min(YY(:)):hxyz:max(YY(:));
+          z = min(ZZ(:)):hxyz:max(ZZ(:));
+          offset = 25;
+          x1 = (min(X(:))-2*offset):hxyz:max(X(:));
+          [X1, ~, ~] = meshgrid(x1, y, z);
+          circshift_dim = 2;
+          A = XX1;
+          % X
+          idx_start = offset;
+          idx_end   = offset+size(XX, 2)-1;
+          % Y
+          idy_start = 1;
+          idy_end = length(y);
+          % Z
+          idz_start = 1;
+          idz_end = length(z);
+          
+        case 'y'
+          x = min(XX(:)):hxyz:max(XX(:));
+          z = min(ZZ(:)):hxyz:max(ZZ(:));
+          offset = 25;
+          y1 = (min(Y(:))-2*offset):hxyz:max(Y(:));
+          [~, Y1, ~] = meshgrid(x, y1, z);
+          circshift_dim = 1;
+          A = Y1;
+          idx_start = 1;
+          idx_end = length(x);
+          idy_start = offset;
+          idy_end   = offset+size(X, 1)-1;
+          idz_start = 1;wave3d(length(time), size(locs, 1)) = 0;
+
+          idz_end = length(z);
+        case 'z'
+          x = min(X(:)):hxyz:max(X(:));
+          y = min(Y(:)):hxyz:max(Y(:));
+          offset = 25;
+          z1 = (min(Z(:))-2*offset):hxyz:max(Z(:));
+          [~, ~, Z1] = meshgrid(x, y, z1);
+          circshift_dim = 3;
+          A = Z1;
+          idx_start = 1;
+          idx_end = length(x);
+          idy_start = 1;
+          idy_end = length(y);
+          idz_start = offset;
+          idz_end   = offset+size(X, 3)-1;
+    end
+
+    for tt=1:length(time)
+        B = circshift(A, velocity*tt, circshift_dim);
+        B = B(idy_start:idy_end, idx_start:idx_end, idz_start:idz_end);
+        % This step is super slow -- so do not run this example for long
+        % timeseries.
+        data_interpolant = scatteredInterpolant(X(:), Y(:), Z(:), B(:), 'linear', 'none');
+        data(tt, :) = data_interpolant(locs(:, 1), locs(:, 2), locs(:, 3));
+    end
+
+    X = locs(:, 1);
+    Y = locs(:, 2);
+    Z = locs(:, 3);
+
+end % function get_unstructured_data()
+
 end % function generate_travellingwave3d_grid()
