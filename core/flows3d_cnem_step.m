5ed6a6305e9ee30d470d620d8368ae8b21179dd6
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-05-15 23:17:36 +1000

    Fix problem with function signature call

--- core/flows3d_cnem_step.m
+++ core/flows3d_cnem_step.m
@@ -1,4 +1,4 @@
-function [ux, uy, uz] = flows3d_cnem_step(F1, F2, alpha_smooth, max_iterations, ux, uy, uz, neighbours_matrix, cnem_B)
+function [ux, uy, uz] = flows3d_cnem_step(F1, F2, alpha_smooth, max_iterations, ux, uy, uz, ht, neighbours_matrix, cnem_B)
 %% NOTE: NOT WORKING ATM --- This function estimates the velocity components between two subsequent 3D 
 % images using the Horn-Schunck optical flow method (HS), but using CNEM for 
 % gradients.
@@ -14,7 +14,7 @@ function [ux, uy, uz] = flows3d_cnem_step(F1, F2, alpha_smooth, max_iterations, 
 %                 3 orthogonal axes. 
 %neighbours_matrix
 % AUTHOR:
-%     Paula Sanz-Leon, QIMR Berghofer September 2019
+%     Paula Sanz-Leon, QIMR Berghofer April 2020
 % USAGE:
 %{
     
@@ -22,23 +22,14 @@ function [ux, uy, uz] = flows3d_cnem_step(F1, F2, alpha_smooth, max_iterations, 
 %   REFERENCES:
 %   B. K. Horn and B. G. Schunck, Determining optical flow,
 %   USA, Tech. Rep., 1980.
-%   
+   
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%           
-if nargin < 3
-    alpha_smooth  = 1; 
-    max_iterations = 32;    
-elseif nargin < 4
-    max_iterations = 32;
-    
-end
-
 
 % Calculate derivatives -- hx, hy, hz not used 
 [Ix, Iy, Iz, It] = flows3d_cnem_calculate_partial_derivatives(F1, F2, ht, cnem_B);
 
 % one_ring_neighbours_matrix = nodes x 6 or 5
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% FIND A WAY TO CALCULATE AVERAGE OF NEIGHBOURS
 
     for tt=1:max_iterations
         ux_avg = calculate_local_average(ux, neighbours_matrix);
