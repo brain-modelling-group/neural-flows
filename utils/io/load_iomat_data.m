9cc4119292e4446edc8fd2216629d22422a8eb67
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-05-15 15:55:22 +1000

    Recycle function to load only original data in matfiles

--- utils/io/load_iomat_data.m
+++ utils/io/load_iomat_data.m
@@ -1,21 +1,8 @@
 function [obj_data, obj_data_sentinel] = load_iomat_data(params)
-% Load interpolated data iomat or put regular data into an appropriate structure?
-% or make a temporary iomatfile??
-% TODO: make a sentinel, delete object handle or file? 
-% Delete handle for interpolated data file but delete file for noninterpolated data
+% Load ioriginal data stored in an iomat file 
 
- % Load interpolated data
-    if strcmp(params.data.grid.type, 'unstructured')
-        % Load file with interpolated datafile
-        obj_data = matfile(fullfile(params.interpolation.file.dir, ...
-                                    params.interpolation.file.name), ...
-                                    'Writable', false);
-    else if strcmp(params.data.grid.type, 'structured')
-        % NOTE: NOT IMPLEMENTED YET
-        obj_data = matfile(fullfile(params.data.file.dir, ...
-                                    params.data.file.name), ...
-                                    'Writable', true);
-    end
-
+obj_data = matfile(fullfile(params.data.file.dir, ...
+                            params.data.file.name), ...
+                            'Writable', true);
 
 end % function load_iomat_data()
