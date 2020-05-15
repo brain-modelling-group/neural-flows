be3aea9ed9342942dcc19fd12c325d91296cc913
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-05-15 15:54:43 +1000

    Load iomat with interpolated data

---
+++ utils/io/load_iomat_interp.m
@@ -0,0 +1,11 @@
+function [obj_data, obj_data_sentinel] = load_iomat_interp(params)
+% Load interpolated data iomat 
+
+% Load file with interpolated datafile
+ obj_data = matfile(fullfile(params.interpolation.file.dir, ...
+                             params.interpolation.file.name), ...
+                             'Writable', false);
+
+
+
+end % function load_iomat_data()
