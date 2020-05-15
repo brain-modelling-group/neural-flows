ea11e8976457bec04a7151512e79b5441f17a2d0
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-05-15 18:53:53 +1000

    Draft function to prepate input data for cnem flows - convex hull of each hemisphere or both together

---
+++ utils/calc/get_boundary_mask.m
@@ -0,0 +1,45 @@
+function get_boundary_masks(params, obj_data, left_idx, right_idx)
+%% Returns a local patch of surface of the neighbourhood around a vertex.
+%
+% ARGUMENTS: params    -- almighty structure
+%            obj_data  -- handle to matfile with original unstructured data
+%            left_idx  -- indices of nodes on the left hemisphere
+%            right_idx -- indices of nodes on the right hemisphere
+%
+% OUTPUT: 
+%         None, saves automagically to obj_data    
+%         
+% REQUIRES: 
+%         
+% USAGE:
+%{
+     
+%}
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Calculate the convex hull of scattered points for each hemisphere separately and both
+
+% Calculate individual convex hulls
+
+
+num_nodes_left = length(left_idx);
+num_nodes_right = length(right_idx);
+
+tri_left  = get_boundary_triangles(obj_data.locs(left_idx, :), params.data.boundary.alpha_radius);
+tri_right = get_boundary_triangles(obj_data.locs(right_idx, :), params.data.boundary.alpha_radius);
+tri_right = tri_right + num_nodes_left; % fix numbering of vertex indices
+
+tri_both = get_boundary_triangles(obj_data.locs, params.data.boundary.alpha_radius);
+
+masks.inner_triangles = tri_both;
+masks.inner_triangles_l = tri_left;
+masks.inner_triangles_r = tri_right;
+
+obj_data.masks = masks;
+
+end % get_boundary_masks()
+
+function get_boundary_triangles(locs, alpha_radius)
+shp = alphaShape(locs, alpha_radius);
+% The boundary of the centroids is an approximation of the cortex
+tri_list = shp.boundaryFacets;
+end
