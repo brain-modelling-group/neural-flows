fac437c66cc439aa56ee8daa82e9f502411295b4
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-05-15 15:30:35 +1000

    Finalise draft of iterative amplitude-based flows using cnem

--- core/flows3d_cnem_loop.m
+++ core/flows3d_cnem_loop.m
@@ -16,7 +16,7 @@ function [params] = flows3d_cnem_loop(obj_data, obj_flows, params, varargin)
 %          None
 %
 % REQUIRES: 
-%           flows3d_hs3d_set_initial_flows()
+%           flows3d_cnem_set_initial_flows()
 %           flows3d_cnem_step()
 % USAGE:
 %{     
@@ -36,7 +36,7 @@ end
 tpts           = params.data.shape.timepoints;
 alpha_smooth   = params.flows.method.cnem.alpha_smooth;
 max_iterations = params.flows.method.cnem.max_iterations;
-grid_size      = params.data.shape.nodes;
+num_nodes = params.data.shape.nodes;
 
 % Save parameters for flows
 params.flows.data.shape.n = params.data.shape.nodes;
@@ -44,21 +44,29 @@ params.flows.data.shape.t = params.data.shape.timepoints-1;
 ht = params.data.ht;
 params.flows.data.ht = ht;
 
+% Get location and boundary masks
 masks = obj_data.masks;
 locs  = obj_data.locs;
 
+% Calculate neighbours matrix for averages
+tr = triangulation(masks.innies_triangles, locs);
+vertex_idx_list = [1:params.data.shape.nodes].'; 
+temp_mat = get_nth_ring_matrix(tr, vertex_idx_list, 1);
+[neighbours_matrix] = build_neighbour_matrix(locs, temp_mat);
+
 % Save masks
 obj_flows.masks = masks;
+% Save matrix
+obj_flows.neighbours_matrix = neighbours_matrix;
 
 B = flows3d_cnem_get_B_mat(locs, masks.innies_triangles);
-
 
 switch params.flows.method.cnem.initial_conditions.mode
      case {'random', 'rand'}
          seed_init = params.flows.method.cnem.initial_conditions.seed;
-         %%%%%%[uxo, uyo, uzo] = flows3d_cnem_set_initial_flows(grid_size, ~masks.outties, seed_init_vel);
+         [uxo, uyo, uzo] = flows3d_cnem_set_initial_flows(num_nodes, seed_init);
      case {'precal', 'precalculated', 'prev', 'user'}
-        fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Info:: Using pre-calculated initial velocity conditions.'))
+        fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Info:: Using pre-calculated initial flow conditions.'))
         % NOTE: I need to pass initial conditions in the flow files 
         uxo = initial_conditions.uxo;
         uyo = initial_conditions.uyo;
@@ -69,19 +77,19 @@ switch params.flows.method.cnem.initial_conditions.mode
 end
 
 % Write to disk 
-obj_flows.ux(params.data.shape.nodes, tpts-1) = 0;    
-obj_flows.uy(params.data.shape.nodes, tpts-1) = 0;
-obj_flows.uz(params.data.shape.nodes, tpts-1) = 0;
+obj_flows.ux(num_nodes, tpts-1) = 0;    
+obj_flows.uy(num_nodes, tpts-1) = 0;
+obj_flows.uz(num_nodes, tpts-1) = 0;
 
 %---------------------------------BURN-IN--------------------------------------%
 % Do a burn-in period for the first frame (eg, two time points of data)
 this_tpt = 1;
-FA = obj_data.data(:, this_tpt);
-FB = obj_data.data(:, this_tpt+1);
+FA = obj_data.data(this_tpt, :);
+FB = obj_data.data(this_tpt+1, :);
 
-if ~isfield(inparams.flows.method.cnem.burnin, 'length')
+if ~isfield(params.flows.method.cnem.burnin, 'length')
     burnin_length = 8; % for iterations, not much but better than one
-    inparams.flows.method.cnem.burnin.length = burnin_length;
+    params.flows.method.cnem.burnin.length = burnin_length;
 end
 
 fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Info:: Started burn-in period for random initial velocity conditions.'))
@@ -90,7 +98,7 @@ for bb=1:burnin_len
     [uxo, uyo, uzo] = flows3d_cnem_step(FA, FB, alpha_smooth, ...
                                         max_iterations, ...
                                         uxo, uyo, uzo, ...
-                                        masks.innies_triangles);       
+                                        neighbours_matrix);       
 end
 fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Info:: Finished burn-in period for random initial velocity conditions.'))
 %---------------------------------BURN-IN--------------------------------------%
@@ -100,20 +108,20 @@ fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Info:: Started estimati
 for this_tpt = 1:tpts-1
     
     % Read activity data                
-    FA = obj_data.data(:, this_tpt);
-    FB = obj_data.data(:, this_tpt+1);
+    FA = obj_data.data(this_tpt, :);
+    FB = obj_data.data(this_tpt+1, :);
 
     % Calculate the velocity components
     [uxo, uyo, uzo] = flows3d_cnem_step(FA, FB, alpha_smooth, ...
                                         max_iterations, ...
                                         uxo, uyo, uzo, ...
-                                        masks.innies_triangles);
+                                        neighbours_matrix);
  
     % Save the velocity components and norm
     obj_flows.ux(:, this_tpt) = single(uxo);
     obj_flows.uy(:, this_tpt) = single(uyo);
     obj_flows.uz(:, this_tpt) = single(uzo);
-    obj_flows.un(:, this_tpt) = single(sqrt(uxo.^2 + uyo.^2 + uzo.^2));;
+    obj_flows.un(:, this_tpt) = single(sqrt(uxo.^2 + uyo.^2 + uzo.^2));
     %Save some other useful information to guesstimate the singularity detection threshold
     flow_stats(this_tpt);
 
@@ -138,5 +146,5 @@ end % function flow_stats()
 
 fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Info:: Finished estimation of flows.'))
 
-end % function flows3d_hs3d_loop()
+end % function flows3d_cnem_loop()
 
