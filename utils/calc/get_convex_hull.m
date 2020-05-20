11475a7859688f8797b622fcbb4b86ad22519576
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-05-20 12:52:08 +1000

    Generalise in case we need the convex hull of only one hemisphere

--- utils/calc/get_convex_hull.m
+++ utils/calc/get_convex_hull.m
@@ -1,4 +1,4 @@
-function get_convex_hull(obj_data, alpha_radius, left_idx, right_idx)
+function get_convex_hull(obj_data, alpha_radius, hemi1_idx, hemi2_idx)
 %% Returns a local patch of surface of the neighbourhood around a vertex.
 %
 % ARGUMENTS: params    -- almighty structure
@@ -21,17 +21,21 @@ function get_convex_hull(obj_data, alpha_radius, left_idx, right_idx)
 % Calculate individual convex hulls
 
 
-num_nodes_left = length(left_idx);
+num_nodes_hemi1 = length(hemi1_idx);
 
-tri_left  = get_boundary_triangles(obj_data.locs(left_idx, :), alpha_radius);
-tri_right = get_boundary_triangles(obj_data.locs(right_idx, :),alpha_radius);
-tri_right = tri_right + num_nodes_left; % fix numbering of vertex indices, assumes all the left nodes are contiguous
+if ~isemtpy(hemi2_idx)
+    tri_hemi1  = get_boundary_triangles(obj_data.locs(hemi1, :), alpha_radius);
+    tri_hemi2 = get_boundary_triangles(obj_data.locs(hemi2_idx, :), alpha_radius);
+    tri_hemi2 = tri_right + num_nodes_hemi1; % fix numbering of vertex indices, assumes all the left nodes are contiguous
+    tri_both = get_boundary_triangles(obj_data.locs, alpha_radius);
 
-tri_both = get_boundary_triangles(obj_data.locs, alpha_radius);
-
-masks.innies_triangles_bi = tri_both;
-masks.innies_triangles_lr = [tri_left; tri_right];
-
+    masks.innies_triangles_bi = tri_both;
+    masks.innies_triangles_lr = [tri_left; tri_right];
+else
+    tri_hemi1  = get_boundary_triangles(obj_data.locs(hemi1_idx, :), alpha_radius);
+    masks.innies_triangles_bi = tri_hemi1;
+    masks.innies_triangles_lr = tri_hemi1; 
+end
 obj_data.masks = masks;
 
 end % get_boundary_masks()
