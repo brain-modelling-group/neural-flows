02a1ee51703ba0e5a75258b9a19186f5ea62ab5f
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-05-21 18:46:39 +1000

    Update test function

--- tests/test_flows3d_estimation__plane_wave_upc.m
+++ tests/test_flows3d_estimation__plane_wave_upc.m
@@ -1,46 +1,19 @@
 function test_flows3d_estimation__plane_wave_upc()
-% NOTE: Takes about XX seconds @dracarys -- cnem is a c++ multithreaded
-% library.
+%% Estimate phase-based flows of a plane wave using CNEM library, 
+% and Roberts et al. 2019 method. 
+% The wave is defined on an unstructured domain (ie, scattered points).
+% This is a wave moving at 4 m/s in the y-direction.
+% NOTE: Assumes this function is called from tests/ directory 
 
-% Generate data
- options.hx = 4;
- options.hy = 4;
- options.hz = 4;
- options.ht = 4;
- options.alpha_radius = 30;
- options.is_phase = true;
- 
- 
- load('513COG.mat', 'COG')
- locs = COG(1:256, :);
- 
- 
- [data, ~] = generate_data3d_plane_wave('hxyz',  options.hx, 'ht', options.ht, ...
-                                        'direction', 'z', ...
-                                        'locs', locs, ...
-                                        'grid_type', 'unstructured');
- 
- 
- data = calculate_insta_phase(data);
- fig_name = 'nflows-test-planewave3d-unstructured-phase-cnem-upc';
- u = flows3d_estimate_cnem_flow(data, locs, options.ht, options);
- 
- fig_hist = figure('Name', fig_name);
+input_params = read_write_json('test-flows3d-estimation_plane-wave_upc_in.json', 'json/', 'read');
 
- subplot(1, 4, 1, 'Parent', fig_hist)
- histogram(u.vxp(:))
- xlabel('ux [mm/ms]')   
- 
- subplot(1, 4 ,2, 'Parent', fig_hist)
- histogram(u.vyp(:))
- xlabel('uy [mm/ms]')
+output_params = main(input_params); 
 
- subplot(1, 4, 3, 'Parent', fig_hist)
- histogram(u.vzp(:))
- xlabel('uz [mm/ms]')
- 
- subplot(1, 4, 4, 'Parent', fig_hist)
- histogram(sqrt(u.vxp(:).^2+u.vyp(:).^2+u.vzp(:).^2))
- xlabel('u_{norm} [mm/ms]')
+[obj_flows] = load_iomat_flows(output_params);
+
+% Plot stuff
+fig_hist = figure('Name', mfilename);
+
+plot_debug_flow_histogram(fig_hist, obj_flows, output_params)
 
 end % function test_flows3d_estimation__plane_wave_upc()
