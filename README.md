89775ad77fb63e111f070c822ef54c33d7cfc2b5
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-05-21 14:42:20 +1000

    Rename

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 
-## Brain Waves: Generation, detection and classification of measured spatiotemporal patterns in brain network models.
-## Patch Flow
-## Neural Flows
+## Neural Flows: estimation of velocities, detection and classification of brainwave dynamics.
+
 If you use this toolbox, please cite:
 
-Roberts JA, Gollo LL, Abeysuriya R, Roberts G, Mitchell PB, Woolrich MW, Breakspear M. Metastable brain waves. (2018) (submitted). [Preprint available on bioarxiv.](https://www.biorxiv.org/content/early/2018/10/03/347054)
+Roberts JA, Gollo LL, Abeysuriya R, Roberts G, Mitchell PB, Woolrich MW, Breakspear M. Metastable brain waves. (2019) (submitted). [Available from](https://www.biorxiv.org/content/early/2018/10/03/347054)
 
+Sanz-Leon P, Gollo LL, and Roberts JA (2020) (submitted). [Available from bioarxiv]()
 
 
 #### Dependencies:
--- tests/test_flows3d_estimation__travelling_wave_sah.m
+++ tests/test_flows3d_estimation__travelling_wave_sah.m
@@ -5,8 +5,8 @@ function test_flows3d_estimation__travelling_wave_sah()
  options.hx = 2;
  options.hy = 2;
  options.hz = 2;
- options.ht = 0.5;
- [data, X, Y, Z, ~] = generate_wave3d_travelling_grid('visual_debugging', true, ...
+ options.ht = 1;
+ [data, X, Y, Z, ~] = generate_data3d_travelling_wave('visual_debugging', true, ...
                                                       'hxyz', options.hx, ...
                                                       'ht', options.ht, ...
                                                       'velocity', 1, ...
@@ -15,7 +15,8 @@ function test_flows3d_estimation__travelling_wave_sah()
  
  % Save data
  full_path_to_file = mfilename('fullpath'); 
- obj_data = matfile([full_path_to_file '.mat'], 'Writable', true);
+ path_to_dir = get_directory_path(full_path_to_file, 'data');
+ obj_data = matfile([path_to_dir filesep mfilename '.mat'], 'Writable', true);
  obj_data.data = data;
  obj_data.X = X;
  obj_data.Y = Y;
@@ -27,34 +28,35 @@ function test_flows3d_estimation__travelling_wave_sah()
  obj_data.ht = options.ht;
  
 %
- input_params_filename = 'test-flow-travellingwave3d-grid-in.json';
- input_params_dir  = '/home/paula/Work/Code/matlab-neuro/neural-flows/tests';
- json_mode = 'read';
-
-% Load options -- TODO: the use of load_data is kind of a hack. The toolbox
-% should make a new matfile for the data. Data should be stored in a basic
-% matfile.
- input_params = read_write_json(input_params_filename, input_params_dir, json_mode);
- [~, input_params] = load_data(input_params);
- 
- output_params = main(input_params); 
- 
- [obj_flows] = load_iomat_flows(output_params);
-
- fig_hist = figure('Name', 'nflows-test-travellingwave3d-grid-hs3d');
-
- subplot(1, 4, 1, 'Parent', fig_hist)
- histogram(obj_flows.ux(2:end-1, 2:end-1, 2:end-1, :))
- xlabel('ux [m/s]')
- 
- subplot(1, 4 ,2, 'Parent', fig_hist)
- histogram(obj_flows.uy(2:end-1, 2:end-1, 2:end-1, :))
- xlabel('uy [m/s]')
- 
- subplot(1, 4, 3, 'Parent', fig_hist)
- histogram(obj_flows.uz(2:end-1, 2:end-1, 2:end-1, :))
- xlabel('uz [m/s]')
-
+input_params_filename = 'test-flow-travellingwave3d-grid-in.json';
+input_params_dir  = '/home/paula/Work/Code/matlab-neuro/neural-flows/tests/json';
+json_mode = 'read';
+% 
+% % Load options -- TODO: the use of load_data is kind of a hack. The toolbox
+% % should make a new matfile for the data. Data should be stored in a basic
+% % matfile.
+input_params = read_write_json(input_params_filename, input_params_dir, json_mode);
+[~, input_params] = load_data(input_params);
+%  
+output_params = main(input_params); 
+%  
+[obj_flows] = load_iomat_flows(output_params);
+% 
+keyboard
+fig_hist = figure('Name', 'nflows-test-travellingwave3d-grid-hs3d');
+% 
+%  subplot(1, 4, 1, 'Parent', fig_hist)
+%  histogram(obj_flows.ux(2:end-1, 2:end-1, 2:end-1, :))
+%  xlabel('ux [m/s]')
+%  
+%  subplot(1, 4 ,2, 'Parent', fig_hist)
+%  histogram(obj_flows.uy(2:end-1, 2:end-1, 2:end-1, :))
+%  xlabel('uy [m/s]')
+%  
+%  subplot(1, 4, 3, 'Parent', fig_hist)
+%  histogram(obj_flows.uz(2:end-1, 2:end-1, 2:end-1, :))
+%  xlabel('uz [m/s]')
+% 
  subplot(1, 4, 4, 'Parent', fig_hist)
  histogram(sqrt(obj_flows.ux(2:end-1, 2:end-1, 2:end-1, :).^2 + ...
                 obj_flows.uy(2:end-1, 2:end-1, 2:end-1, :).^2 + ...
