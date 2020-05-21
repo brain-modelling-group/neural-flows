74e49c18a1b15c12d0c8c8e42ca1d7fd181be12f
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-05-21 15:43:30 +1000

    Finish test

--- tests/test_flows3d_estimation__plane_wave_sah.m
+++ tests/test_flows3d_estimation__plane_wave_sah.m
@@ -1,7 +1,8 @@
 function test_flows3d_estimation__plane_wave_sah()
 % Estimate amplitude-based flows of a plane wave using Horn-Schunk 3D algorithm.
 % Assumes this function is called from tests/ directory 
- 
+% This is a plane wave moving at 4 m/s in the y-direction
+
 input_params = read_write_json('test-flows3d-estimation_plane-wave_sah_in.json', 'json/', 'read');
 
 output_params = main(input_params); 
