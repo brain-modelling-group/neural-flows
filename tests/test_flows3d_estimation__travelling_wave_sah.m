94cd6f9076752034b3453caa3f4b2db1cab5860b
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-05-21 17:23:17 +1000

    Update docs

--- tests/test_flows3d_estimation__travelling_wave_sah.m
+++ tests/test_flows3d_estimation__travelling_wave_sah.m
@@ -1,8 +1,8 @@
 function test_flows3d_estimation__travelling_wave_sah()
 % Estimate amplitude-based flows of a plane wave using Horn-Schunk 3D algorithm.
 % The wave is defined on an structured domain (ie, regular grid).
-% This is a wave moving at 2 m/s in the y-direction, and then travelling
-% back in the same direction but opposite orientation (- 2 m/s)
+% This is a wave moving at 4 m/s in the y-direction, and then travelling
+% back in the same direction but opposite orientation (- 4 m/s)
 % NOTE: Assumes this function is called from tests/ directory 
 
 input_params = read_write_json('test-flows3d-estimation_travelling-wave_sah_in.json', 'json/', 'read');
