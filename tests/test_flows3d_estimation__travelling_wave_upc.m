25173f99c9860eeb3981e10ae24cad57009c937e
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-05-21 17:42:43 +1000

    Update test function

--- tests/test_flows3d_estimation__travelling_wave_upc.m
+++ tests/test_flows3d_estimation__travelling_wave_upc.m
@@ -1,52 +1,20 @@
 function test_flows3d_estimation__travelling_wave_upc()
-% grid-input-method
-% u: unstructured
-% p: phase-based
-% c: cnem
-% s: structured (grid)
-% a: activity-based (input data)
-% h: hornschunk (method)
+%% Estimate phase-based flows of a travelling wave using CNEM library, 
+% and Roberts et al. 2019 method. 
+% The wave is defined on an unstructured domain (ie, scattered points).
+% This is a wave moving at 4 m/s in the y-direction, and then travelling
+% back in the same direction but opposite orientation (- 4 m/s)
+% NOTE: Assumes this function is called from tests/ directory 
 
-%NOTE: takes about 51 seconds @dracarys
- %Generate data
- options.hx = 2;
- options.hy = 2;
- options.hz = 2;
- options.ht = 0.5;
- options.alpha_radius = 30;
- options.is_phase = true;
- % With these parameters the wave is moving at 2 m/s along the x-axis
- 
- % Load centres of gravity
- load('513COG.mat', 'COG')
- locs = COG(1:256, :);
- locs = [locs; COG(513, :)]; % amygdala on that side.
+input_params = read_write_json('test-flows3d-estimation_travelling-wave_upc_in.json', 'json/', 'read');
 
- % Generate fake data
- [data, ~] = generate_data3d_travelling_wave('locs', locs, 'hxyz',  options.hx, 'ht', options.ht, 'direction', 'y');
- 
- % Transform data into phase via hilber transform
- data = calculate_insta_phase(data);
- fig_name = 'nflows-test-travellingwave3d-unstructured-phase-cnem';
-  
- u = flows3d_estimate_cnem_flow(data, locs, options.ht);
- 
- fig_hist = figure('Name', fig_name);
+output_params = main(input_params); 
 
- subplot(1, 4, 1, 'Parent', fig_hist)
- histogram(u.vxp(:, 1:50))
- xlabel('ux [m/s]')
- 
- subplot(1, 4 ,2, 'Parent', fig_hist)
- histogram(u.vyp(:, 1:50))
- xlabel('uy [m/s]')
+[obj_flows] = load_iomat_flows(output_params);
 
- subplot(1, 4, 3, 'Parent', fig_hist)
- histogram(u.vzp(:, 1:50))
- xlabel('uz [m/s]')
+% Plot stuff
+fig_hist = figure('Name', mfilename);
+
+plot_debug_flow_histogram(fig_hist, obj_flows, output_params)
  
- subplot(1, 4, 4, 'Parent', fig_hist)
- histogram(sqrt(u.vxp(:).^2+u.vyp(:).^2+u.vzp(:).^2))
- xlabel('u_{norm} [mm/ms]')
- 
-end % function test_flow_estimation__travellingwave3d_scattered_cnem()
+end % function test_flows3d_estimation__travelling_wave_upc()
