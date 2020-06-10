28ccb2135518301c373de7b3cad8d994c5bb8146
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-06-10 12:19:58 +1000

    REFACTOR: Change internal handling of obj_streams

--- visualisation/plot3d_nodal_streamlines.m
+++ visualisation/plot3d_nodal_streamlines.m
@@ -1,17 +1,18 @@
 function plot3d_nodal_streamlines(obj_streams)
 
 
-locs = obj_streams.seed_locs;
+locs = obj_streams.seeding_locs;
 
 
 % Start locations
 plot3(locs(:, 1), locs(:, 2), locs(:, 3), 'k.')
 hold on
 
-for tt = 1:200
-	streams = obj_streams.streamlines(1, tt).streams;
+for tt = 1:10
+	temp_frame = obj_streams.streamlines(1, tt);
+    streams = temp_frame.paths;
 
-    for ii=1:length(streams);
+    for ii=1:length(streams)
        plot3(squeeze(streams{ii}(:, 1)), ...
              squeeze(streams{ii}(:, 2)), ...
              squeeze(streams{ii}(:, 3)), 'color', [0.5 0.5 0.5 0.1]);
