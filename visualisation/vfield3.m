890727281a99c76d75e61a698b0fe069de8bdda0
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-06-13 17:27:17 +1000

    Refactor: clean up, use end of functions

--- visualisation/vfield3.m
+++ visualisation/vfield3.m
@@ -1,4 +1,4 @@
-function theHandle = vfield3(axh, x,y,z,u,v,w,varargin)
+function theHandle = vfield3(axh,x,y,z,u,v,w,varargin)
 %VFIELD3   Plot 3D velocity field
 %   Plots 3-D arrows as cones. Is similar to CONEPLOT but the inputs
 %   can have any dimension.
@@ -56,7 +56,7 @@ useColor = 0;
 color    = 'b';
 for i=1:length(varargin)
   vin = varargin{i};
-  if isnumeric(varargin{1});
+  if isnumeric(varargin{1})
     C = varargin{1};
     useC = 1;
   end
@@ -78,7 +78,7 @@ for i=1:length(varargin)
   end
 end
 tt=linspace(0,2*pi,n);
-nvals=prod(size(x));
+nvals=numel(x);
 x0 = reshape(x,nvals,1);
 y0 = reshape(y,nvals,1);
 z0 = reshape(z,nvals,1);
@@ -86,11 +86,12 @@ u  = reshape(u,nvals,1);
 v  = reshape(v,nvals,1);
 w  = reshape(w,nvals,1);
 speed = sqrt(u.^2+v.^2+w.^2);
-if ~isstr(r)
+if ~ischar(r)
   L = speed.*r;
 else
-  L = repmat(str2num(r),size(speed));
+  L = repmat(str2double(r), size(speed));
 end
+
 rr = L*tan(fi*pi/180);
 x  = rr*cos(tt);
 y  = rr*sin(tt);
@@ -101,25 +102,25 @@ x=[zz  x xxz  xxz zz];
 y=[zz  y yyz  yyz zz];
 z=[speed repmat(speed-L,1,length(tt)) repmat(speed-L,1,length(tt)),...
    zeros(nvals,length(tt)) zeros(1,length(x0))'];
-[th,phi,r]=cart2sph(u,v,w);
+[th,phi,~]=cart2sph(u,v,w);
 th  = repmat(th, 1,size(x,2));
 phi = repmat(phi,1,size(x,2))-pi/2;
 [x,y,z]=rot3d(x,y,z,th*180/pi,phi*180/pi);
 x=x+repmat(x0,1,size(x,2));
 y=y+repmat(y0,1,size(x,2));
 z=z+repmat(z0,1,size(x,2));
-x = reshape(x',prod(size(x)),1);
-y = reshape(y',prod(size(y)),1);
-z = reshape(z',prod(size(z)),1);
+x = reshape(x',numel(x),1);
+y = reshape(y',numel(y),1);
+z = reshape(z',numel(z),1);
 fff = [
-       ones(n-1,1)       ones(n-1,1)     [2     : n    ]'    [3    : n+1  ]';
-       [2     : n    ]'  [3   : n+1   ]' [n+3   : 2*n+1]'    [n+2  : 2*n  ]';
-       [n+3   : 2*n+1]'  [n+2 : 2*n   ]' [2*n+2 : 3*n  ]'    [2*n+3: 3*n+1]';
-       [2*n+2 : 3*n  ]'  [2*n+3: 3*n+1]' (3*n+2)*ones(n-1,1) (3*n+2)*ones(n-1,1)
+       ones(n-1,1)        ones(n-1,1)      (2     : n    ).'    (3    : n+1  ).';
+       (2     : n    ).'  (3   : n+1   ).' (n+3   : 2*n+1).'    (n+2  : 2*n  ).';
+       (n+3   : 2*n+1).'  (n+2 : 2*n   ).' (2*n+2 : 3*n  ).'    (2*n+3: 3*n+1).';
+       (2*n+2 : 3*n  ).'  (2*n+3: 3*n+1).' (3*n+2)*ones(n-1,1) (3*n+2)*ones(n-1,1)
 ];
-add = [0:nvals-1]*(3*n+2);
+add = (0:nvals-1)*(3*n+2);
 add = repmat(add',1,size(fff,1));
-add = reshape(add',prod(size(add)),1);
+add = reshape(add',numel(add),1);
 add = repmat(add,1,size(fff,2));
 if ~useColor
   if ~useC
@@ -128,7 +129,7 @@ if ~useColor
     C = reshape(C,nvals,1);
   end
   C = repmat(C,1,3*n+2);
-  C = reshape(C',prod(size(C)),1);
+  C = reshape(C',numel(C),1);
   p = patch(axh, x,y,z,C);
 else
   p = patch(axh, x,y,z, color);
@@ -139,6 +140,7 @@ fff = fff+add;
 set(p,'faces',fff);
 set(p,'edgecolor','none');
 theHandle = p;
+end % function vfield3()
 
 function [x2,y2,z2]=rot3d(x,y,z,tt,fi)
 %ROT3D   3D solid rotation
@@ -180,15 +182,13 @@ function [x2,y2,z2]=rot3d(x,y,z,tt,fi)
 %   University of Aveiro, Portugal
 %   07-07-2004 - Correction
 %   02-10-2005 - allow inputs as N-D arrays
-x2=[];
-y2=[];
-z2=[];
+
 theSize = size(x);
 n = prod(theSize);
 x = reshape(x,n,1);
 y = reshape(y,n,1);
 z = reshape(z,n,1);
-if size(tt)==1 & size(fi)==1
+if size(tt)==1 && size(fi)==1
   tt = repmat(tt,n,1);
   fi = repmat(fi,n,1);
 else
@@ -200,9 +200,11 @@ v2 = rot(v,tt,fi);
 x2=v2(1,:);  x2 = reshape(x2,theSize);
 y2=v2(2,:);  y2 = reshape(y2,theSize);
 z2=v2(3,:);  z2 = reshape(z2,theSize);
+end % function rot3d()
+
 function v2 = rot(v,tt,fi)
 v = repmat(v,1,3);
-v  = reshape(v',1,prod(size(v)));
+v  = reshape(v', 1, numel(v));
 tt=-tt*pi/180; tt=tt';
 fi=-fi*pi/180; fi=fi';
 n=length(tt);
@@ -214,3 +216,4 @@ v2 = M.*v;
 v2 = reshape(v2,3,3*n);
 v2 = sum(v2);
 v2 = reshape(v2,3,n);
+end % function rot()
