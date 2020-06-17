bcdb6bca27713584c18292a6dc5b287a6bbc44df
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-06-17 14:29:36 +1000

    Create main for automated plot generation

---
+++ main_visualisation.m
@@ -0,0 +1,32 @@
+function ouparams = main_visualisation(inparams)
+%% Wrapper function to perform basic analysis:
+%  (1): svd decomposition
+%  (2): singularity analysis
+
+% Tic
+tstart = tik();
+
+% Copy structure
+tmp_params = inparams;
+
+%---------------------------------DECOMPOSITION-------------------------%
+if inparams.flows.decomposition.svd.enabled
+    % Check if we need to interpolate data
+    perform_svd_mode_decomposition(inparams);
+
+end
+%---------------------------SINGULARITY---------------------------------%
+if inparams.singularity.quantification.enabled
+
+end
+%-------------------------------------------------------------------------------%
+% save_tmp_params(tmp_params)
+%TRACKING
+% TODO: write output parameters if reequired
+
+% Toc
+ tok(tstart, 'seconds');
+ tok(tstart, 'minutes');
+ tok(tstart, 'hours');
+
+end % function main_analysis()
