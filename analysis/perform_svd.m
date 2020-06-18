39e1e07676302c6dea322320c7fa562479e7eca8
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-06-18 21:27:36 +1000

    Debug: make file writable

---
+++ analysis/perform_svd.m
@@ -0,0 +1,20 @@
+function perform_svd(params)
+% Performs singular vector decomposition of a vector field.
+% Plots most dominant spatial modes and their time series 
+% 
+% USAGE:
+%{
+    
+%}
+% AUTHOR:
+% Paula Sanz-Leon, QIMR Berghofer, 2020
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
+
+% Load flows data
+
+svd_struct = svd_3d(params);
+obj_flows = load_iomat_flows(params);
+obj_flows.Properties.Writable = true;
+obj_flows.svd = svd_struct;
+obj_flows.Properties.Writable = false;
+end % function perform_mode_decomposition()
