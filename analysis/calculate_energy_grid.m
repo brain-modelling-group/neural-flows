b81456770ed8c8b91faee546c9b9b106afa1d0b2
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-06-18 18:07:48 +1000

    Debug: fix syntax erros

--- analysis/calculate_energy_grid.m
+++ analysis/calculate_energy_grid.m
@@ -1,4 +1,4 @@
-function [energy] = calculate_energy_grid(obj_flows)
+function [energy] = calculate_energy_grid(params, obj_flows)
 %Computation of kinetic energy. The kinetic energy is similar to the norm of the
 % vector field, but it emphasizes low ( val < 1) vs high energy states (val > 1)
 % ARGUMENTS:
@@ -21,26 +21,18 @@ function [energy] = calculate_energy_grid(obj_flows)
 %     Paula Sanz-Leon, QIMR Berghofer 2018
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% NOTE: Displacement energy, this is a kinectic energy-like expression: 1/2 m v^2
-% The mass is m=1
-
-params = obj_flows.params; 
 tpts   = params.flows.data.shape.t;
 
-
 % Probably best to iterate over time
-innies_idx = find(obj_flows.masks.innies == true);
-temp_energy(length(innies)), tpts) = 0;
-temp_energy(length(innies)), tpts) = 0;
+innies_idx = obj_flows.masks.innies == true;
+temp_energy(length(innies), tpts) = 0;
 
 parfor tt=1:tpts
     un = obj_flows.un(:, :, :, tt);
     temp_energy(:, tt) = kinetic_energy(un(innies_idx));
 end
 % 
-norm_sum_flow = sqrt(obj_flows.ux_sum.^2 + obj_flows.uy_sum.^2 + obj_flows.uz_sum.^2) 
-
+norm_sum_flow = sqrt(obj_flows.ux_sum.^2 + obj_flows.uy_sum.^2 + obj_flows.uz_sum.^2); 
 energy.spatial_sum = nansum(temp_energy);
 energy.spatial_average = nanmean(temp_energy);
 energy.spatial_median  = nanmedian(temp_energy);
--- analysis/calculate_energy_nodal.m
+++ analysis/calculate_energy_nodal.m
@@ -1,4 +1,4 @@
-function [energy] = calculate_energy_nodal(flow_field)
+function [energy] = calculate_energy_nodal(params, obj_flows)
 %Computation of kinetic energy. The kinetic energy is similar to the norm of the
 % vector field, but it emphasizes low ( val < 1) vs high energy states (val > 1)
 % ARGUMENTS:
@@ -28,8 +28,6 @@ function [energy] = calculate_energy_nodal(flow_field)
 % NOTE: Displacement energy, this is a kinectic energy-like expression: 1/2 m v^2
 % The mass is m=1, although nodes could have different masses/weigths (eg, hubs/nonhubs)
 
-
-params = obj_flows.params; 
 tpts   = params.flows.data.shape.t;
 num_nodes  = size(locs, 1);
 
@@ -41,7 +39,7 @@ parfor tt=1:tpts
     temp_energy(:, tt) = kinetic_energy(un);
 end
 % Calculate "norm" using the sum (over space) of each individual velocity component
-norm_sum_flow = sqrt(obj_flows.ux_sum.^2 + obj_flows.uy_sum.^2 + obj_flows.uz_sum.^2) 
+norm_sum_flow = sqrt(obj_flows.ux_sum.^2 + obj_flows.uy_sum.^2 + obj_flows.uz_sum.^2); 
 
 energy.spatial_sum = nansum(temp_energy);
 energy.spatial_average = nanmean(temp_energy);
