c0716671ebcaff5a8f91a67d55a3afc57992d1d8
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-06-18 18:18:30 +1000

    Debug: fix function name and variables

--- utils/calc/kinetic_energy.m
+++ utils/calc/kinetic_energy.m
@@ -1,8 +1,8 @@
-function ke  = kinetic_eenrgy(v, varargin)
+function ke  = kinetic_energy(v, varargin)
 
-if nargin < 2,
+if nargin < 2
 	m = 1.0; % mass is 1
 end
 % E = (1/2)*m*v^2
-    ke = 0.5.*m.*(Fx.^2 + Fy.^2 + Fz.^2).^2;	
+    ke = 0.5.*m.*v.^2;	
 end % function kinetic_energy()
