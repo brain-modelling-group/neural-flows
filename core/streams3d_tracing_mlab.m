1cf685179fcf3e31036ecfd5cea39c325f18cf7a
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-06-19 20:09:38 +1000

    Reafactor: enable streamline tracing for structured grids

--- core/streams3d_tracing_mlab.m
+++ core/streams3d_tracing_mlab.m
@@ -1,7 +1,6 @@
 function obj_streams = streams3d_tracing_mlab(obj_flows, obj_streams, params)
 %% Traces streamlines using a velocity field defined on a grid in space
-%  based on.
-%
+% % TODOC
 % REQUIRES: 
 %          
 %          
@@ -14,31 +13,28 @@ function obj_streams = streams3d_tracing_mlab(obj_flows, obj_streams, params)
 % MODIFICATION HISTORY:
 %     Paula Sanz-Leon, QIMR Berghofer, 2019, 2020
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Trace streamlines for one frame 
-tt=2;
-
-% Save grid 
-obj_streams.X = obj_flows.X;
-obj_streams.Y = obj_flows.Y;
-obj_streams.Z = obj_flows.Z;
 
 
-x_dim = 1;
-y_dim = 2;
-z_dim = 3;
 
+% Get some useful information for what we're about to do
 masks = obj_flows.masks;
 innies_idx = find(masks.innies) == true;
-
+time_step = params.streamlines.tracing.time_step; % fake time step for streamline tracing
+max_stream_length = params.streamlines.tracing.max_stream_length;;
 % Get seeding locations
 seeding_locs = get_seeding_locations_3d_slice(obj_flows.X(innies_idx), obj_flows.Y(innies_idx), obj_flows.Z(innies_idx), ...
                                               params.streamlines.tracing.seeding_points.modality, ...
                                               params.streamlines.tracing.seeding_points.seed);
-% Save seeding locations
+% Save seeding locations & grid
 obj_streams.seeding_locs = seeding_locs;
+obj_streams.X = obj_flows.X;
+obj_streams.Y = obj_flows.Y;
+obj_streams.Z = obj_flows.Z;
 
-
+% Allocate struct
 streamlines.paths = [];
+
+% Trace streamlines
 tracing_mlab_time_loop()
 
 function tracing_mlab_time_loop()
@@ -56,10 +52,8 @@ verts = stream3(X, Y, Z, obj_flows.ux(:, :, :, tt), ...
                          obj_flows.uz(:, :, :, tt), ...
                          seeding_locs.X, ...
                          seeding_locs.Y, ...
-                         seeding_locs.Z, [2^-12]); 
+                         seeding_locs.Z, [time_step max_stream_length]); 
 
 end % function tracing_mlab_time_loop()
-
-
 
 end % function streams3d_tracing_mlab()
