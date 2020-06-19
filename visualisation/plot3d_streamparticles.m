fc101eb8fa000ceadeec5d46da73e760c97f3b93
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-06-19 22:46:31 +1000

    Refactor: draft high-level plotting function as described in [#10]

--- visualisation/plot3d_streamparticles.m
+++ visualisation/plot3d_streamparticles.m
@@ -1,45 +1,27 @@
+function fig_handle = plot3d_streamparticles(params)	
+% Set info we need
+%these_frames = params.stremalines.visualisation.particles.frames;
+these_frames = [10];
 
+% Load file handles
+obj_flows = load_iomat_flows(params);
+obj_streams = load_iomat_streams(params);
 
-% Remove nan vertices from streamlines (points outside the convex hull)                     
-verts = cellfun(@remove_nans, verts, 'UniformOutput', false);
-% Make all streamlines of the same length so we can use streamparticles
-stream_lengths   = cellfun(@(c)  size(c, 1), verts);
-max_length = max(stream_lengths);
-wrap_func = @(verts) add_vertices(verts, max_length);
-verts = cellfun(wrap_func, dummy_cell, 'UniformOutput', false);
+% Load info we need for visualisation
+xyz_lims = obj_flows.xyz_lims; 
 
-%verts = cellfun(@add_vertices, verts, 'UniformOutput', false);
-
-sl = streamline(verts);
-set(sl,'LineWidth',1);
-set(sl,'Color',[0.5 0.5 0.5 0.5]);
-%set(sl,'Visible','off');
-% interpolated vertices
-%iverts = interpstreamspeed(X, Y, Z, mfile_vel.ux(:, :, :, tt), ...
-%                                    mfile_vel.uy(:, :, :, tt), ....
-%                                    mfile_vel.uz(:, :, :, tt), verts,.5);
-%
-%axis tight; view(30,30); 
-haxes = gca;
-%haxes.SortMethod = 'ChildOrder';
-%camproj perspective; box on
-%camva(44); camlookat; camdolly(0,0,.4, 'f');
-%h = line;
-haxes.XLim = [min(X(:)), max(X(:))];
-haxes.YLim = [min(Y(:)), max(Y(:))];
-
-haxes.ZLim = [min(Z(:)), max(Z(:))];
-
-streamparticles(haxes, verts, 42, 'animate', Inf, 'ParticleAlignment', 'on', 'MarkerfaceColor', 'blue');
-
-displaynow 
-
-
+for ff=1:length(these_frames)
+    stream_verts = get_verts(these_frames(ff));
+    fig_handle{ff} = figure('Name', 'nflows-flows-streamparticles')
+    axs_handle{ff} = p3_streamparticles(fig_handle, verts, xyz_lims)
 end
 
+function verts = get_verts(idx)
+  tmp = obj_streams.streamlines(1, idx);
+  verts = tmp.paths;
+  verts = make_streams_uniform(verts);
+end % get_verts()
 
-function x = remove_nans(x)
-% Dummy function to be call by cellfun
-    x(isnan(x)) = [];
-    
-end
+end % function plot3d_streamparticles()
+
+
