03e9cf996ec2268eb761eb16c1948e06d8cda47c
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-06-19 19:51:31 +1000

    Refactor: change output signature

--- utils/calc/get_seeding_locations_2d_slice.m
+++ utils/calc/get_seeding_locations_2d_slice.m
@@ -1,4 +1,4 @@
-function [start_X, start_Y] = get_seeing_locations_2d_slice(X0, Y0, modality, seed)
+function [seeding_locs] = get_seeing_locations_2d_slice(X0, Y0, modality, seed)
 
 if nargin < 4
     seed = 2020;
@@ -8,14 +8,14 @@ rng(seed)
 
 switch modality
     case {'grid', 'gridded'}
-        start_X = X0(:); 
-        start_Y = Y0(:);
+        seeding_locs.X = X0(:); 
+        seeding_locs.Y = Y0(:);
     case {'random_sparse', 'random-sparse'}
-        start_X = min(X0(:)) + (max(X0(:)) - min(X0(:))) * rand(floor(size(X0)./2));
-        start_Y = min(Y0(:)) + (max(Y0(:)) - min(Y0(:))) * rand(floor(size(Y0)./2));        
+        seeding_locs.X = min(X0(:)) + (max(X0(:)) - min(X0(:))) * rand(floor(size(X0)./2));
+        seeding_locs.Y = min(Y0(:)) + (max(Y0(:)) - min(Y0(:))) * rand(floor(size(Y0)./2));        
     case {'random_dense', 'random-dense'}
-        start_X = min(X0(:)) + (max(X0(:)) - min(X0(:))) * rand(size(X0));
-        start_Y = min(Y0(:)) + (max(Y0(:)) - min(Y0(:))) * rand(size(Y0));
+        seeding_locs.X = min(X0(:)) + (max(X0(:)) - min(X0(:))) * rand(size(X0));
+        seeding_locs.Y = min(Y0(:)) + (max(Y0(:)) - min(Y0(:))) * rand(size(Y0));
     otherwise
         error(['neural-flows::' mfilename '::UnknownCase'], ...
               'Unknown modality for seeding streamlines');
