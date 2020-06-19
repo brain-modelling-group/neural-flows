c60739461b92a0a35f28c15bb9af2916d297f68f
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-06-19 22:09:28 +1000

    Debug: fix generation of seeding locations. Again.

--- utils/calc/get_seeding_locations_3d_slice.m
+++ utils/calc/get_seeding_locations_3d_slice.m
@@ -12,13 +12,13 @@ switch modality
         seeding_locs.Y = Y0(:);
         seeding_locs.Z = Z0(:);
     case {'random_sparse', 'random-sparse'}
-        seeding_locs.X = min(X0(:)) + (max(X0(:)) - min(X0(:))) * rand(floor(size(X0,1)./2));
-        seeding_locs.Y = min(Y0(:)) + (max(Y0(:)) - min(Y0(:))) * rand(floor(size(Y0,1)./2)); 
-        seeding_locs.Z = min(Z0(:)) + (max(Z0(:)) - min(Z0(:))) * rand(floor(size(Z0,1)./2));               
+        seeding_locs.X = min(X0(:)) + (max(X0(:)) - min(X0(:))) * rand(floor(size(X0,1)./2), 1);
+        seeding_locs.Y = min(Y0(:)) + (max(Y0(:)) - min(Y0(:))) * rand(floor(size(Y0,1)./2), 1); 
+        seeding_locs.Z = min(Z0(:)) + (max(Z0(:)) - min(Z0(:))) * rand(floor(size(Z0,1)./2), 1);               
     case {'random_dense', 'random-dense'}
-        seeding_locs.X = min(X0(:)) + (max(X0(:)) - min(X0(:))) * rand(size(X0,1));
-        seeding_locs.Y = min(Y0(:)) + (max(Y0(:)) - min(Y0(:))) * rand(size(Y0,1));
-        seeding_locs.Z = min(Z0(:)) + (max(Z0(:)) - min(Z0(:))) * rand(size(Z0,1));
+        seeding_locs.X = min(X0(:)) + (max(X0(:)) - min(X0(:))) * rand(size(X0,1),1);
+        seeding_locs.Y = min(Y0(:)) + (max(Y0(:)) - min(Y0(:))) * rand(size(Y0,1),1);
+        seeding_locs.Z = min(Z0(:)) + (max(Z0(:)) - min(Z0(:))) * rand(size(Z0,1),1);
     otherwise
         error(['neural-flows::' mfilename '::UnknownCase'], ...
               'Unknown modality for seeding streamlines on a structured 3D grid.');        
