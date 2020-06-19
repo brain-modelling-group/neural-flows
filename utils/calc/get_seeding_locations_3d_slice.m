7cc5af0f01954d08e787ac8df8a0ca216f895697
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-06-19 23:33:56 +1000

    Debug: fix seeding strategy to ensure locs are inside the convex hull.

--- utils/calc/get_seeding_locations_3d_slice.m
+++ utils/calc/get_seeding_locations_3d_slice.m
@@ -8,20 +8,20 @@ rng(seed)
 
 switch modality
     case {'grid', 'gridded'}
-        seeding_locs.X = X0(:); 
-        seeding_locs.Y = Y0(:);
-        seeding_locs.Z = Z0(:);
+        num_points = size(X0, 1);
+        locs_idx = 1:num_points;       
     case {'random_sparse', 'random-sparse'}
-        seeding_locs.X = min(X0(:)) + (max(X0(:)) - min(X0(:))) * rand(floor(size(X0,1)./8), 1);
-        seeding_locs.Y = min(Y0(:)) + (max(Y0(:)) - min(Y0(:))) * rand(floor(size(Y0,1)./8), 1); 
-        seeding_locs.Z = min(Z0(:)) + (max(Z0(:)) - min(Z0(:))) * rand(floor(size(Z0,1)./8), 1);               
+        num_points = floor(size(X0,1)/8);
+        locs_idx  = randi([1 length(X0(:))], num_points, 1);              
     case {'random_dense', 'random-dense'}
-        seeding_locs.X = min(X0(:)) + (max(X0(:)) - min(X0(:))) * rand(size(X0,1),1);
-        seeding_locs.Y = min(Y0(:)) + (max(Y0(:)) - min(Y0(:))) * rand(size(Y0,1),1);
-        seeding_locs.Z = min(Z0(:)) + (max(Z0(:)) - min(Z0(:))) * rand(size(Z0,1),1);
+        num_points = floor(size(X0,1)/4);
+        locs_idx  = randi([1 length(X0(:))], num_points, 1);
     otherwise
         error(['neural-flows::' mfilename '::UnknownCase'], ...
               'Unknown modality for seeding streamlines on a structured 3D grid.');        
 end
+seeding_locs.X = X0(locs_idx); 
+seeding_locs.Y = Y0(locs_idx);
+seeding_locs.Z = Z0(locs_idx);
 
 end % function get_seeding_locations_3d_slice()
