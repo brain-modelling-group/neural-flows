7d2ee8f21383b4e85bfface1c7397c7ea7cd8ef8
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-06-19 20:22:21 +1000

    refactor: Unify cases of random seeding

--- utils/calc/get_seeding_locations.m
+++ utils/calc/get_seeding_locations.m
@@ -8,11 +8,18 @@ switch modality
     case {"nodes", "regions", "nodal"}
         seed_locs = locs;
         % do something
-    case {"random"}
+    case {"random-dense", "random_dense"}
         % seeds 2*num nodes streamlines
         rng(seed)
         node_idx1 = randi([1, size(locs, 1)], size(locs, 1),  1);
         node_idx2 = randi([1, size(locs, 1)], size(locs, 1),  1);
+        seed_locs = (locs(node_idx1, :) + locs(node_idx2, :))/2;
+        seed_locs = vertcat(seed_locs, locs);
+     case {"random-sparse", "random_sparse"}
+        % seeds 2*num nodes streamlines
+        rng(seed)
+        node_idx1 = randi([1, size(locs, 1)], round(size(locs, 1)/2), 1);
+        node_idx2 = randi([1, size(locs, 1)], round(size(locs, 1)/2), 1);
         seed_locs = (locs(node_idx1, :) + locs(node_idx2, :))/2;
         seed_locs = vertcat(seed_locs, locs);
     otherwise
