cd6d724d1fc391579f865c771826959e804a9f68
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-06-19 23:00:29 +1000

    Reduce seeding locations to avoid using lots of space

--- utils/calc/get_seeding_locations_3d_slice.m
+++ utils/calc/get_seeding_locations_3d_slice.m
@@ -12,9 +12,9 @@ switch modality
         seeding_locs.Y = Y0(:);
         seeding_locs.Z = Z0(:);
     case {'random_sparse', 'random-sparse'}
-        seeding_locs.X = min(X0(:)) + (max(X0(:)) - min(X0(:))) * rand(floor(size(X0,1)./4), 1);
-        seeding_locs.Y = min(Y0(:)) + (max(Y0(:)) - min(Y0(:))) * rand(floor(size(Y0,1)./4), 1); 
-        seeding_locs.Z = min(Z0(:)) + (max(Z0(:)) - min(Z0(:))) * rand(floor(size(Z0,1)./4), 1);               
+        seeding_locs.X = min(X0(:)) + (max(X0(:)) - min(X0(:))) * rand(floor(size(X0,1)./8), 1);
+        seeding_locs.Y = min(Y0(:)) + (max(Y0(:)) - min(Y0(:))) * rand(floor(size(Y0,1)./8), 1); 
+        seeding_locs.Z = min(Z0(:)) + (max(Z0(:)) - min(Z0(:))) * rand(floor(size(Z0,1)./8), 1);               
     case {'random_dense', 'random-dense'}
         seeding_locs.X = min(X0(:)) + (max(X0(:)) - min(X0(:))) * rand(size(X0,1),1);
         seeding_locs.Y = min(Y0(:)) + (max(Y0(:)) - min(Y0(:))) * rand(size(Y0,1),1);
