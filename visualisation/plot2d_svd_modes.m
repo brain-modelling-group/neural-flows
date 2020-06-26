23f6c82c75e9aa604b26f76051c6047bfe641c70
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-06-26 22:28:52 +1000

    Debug:New: add missing function to plot svd modes - bug found by @j-roberts

---
+++ visualisation/plot2d_svd_modes.m
@@ -0,0 +1,18 @@
+function fig_handle = plot2d_svd_modes(params)
+% wrapper function
+
+% Lod necessary data
+obj_flows = load_iomat_flows(params);
+svd_data = obj_flows.svd; 
+
+% Define a few vecs we need
+time_vec = params.data.ht:params.data.ht:params.flows.data.shape.t*params.data.ht; 
+quiver_scale_factor = params.visualisation.quiver.scale;
+num_modes = params.flows.decomposition.svd.modes;
+
+fig_handle =  plot_svd_modes(vertcat(svd_data.V.vx, svd_data.V.vy, svd_data.V.vy), ...
+	                         svd_data.U, svd_data.XYZ.X, svd_data.XYZ.Y, svd_data.XYZ.Z, ...
+                             time_vec, num_modes, svd_data.num_points, ...
+                             svd_data.prct_var, quiver_scale_factor);
+
+end % function plot2d_svd_modes()
