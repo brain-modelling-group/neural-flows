7da9a1f6cd7aeea0d07c72de0433c1a778bfbd02
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-06-26 12:15:32 +1000

    Udpate: enable/disble new featrues

--- analysis/energy_states.m
+++ analysis/energy_states.m
@@ -76,22 +76,22 @@ maxima = sortrows([-energy(maxima); maxima]').';
 maxima = maxima(2,:);
 
 % Display displacement energy and locations of maxima/minima
-if display_flag
-  hold(ax_handles(1), 'on')
-  plot(ax_handles(1), time_vec, energy, 'color', [0.8 0.8 0.8])
-  xlabel('Time [ms]')
-  ylabel('\partial Energy')
-  ax_handles(1).XLim = [time_vec(1) time_vec(end)];
-  ax_handles(1).YLim = [0.95*min(energy) 1.05*max(energy)];
-  plot(ax_handles(1), time_vec(minima), energy(minima), 'linestyle', 'none', ...
-      'marker', 'v', 'markerfacecolor', [111,203,159]/255, 'markeredgecolor', 'none') 
-  plot(ax_handles(1), time_vec(maxima), energy(maxima), 'linestyle', 'none', ...
-      'marker', '^', 'markerfacecolor', [255,111,105]/255, 'markeredgecolor', 'none');
-  
-  ax_handles(1).XLabel.String = 'time [ms]';
-  ax_handles(1).YLabel.String ='\partial Energy';
-  ax_handles(1).Box = 'on';
-end
+% if display_flag
+%   hold(ax_handles(1), 'on')
+%   plot(ax_handles(1), time_vec, energy, 'color', [0.8 0.8 0.8])
+%   xlabel('Time [ms]')
+%   ylabel('\partial Energy')
+%   ax_handles(1).XLim = [time_vec(1) time_vec(end)];
+%   ax_handles(1).YLim = [0.95*min(energy) 1.05*max(energy)];
+%   plot(ax_handles(1), time_vec(minima), energy(minima), 'linestyle', 'none', ...
+%       'marker', 'v', 'markerfacecolor', [111,203,159]/255, 'markeredgecolor', 'none') 
+%   plot(ax_handles(1), time_vec(maxima), energy(maxima), 'linestyle', 'none', ...
+%       'marker', '^', 'markerfacecolor', [255,111,105]/255, 'markeredgecolor', 'none');
+%   
+%   ax_handles(1).XLabel.String = 'time [ms]';
+%   ax_handles(1).YLabel.String ='\partial Energy';
+%   ax_handles(1).Box = 'on';
+% end
 
 
 % Find transient states
