3233fde361c584507623e2717b5b8f6d7ee12505
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-07-08 21:57:34 +1000

    Add missing semicolon

--- examples/rotating_wave.m
+++ examples/rotating_wave.m
@@ -8,10 +8,10 @@ function rotating_wave(case_label)
 % |    Host   | Data Domain    | Data Mode   | Flow Method     | # workers  |  Runtime |  Memory |
 % |-----------|----------------|-------------|-----------------|------------|----------|---------|
 % | dracarys  | (u)nstructured | (a)mplitude | (h)orn-schunk3d |      11    |  5  min  |   6 GB  |
-% | dracarys  | (u)nstructured | (a)mplitude | (c)nem|         |      8     |  X       |    X    | 
+% | dracarys  | (u)nstructured | (a)mplitude | (c)nem          |      8     |  X       |    X    | 
 % | tesseract | (u)nstructured | (a)mplitude | (h)orn-schunk3d |      6     |  3.73 min| 2.5 GB  |  
-% | tesseract | (u)nstructured | (a)mplitude | (c)nem|         |      8     |  0.75 min| 500 MB  |
-% | tesseract | (u)nstructured | (p)hase     | (c)nem|         |      8     |  0.75 min| 500 MB  |
+% | tesseract | (u)nstructured | (a)mplitude | (c)nem          |      8     |  0.75 min| 500 MB  |
+% | tesseract | (u)nstructured | (p)hase     | (c)nem          |      8     |  0.75 min| 500 MB  |
 
 if nargin < 1
     case_label = 'uah';
@@ -39,7 +39,7 @@ output_params = main_core(input_params);
 main_analysis(output_params);
 
 %% Visualisation
-main_visualisation(output_params)
+main_visualisation(output_params);
 
 
 end %function example_rotating_wave()
