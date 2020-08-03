e8bb521cfd5e62cd860f6debc3311621db337ba0
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-08-03 17:27:11 +1000

    Update: use bars for linear counts

--- visualisation/plot1d_singularity_traces_counts.m
+++ visualisation/plot1d_singularity_traces_counts.m
@@ -19,9 +19,10 @@ function figure_handle = plot1d_singularity_traces_counts(singularity_counts, va
 
 %}
 
-figure_handle = plot_fun(singularity_counts);
+figure_handle{1} = plot_fun(singularity_counts, 'log');
+figure_handle{2} = plot_fun(singularity_counts, 'lin');
 
-    function figure_handle = plot_fun(singularity_counts) 
+    function figure_handle = plot_fun(singularity_counts, modality)
         base_list = s3d_get_base_singularity_list();
         cmap_base_list = s3d_get_colours('critical-points');
         
@@ -35,9 +36,11 @@ figure_handle = plot_fun(singularity_counts);
             ax(jj) = subplot(4, 2, jj, 'Parent', figure_handle);
             hold(ax(jj), 'on')
         end
+        
+        if strcmp(modality, 'log')
 
         for jj=1:numsubplots
-             stem(ax(jj), log10(singularity_counts(:, jj)), 's', 'markerfacecolor', [0.65 0.65 0.65], ...
+             stem(ax(jj), log10(singularity_counts(:, jj)), 'o', 'linewidth', 1.5, 'markerfacecolor', [0.65 0.65 0.65], ...
                                                             'markeredgecolor',cmap_base_list(jj, :), ...
                                                             'color',  cmap_base_list(jj, :), 'markersize', 3.4)
             ax(jj).Title.String = base_list{jj};
@@ -46,8 +49,27 @@ figure_handle = plot_fun(singularity_counts);
             ax(jj).XLim = [-1 tpts+1];
             ax(jj).YLim = [-0.1 2.5];
         end
+        else
+           for jj=1:numsubplots
+%             stem(ax(jj), singularity_counts(:, jj), 'o', 'linewidth', 1.5, 'markerfacecolor', [0.65 0.65 0.65], ...
+%                                                           'markeredgecolor',cmap_base_list(jj, :), ...
+%                                                           'color',  cmap_base_list(jj, :), 'markersize', 3.4)
+                                                      
+            bar(ax(jj), singularity_counts(:, jj), 'facecolor',  cmap_base_list(jj, :), 'edgecolor', 'k')
+            ax(jj).Title.String = base_list{jj};
+            if jj>6 
+            ax(jj).XLabel.String = 'time';
+            else
+                ax(jj).XLabel.String = '';
+                ax(jj).XTick = [];
+            end
+            ax(jj).YLabel.String = '#';
+            ax(jj).XLim = [-1 tpts+1];
+            %ax(jj).YLim = [-0.1 2.5];
+          end
         
         linkaxes(ax, 'x')
+        end
     end
 
 end
