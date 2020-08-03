8e3936a5466bb997ba4511d93b43826c3c50b31f
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-08-03 16:39:20 +1000

    New: composite plotting function for plots with descriptive stats

---
+++ visualisation/plot1d_singularity_stats.m
@@ -0,0 +1,11 @@
+function figure_handles = plot1d_singularity_stats(inparams)
+% Composite plotting function to display histograms and traces of each
+% singularity over time
+
+obj_sings = load_iomat_singularity(inparams);
+
+figure_handles{1} = plot1d_singularity_counts(obj_sings.count);
+figure_handles{2} = plot_cp_histograms(obj_sings.classification_str);
+
+
+end
