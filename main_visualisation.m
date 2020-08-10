145714f4055935254c25b2acd4727e314919122a
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-08-10 13:11:34 +1000

    Update main vis fun

--- main_visualisation.m
+++ main_visualisation.m
@@ -8,14 +8,14 @@ tstart = tik();
 
 %---------------------------------DECOMPOSITION--------------------------------%
 if inparams.flows.visualisation.enabled
-    %plot1d_speed_distribution(inparams);
-    %plot2d_svd_modes(inparams);
+    plot1d_speed_distribution(inparams);
+    plot2d_svd_modes(inparams);
 end
 %---------------------------SINGULARITY----------------------------------------%
 if inparams.singularity.visualisation.enabled
-   %plot2d_singularity_occupancy(inparams);
+   plot2d_singularity_occupancy(inparams);
    plot1d_singularity_traces_sandplot(inparams);
-   %plot1d_singularity_stats(inparams)
+   plot1d_singularity_stats(inparams)
 end
 %------------------------------------------------------------------------------%
 % save_tmp_params(tmp_params)
