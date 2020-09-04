d074eaa9b323a9764633be0235ab7d1dc2f8599b
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-09-04 12:15:08 +1000

    Enable histograms using node-based singularities

--- visualisation/plot1d_singularity_stats.m
+++ visualisation/plot1d_singularity_stats.m
@@ -5,6 +5,10 @@ function figure_handles = plot1d_singularity_stats(inparams)
 obj_sings = load_iomat_singularity(inparams);
 
 figure_handles{1} = plot1d_singularity_traces_counts(obj_sings.count);
-figure_handles{2} = plot1d_singularity_histograms(obj_sings.count);
 
+if inparams.singularity.quantification.nodal_occupancy.enabled
+    figure_handles{2} = plot1d_singularity_histograms(obj_sings.tracking_3d_matrix);
+else
+    figure_handles{2} = plot1d_singularity_histograms(obj_sings.count);
+end
 end % function plot1d_singularity_stats()
