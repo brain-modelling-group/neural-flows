3aa9e4cc310aa8ded7700a9806ba27d8854c0ba0
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-10-20 15:31:36 +1000

    Implement quick pathline visualisation

---
+++ visualisation/plot3d_pathlines.m
@@ -0,0 +1,23 @@
+function plot3d_pathlines(obj_streams)
+
+
+locs = obj_streams.initial_seeding_locs;
+
+
+% Start locations
+plot3(locs(:, 1), locs(:, 2), locs(:, 3), 'k.')
+hold on
+
+
+	pathlines = obj_streams.pathlines;
+
+    for ii=1:length(pathlines)
+       plot3(squeeze(pathlines{ii}(:, 1)), ...
+             squeeze(pathlines{ii}(:, 2)), ...
+             squeeze(pathlines{ii}(:, 3)), 'color', [0.5 0.5 0.5 0.1]);
+        plot3(squeeze(pathlines{ii}(end, 1)), ...
+              squeeze(pathlines{ii}(end, 2)), ...
+              squeeze(pathlines{ii}(end, 3)), '.', 'markeredgecolor', [1 0.0 0.0], 'markersize', 0.042);
+    end
+ 
+end % function plot3d_pathlines()
