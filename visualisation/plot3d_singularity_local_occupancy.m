e39bd047b64ae9b7da9496c4f45261a9dcb9d2a8
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-11-12 12:30:39 +1000

    Fix bug with size data

--- visualisation/plot3d_singularity_local_occupancy.m
+++ visualisation/plot3d_singularity_local_occupancy.m
@@ -8,7 +8,7 @@ num_base_sngs = size(obj_singularity, 'tracking_3d_matrix', 1);
 total_frames = size(obj_singularity, 'tracking_3d_matrix', 3);
 
 % Load data to plot
-data = sum(obj_singularity.tracking_3d_matrix, 3);
+nodal_singularity_summary = obj_singularity.nodal_singularity_summary;
 
 plot_spheres();
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Plotting functions %%%%%%%%%%%%%%%%%%%%%%%
@@ -16,14 +16,17 @@ plot_spheres();
          
         color_a = [0 0 0];
         for kk=1:num_base_sngs
+            data = nodal_singularity_summary.occupancy_partial(kk, :) / total_frames; 
             color_c = cmap(kk, :);
             color_b = color_c/2;
             color_map = interpolated_colourmap(color_a, color_b, color_c, 64);
-            sizedata = data(kk, :);
+            sizedata = data;
             sizedata(sizedata > 0) = 4;
             sizedata(sizedata == 0)= 1;
-            plot_sphereanim(data(kk, :)/total_frames, obj_singularity.locs, 1, [0 0.35], color_map, 'workspace', sizedata)
+            min_val = 0.0;
+            max_val = max(data)*100;
+            plot_sphereanim(data*100, obj_singularity.locs, 1, [min_val max_val], color_map, 'workspace', sizedata)
         end 
     end % function plot_spheres()
    
-end % function plot1d_singularity_spheres()
+end % function plot3d_singularity_local_occupancy()
