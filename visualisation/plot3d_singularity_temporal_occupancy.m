f24e42ee6b37a0e83259623c2edc581b203cb97c
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-11-12 16:08:16 +1000

    Use 'handle' animation environment; return figure handles.

--- visualisation/plot3d_singularity_temporal_occupancy.m
+++ visualisation/plot3d_singularity_temporal_occupancy.m
@@ -1,4 +1,4 @@
-function plot3d_singularity_local_occupancy(params)
+function fig_handles = plot3d_singularity_temporal_occupancy(params)
 
 obj_singularity = load_iomat_singularity(params);
 % TODO: configiure graphics property to handle paper units so we can plot
@@ -10,11 +10,12 @@ total_frames = size(obj_singularity, 'tracking_3d_matrix', 3);
 % Load data to plot
 nodal_singularity_summary = obj_singularity.nodal_singularity_summary;
 
-plot_spheres();
+fig_handles = plot_spheres();
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Plotting functions %%%%%%%%%%%%%%%%%%%%%%%
-    function plot_spheres()
+    function fig_handles = plot_spheres()
          
         color_a = [0 0 0];
+        fig_handles = cell(num_base_sngs, 1);
         for kk=1:num_base_sngs
             data = nodal_singularity_summary.occupancy_partial(kk, :) / total_frames; 
             color_c = cmap(kk, :);
@@ -25,7 +26,8 @@ plot_spheres();
             sizedata(sizedata == 0)= 1;
             min_val = 0.0;
             max_val = max(data)*100;
-            plot_sphereanim(data*100, obj_singularity.locs, 1, [min_val max_val], color_map, 'workspace', sizedata)
+            [fig_handles{kk}] = plot_sphereanim(data*100, obj_singularity.locs, 1, [min_val max_val], color_map, 'handle', sizedata);
+            fig_handles{kk}.Children(2).Visible = 'off';
         end 
     end % function plot_spheres()
    
