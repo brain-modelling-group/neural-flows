de8231f50720c9351c804a6c235a4f107f2025b4
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-11-12 21:21:37 +1000

    Fix path to resolution fields

--- analysis/calculate_kinetic_energy.m
+++ analysis/calculate_kinetic_energy.m
@@ -39,7 +39,7 @@ display_flag = 'true';
 extrema_detection = 'peaks';
 % TODO: TOFIX: use resolution and length of FLOWS/NOT DATA
 time_vec = params.data.resolution.ht:params.data.resolution.ht:params.flows.data.shape.t*params.data.resolution.ht;
-min_duration_stable_state = params.data.ht * 30;
+min_duration_stable_state = params.data.resolution.ht * 30;
 [stable, transient, stablePoints, transientPoints] = energy_states(energy_struct.component_sum_norm, time_vec, params.data.ht, min_duration_stable_state, extrema_detection, display_flag);
 
 
