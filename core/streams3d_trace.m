3c75d5d68a139ff2b997f2622f2a5c73fac59592
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-11-13 12:08:47 +1000

    Document

--- core/streams3d_trace.m
+++ core/streams3d_trace.m
@@ -48,8 +48,8 @@ function  [params, obj_streams, obj_streams_sentinel] = streams3d_trace(params)
     end
        
     fprintf('%s \n', strcat('neural-flows:: ', mfilename, '::Info:: Started tracing of streamlines.'))
-    % Check if it's phase-based streams, or amplitude based
-    % If it's amplitude-based it can be nodal or gridded
+    % Check if it's phase-based streams, or amplitude-based
+    % If it's amplitude-based it can be meshless or mesh-based 
     switch params.streamlines.tracing.implementation
         case 'meshless'
             streams3d_tracing_cnem(obj_flows, obj_streams, params);
